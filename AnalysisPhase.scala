ce41fa9fd9f84d2e28cbb97400e620129e8bbb27
Philippe Suter <philippe.suter@gmail.com>
2012-11-23 17:21:17 +0100

    Work in progress.

--- AnalysisPhase.scala
+++ AnalysisPhase.scala
@@ -7,6 +7,10 @@ object AnalysisPhase extends LeonPhase[Program,VerificationReport] {
   val name = "Analysis"
   val description = "Leon Verification"
 
+  override def definedOptions = Set(
+    LeonFlagOptionDef("no-luck", "--no-luck", "Disable early model detection in solving loop.")
+  )
+
   def run(ctx: LeonContext)(program: Program) : VerificationReport = {
     new Analysis(program, ctx.reporter).analyse
   }
