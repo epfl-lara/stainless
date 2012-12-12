a24893295ebb4363b56e1a8fb6484218a8d769d1
Régis Blanc <regwblanc@gmail.com>
2012-12-12 22:22:01 +0100

    error tests also for purescala

---
+++ error/InstanceOf1.scala
@@ -0,0 +1,23 @@
+object InstanceOf1 {
+
+  abstract class A
+  case class B(i: Int) extends A
+  case class C(i: Int) extends A
+
+  abstract class Z
+  case class Y(i: Int) extends Z
+
+  def foo(): Int = {
+    //require(3.isInstanceOf[Int])
+    val b: A = B(2)
+    if(b.isInstanceOf[Y])
+      0
+    else
+      -1
+  } ensuring(_ == 0)
+
+  def bar(): Int = foo()
+
+}
+
+// vim: set ts=4 sw=4 et:
