423b3795da761a9318ba03f084de7a53ea8698d2
Régis Blanc <regwblanc@gmail.com>
2012-12-14 20:53:37 +0100

    Remove EmptySet, EmptyMap and SingletonMap
    EmptySet, EmptyMap and SingletonMap are redundant, so they are removed.
    They are subsumed by FiniteSet and FiniteMap.
    
    This commit also deletes two testcases that were testing for equality
    between EmptySet and FiniteSet of 0 element. Obviously they no longer
    relevant. Additionnaly, this commit adds some new regression testcases for
    Maps and Sets.

---
+++ valid/MyMap.scala
@@ -0,0 +1,16 @@
+import leon.Utils._
+
+object MyMap {
+
+  def map1(): Int = {
+    val m = Map(1 -> 2, 2 -> 3, 3 -> 4)
+    m(2)
+  } ensuring(_ == 3)
+
+  def map2(): Boolean = {
+    val m1 = Map[Int, Int]()
+    val m2 = Map.empty[Int, Int]
+    m1 == m2
+  } holds
+
+}
---
+++ valid/MySet.scala
@@ -0,0 +1,16 @@
+import leon.Utils._
+
+object MySet {
+
+  def set1(): Boolean = {
+    val s = Set(1, 2, 3, 4)
+    s.contains(3)
+  } holds
+
+  def set2(): Boolean = {
+    val s1 = Set[Int]()
+    val s2 = Set.empty[Int]
+    s1 == s2
+  } holds
+
+}
