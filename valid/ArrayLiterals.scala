d0af2222ee73a41109009460cabc59a857689dd8
Etienne Kneuss <colder@php.net>
2013-10-08 19:12:07 +0200

    Support for literal arrays

---
+++ valid/ArrayLiterals.scala
@@ -0,0 +1,8 @@
+import leon.Utils._
+object Numerals {
+  def foo(): Int = {
+    val b : Array[Int] = Array[Int](1,2,3)
+    val a : Array[Int] = Array(1,2,3)
+    a.length
+  } ensuring { _ > 0 }
+}
