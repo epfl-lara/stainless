bfd498e7efb4d89c9ae9fa7eef5e8065c950557a
Etienne Kneuss <colder@php.net>
2014-01-14 14:09:03 +0100

    Implement generics for functions and ADTs
    - Calling functions are now typed, FunctionInvokation takes a
      TypedFunDef which is basically a FunDef with type parameters' values.
      Instantiation of types within the signature/body is done on demand
      through this wrapper class.
    
    - Operations on ADTs are now taking a *ClassType instead of *ClassDef.
      Similarly, Case/AbstractClassType takes values for type parameters.
    
    - Introduces a GenericValue tree for models targeting abstract types.
      e.g. foo[T](a: T, b: T) { a == b } ensuring (_) will find a model with
      { a -> T#1, b -> T#2 }
    
    - Only "simple" hierarchies allowed with type parameters. All members of
      the hierarchy must define the same number of type parameters and
      correctly pass them to parent classes.
    
    - Type parameters are invariant.

---
+++ invalid/Generics.scala
@@ -0,0 +1,13 @@
+import leon.Utils._
+
+object Generics1 {
+  abstract class List[T]
+  case class Cons[A](head: A, tail: List[A]) extends List[A]
+  case class Nil[B]() extends List[B]
+
+  def size[T](l: List[T]): Int = (l match {
+    case Nil() => 0
+    case Cons(h, t) => 1+size(t)
+  })ensuring { _ > 0 }
+
+}
