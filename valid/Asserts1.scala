ae721d20d6ec72bf8ba7d4051e4166c0ec45c07d
Etienne Kneuss <colder@php.net>
2014-05-02 13:39:58 +0200

    New Ensuring/Require/Assert trees
    - Ensurings/Require are now proper purescala trees
    - Introduce Assert as a tree
    - Simplify tactics to support arbitrary asserts/errors/inner ensurings
    - New phase to inject assertions for unsafe calls (array/map access)
    - Refactor Default and Induction tactic

---
+++ valid/Asserts1.scala
@@ -0,0 +1,32 @@
+import leon.lang._
+import leon.annotation._
+import leon._
+
+object Operators {
+
+  def foo(a: Int): Int = {
+    require(a > 0)
+
+    {
+      val b = a
+      assert(b > 0, "Hey now")
+      b + bar(1)
+    } ensuring { _ > 2 }
+
+  } ensuring {
+    _ > a
+  }
+
+  def bar(a: Int): Int = {
+    require(a > 0)
+
+    {
+      val b = a
+      assert(b > 0, "Hey now")
+      b + 2
+    } ensuring { _ > 2 }
+
+  } ensuring {
+    _ > a
+  }
+}
