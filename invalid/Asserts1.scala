ffbd30c3269485ef92b0e334afc42cf54b5833c3
Emmanouil (Manos) Koukoutos <emmanouil.koukoutos@epfl.ch>
2014-11-27 16:22:57 +0100

    Rename Passes to Gives, and further integration

--- invalid/Asserts1.scala
+++ invalid/Asserts1.scala
@@ -6,27 +6,19 @@ object Operators {
 
   def foo(a: Int): Int = {
     require(a > 0)
-
-    {
-      val b = a
-      assert(b > 0, "Hey now")
-      b + bar(1)
-    } ensuring { _ < 2 }
-
-  } ensuring {
-    _ > a
+    val b = a
+    assert(b > 0, "Hey now")
+    b + bar(1)
+  } ensuring { res =>
+    res > a && res < 2
   }
 
   def bar(a: Int): Int = {
     require(a > 0)
-
-    {
-      val b = a
-      assert(b > 0, "Hey now")
-      b + 2
-    } ensuring { _ > 2 }
-
-  } ensuring {
-    _ > a
+    val b = a
+    assert(b > 0, "Hey now")
+    b + 2
+  } ensuring { res =>
+    res > a && res > 2
   }
 }
