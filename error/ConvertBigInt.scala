8194d49f2cd56dd9e401f473dbd4c1c39b1be4cf
Regis Blanc <regwblanc@gmail.com>
2015-02-20 15:51:35 +0100

    cleaner BigInt extractor front-end

---
+++ error/ConvertBigInt.scala
@@ -0,0 +1,7 @@
+/* Copyright 2009-2014 EPFL, Lausanne */
+
+object ConvertBigInt {
+
+  def safeInt(x: Int): BigInt = x
+
+}
---
+++ error/LiteralBigInt.scala
@@ -0,0 +1,7 @@
+/* Copyright 2009-2014 EPFL, Lausanne */
+
+object LiteralBigInt {
+
+  def safeInt(x: Int): BigInt = BigInt(x)
+
+}
