8d79a55b933e02ae28d8068a0b101010eb1ebb62
Etienne Kneuss <ekneuss@gmail.com>
2015-04-01 13:13:25 +0200

    Extract ensuring(expr: Boolean) as ensuring(res => expr)

---
+++ invalid/EnsuringBoolean.scala
@@ -0,0 +1,10 @@
+import leon._
+import leon.lang._
+import leon.annotation._
+import scala.language.postfixOps 
+object EnsuringBoolean {
+  def congR(x: BigInt)(implicit mod: BigInt): Unit = {
+    require(mod >= 1);
+    ()
+  } ensuring(false)
+}
