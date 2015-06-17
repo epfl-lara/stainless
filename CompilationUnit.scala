36cf5aa6539c55d3a47ac6df138fc2ea2d5c2f0a
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2015-06-17 21:06:59 +0200

    Simplify/clean up extractors

--- CompilationUnit.scala
+++ CompilationUnit.scala
@@ -231,7 +231,7 @@ class CompilationUnit(val ctx: LeonContext,
       else GenericValue(tp, id).copiedFrom(gv)
 
     case (set: runtime.Set, SetType(b)) =>
-      finiteSet(set.getElements.asScala.map(jvmToExpr(_, b)).toSet, b)
+      FiniteSet(set.getElements.asScala.map(jvmToExpr(_, b)).toSet, b)
 
     case (map: runtime.Map, MapType(from, to)) =>
       val pairs = map.getElements.asScala.map { entry =>
@@ -239,7 +239,7 @@ class CompilationUnit(val ctx: LeonContext,
         val v = jvmToExpr(entry.getValue, to)
         (k, v)
       }
-      finiteMap(pairs.toSeq, from, to)
+      FiniteMap(pairs.toSeq, from, to)
 
     case (lambda: runtime.Lambda, _: FunctionType) =>
       val cls = lambda.getClass
