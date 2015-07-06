83a2b09e297782c3e17f5db90fd6dc49270cf886
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2015-07-06 17:26:32 +0200

    More support for inheritance
    Methods can be overriden in subclasses
    Non-root classes can define methods
    Deep hierarchies are supported

--- InductionTactic.scala
+++ InductionTactic.scala
@@ -40,7 +40,7 @@ class InductionTactic(vctx: VerificationContext) extends DefaultTactic(vctx) {
           }
 
           val vc = implies(
-            and(CaseClassInstanceOf(cct, arg.toVariable), precOrTrue(fd)), 
+            and(IsInstanceOf(cct, arg.toVariable), precOrTrue(fd)),
             implies(andJoin(subCases), application(post, Seq(body)))
           )
 
@@ -77,7 +77,7 @@ class InductionTactic(vctx: VerificationContext) extends DefaultTactic(vctx) {
           }
 
           val vc = implies(
-            andJoin(Seq(CaseClassInstanceOf(cct, arg.toVariable), precOrTrue(fd), path) ++ subCases),
+            andJoin(Seq(IsInstanceOf(cct, arg.toVariable), precOrTrue(fd), path) ++ subCases),
             tfd.withParamSubst(args, pre)
           )
 
