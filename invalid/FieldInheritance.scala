f25ed9368c6474a186d02ad562e4672e8d4ddf73
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2015-08-27 12:03:58 +0200

    Handle case-class field implementing method

---
+++ invalid/FieldInheritance.scala
@@ -0,0 +1,22 @@
+import leon.lang._
+
+object FieldInheritance {
+
+  abstract class Foo[B] {
+    val thisIsIt: BigInt = 1
+    val y: BigInt
+    val weird: B
+  }
+
+  case class Bar[X](override val thisIsIt: BigInt, weird: X) extends Foo[X] {
+    val y = thisIsIt
+  }
+  
+  case class Baz[X](weird: X) extends Foo[X] {
+    val y = thisIsIt + 1
+  }
+
+
+  def foo[A](f: Foo[A]) = (f.thisIsIt == 1).holds
+
+}
