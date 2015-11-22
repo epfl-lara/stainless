c6e40a5093b9ac4e99601e9f263ca18a82af6cab
Nicolas Voirol <voirol.nicolas@gmail.com>
2015-11-22 20:38:22 +0100

    Efficient procedure for transitive dependencies in quantifiers

---
+++ invalid/Existentials.scala
@@ -0,0 +1,16 @@
+import leon.lang._
+
+object Existentials {
+
+  def exists[A](p: A => Boolean): Boolean = !forall((x: A) => !p(x))
+
+  def check1(y: BigInt, p: BigInt => Boolean) : Boolean = {
+    p(y) == exists((y1:BigInt) => p(y1))
+  }.holds
+
+  /*
+  def check2(y: BigInt, p: BigInt => Boolean) : Boolean = {
+    p(y) ==> exists((y1:BigInt) => p(y1))
+  }.holds
+  */
+}
