4aa7d4cce3f1d5a175d8f1cce305d8c2486fa0ad
Nicolas Voirol <voirol.nicolas@gmail.com>
2015-12-09 17:36:23 +0100

    Improved terminator reliability by increasing timeout

---
+++ valid/Client.scala
@@ -0,0 +1,18 @@
+import leon.collection._
+import leon.lang._
+
+object Minimal {
+
+  case class Client(f: Int => List[Int])
+
+  val client = Client(x => List(1))
+
+  //   def f(x: Int) = List(1)
+  //   val client = Client(f)
+
+  def theorem() = {
+    client.f(0).size != BigInt(0)
+  } holds
+
+}
+
