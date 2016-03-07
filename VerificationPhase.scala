fdbf561bf19d7eef156c771a56a373e379f79b66
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2016-03-07 17:42:44 +0100

    SharedOptions -> GlobalOptions

--- VerificationPhase.scala
+++ VerificationPhase.scala
@@ -22,8 +22,8 @@ object VerificationPhase extends SimpleLeonPhase[Program,VerificationReport] {
   implicit val debugSection = utils.DebugSectionVerification
 
   def apply(ctx: LeonContext, program: Program): VerificationReport = {
-    val filterFuns: Option[Seq[String]] = ctx.findOption(SharedOptions.optFunctions)
-    val timeout:    Option[Long]        = ctx.findOption(SharedOptions.optTimeout)
+    val filterFuns: Option[Seq[String]] = ctx.findOption(GlobalOptions.optFunctions)
+    val timeout:    Option[Long]        = ctx.findOption(GlobalOptions.optTimeout)
 
     val reporter = ctx.reporter
 
