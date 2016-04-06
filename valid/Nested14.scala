e490fa95a3474281a3764bc16064d9d165836aaa
Nicolas Voirol <voirol.nicolas@gmail.com>
2016-04-06 16:01:05 +0200

    Bags in Leon

--- valid/Nested14.scala
+++ valid/Nested14.scala
@@ -4,7 +4,11 @@ object Nested14 {
 
   def foo(i: Int): Int = {
     def rec1(j: Int): Int = {
-      def rec2(k: Int): Int = if(k == 0) 0 else rec1(j-1)
+      require(j >= 0)
+      def rec2(k: Int): Int = {
+        require(j > 0 || j == k)
+        if(k == 0) 0 else rec1(j-1)
+      }
       rec2(j)
     }
     rec1(3)
