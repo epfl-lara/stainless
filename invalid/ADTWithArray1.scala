83d76547da0531d6b218cedb83fdfcb95a2b6611
Regis Blanc <regwblanc@gmail.com>
2016-06-15 14:48:54 +0200

    extra tests for array and adts

---
+++ invalid/ADTWithArray1.scala
@@ -0,0 +1,13 @@
+object ADTWithArray1 {
+
+  case class A(x: Int)
+
+  case class B(content: Array[A]) {
+    require(content.length > 0 && content.length < 100)
+
+    def contains(y: Int): Boolean = {
+      content(0).x == y
+    } ensuring(res => res)
+  }
+
+}
