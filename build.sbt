05208ea686671e6090b09e749d5ad04d9c617c4f
Nicolas Voirol <voirol.nicolas@gmail.com>
2016-10-17 14:15:08 +0200

    Update to latest Inox + Array handling in InoxEncoder

--- build.sbt
+++ build.sbt
@@ -103,7 +103,7 @@ testOptions in IntegrationTest := Seq(Tests.Argument("-oDF"))
 
 def ghProject(repo: String, version: String) = RootProject(uri(s"${repo}#${version}"))
 
-lazy val inox = ghProject("git://github.com/epfl-lara/inox.git", "8eef6f0c274d6ce902c03aa5a9f054b8e8ccf65d")
+lazy val inox = ghProject("https://github.com/epfl-lara/inox.git", "11912ad9d51318cf4c8935222024f852c920cf2e")
 lazy val dotty = ghProject("git://github.com/lampepfl/dotty.git", "fb1dbba5e35d1fc7c00250f597b8c796d8c96eda")
 lazy val cafebabe = ghProject("git://github.com/psuter/cafebabe.git", "49dce3c83450f5fa0b5e6151a537cc4b9f6a79a6")
 
--- src/main/scala/stainless/solvers/InoxEncoder.scala
+++ src/main/scala/stainless/solvers/InoxEncoder.scala
@@ -3,66 +3,176 @@
 package stainless
 package solvers
 
+import inox.ast._
+
 trait InoxEncoder extends inox.solvers.combinators.ProgramEncoder {
   val sourceProgram: Program
   val t: inox.trees.type = inox.trees
 
-  val encoder: inox.ast.SymbolTransformer {
-    val transformer: Encoder
+  import t.dsl._
+
+  protected val arrayID = FreshIdentifier("array")
+  protected val arr  = FreshIdentifier("arr")
+  protected val size = FreshIdentifier("size")
+
+  protected val arrayInvariantID = FreshIdentifier("array_inv")
+  protected val arrayInvariant = mkFunDef(arrayInvariantID)("A") { case Seq(aT) => (
+    Seq("array" :: T(arrayID)(aT)), t.BooleanType, { case Seq(array) =>
+      array.getField(size) >= E(0)
+    })
   }
 
-  val decoder: inox.ast.SymbolTransformer {
-    val transformer: Decoder
+  protected val arrayADT: t.ADTConstructor = {
+    val tdef = t.TypeParameterDef(t.TypeParameter(FreshIdentifier("A")))
+    new t.ADTConstructor(arrayID, Seq(tdef), None,
+      Seq(t.ValDef(arr, t.MapType(t.Int32Type, tdef.tp)), t.ValDef(size, t.Int32Type)),
+      Set(t.HasADTInvariant(arrayInvariantID))
+    )
   }
 
-  trait Encoder extends inox.ast.TreeTransformer {
-    val s: InoxEncoder.this.s.type = InoxEncoder.this.s
-    val t: InoxEncoder.this.t.type = InoxEncoder.this.t
+  protected val newADTs: Set[t.ADTDefinition] = Set(arrayADT)
+  protected val newFunctions: Set[t.FunDef] = Set.empty
 
-    object deconstructor extends {
-      val s: Encoder.this.s.type = Encoder.this.s
-      val t: Encoder.this.t.type = Encoder.this.t
-    } with inox.ast.TreeDeconstructor
+  val encoder: SymbolEncoder
+  val decoder: SymbolDecoder
 
-    import sourceProgram.symbols._
+  protected trait SymbolEncoder extends SymbolTransformer {
+    val transformer: TreeEncoder = new TreeEncoder
 
-    override def transform(e: s.Expr): t.Expr = e match {
-      case m: s.MatchExpr =>
-        transform(matchToIfThenElse(m))
+    override def transform(syms: s.Symbols): t.Symbols =
+      super.transform(syms).withADTs(newADTs.toSeq).withFunctions(newFunctions.toSeq)
 
-      case s.NoTree(_) =>
-        throw inox.FatalError("Unexpected tree: " + e)
+    protected class TreeEncoder extends TreeTransformer {
+      val s: InoxEncoder.this.s.type = InoxEncoder.this.s
+      val t: InoxEncoder.this.t.type = InoxEncoder.this.t
 
-      case s.Error(tpe, desc) =>
-        t.Variable(inox.ast.FreshIdentifier("error: " + desc, true), transform(tpe)).copiedFrom(e)
+      object deconstructor extends {
+        val s: TreeEncoder.this.s.type = TreeEncoder.this.s
+        val t: TreeEncoder.this.t.type = TreeEncoder.this.t
+      } with TreeDeconstructor
 
-      case s.Require(pred, body) =>
-        t.Assume(transform(pred), transform(body)).copiedFrom(e)
+      import sourceProgram.symbols._
 
-      case s.Ensuring(s.Require(pred, body), s.Lambda(Seq(res), post)) =>
-        val vd = t.ValDef(res.id, transform(res.tpe))
-        t.Assume(transform(pred),
-          t.Let(vd, transform(body), t.Assume(transform(post), vd.toVariable)))
+      override def transform(e: s.Expr): t.Expr = e match {
+        case m: s.MatchExpr =>
+          transform(matchToIfThenElse(m))
 
-      case s.Ensuring(body, s.Lambda(Seq(res), post)) =>
-        val vd = t.ValDef(res.id, transform(res.tpe))
-        t.Let(vd, transform(body), t.Assume(transform(post), vd.toVariable))
+        case s.NoTree(_) =>
+          throw inox.FatalError("Unexpected tree: " + e)
 
-      case s.Assert(pred, error, body) =>
-        t.Assume(transform(pred), transform(body))
+        case s.Error(tpe, desc) =>
+          t.Variable(FreshIdentifier("error: " + desc, true), transform(tpe)).copiedFrom(e)
 
-      case _ => super.transform(e)
+        case s.Require(pred, body) =>
+          t.Assume(transform(pred), transform(body)).copiedFrom(e)
+
+        case s.Ensuring(s.Require(pred, body), s.Lambda(Seq(res), post)) =>
+          val vd = t.ValDef(res.id, transform(res.tpe))
+          t.Assume(transform(pred),
+            t.Let(vd, transform(body), t.Assume(transform(post), vd.toVariable)))
+
+        case s.Ensuring(body, s.Lambda(Seq(res), post)) =>
+          val vd = t.ValDef(res.id, transform(res.tpe))
+          t.Let(vd, transform(body), t.Assume(transform(post), vd.toVariable))
+
+        case s.Assert(pred, error, body) =>
+          t.Assume(transform(pred), transform(body))
+
+        case s.FiniteArray(elems, base) =>
+          t.ADT(t.ADTType(arrayID, Seq(transform(base))), Seq(
+            t.FiniteMap(
+              elems.zipWithIndex.map { case (e, i) => t.IntLiteral(i) -> transform(e) },
+              transform(simplestValue(base)),
+              t.Int32Type),
+            t.IntLiteral(elems.size)
+          ))
+
+        case s.LargeArray(elems, dflt, size) =>
+          t.ADT(t.ADTType(arrayID, Seq(transform(dflt.getType))), Seq(
+            t.FiniteMap(
+              elems.toSeq.map(p => t.IntLiteral(p._1) -> transform(p._2)),
+              transform(dflt),
+              t.Int32Type),
+            transform(size)
+          ))
+
+        case s.ArraySelect(array, index) =>
+          t.MapApply(t.ADTSelector(transform(array), arr), transform(index))
+
+        case s.ArrayUpdated(array, index, value) =>
+          val na = transform(array)
+          t.ADT(transform(array.getType).asInstanceOf[t.ADTType], Seq(
+            t.MapUpdated(t.ADTSelector(na, arr), transform(index), transform(value)),
+            t.ADTSelector(na, size)
+          ))
+
+        case s.ArrayLength(array) =>
+          t.ADTSelector(transform(array), size)
+
+        case _ => super.transform(e)
+      }
+    }
+
+    override def transform(tpe: s.Type): t.Type = tpe match {
+      case s.ArrayType(base) => t.ADTType(arrayID, Seq(transform(base)))
+      case _ => super.transform(tpe)
     }
   }
 
-  trait Decoder extends inox.ast.TreeTransformer {
-    val s: InoxEncoder.this.t.type = InoxEncoder.this.t
-    val t: InoxEncoder.this.s.type = InoxEncoder.this.s
+  protected trait SymbolDecoder extends SymbolTransformer {
+    val transformer: TreeDecoder = new TreeDecoder
 
-    object deconstructor extends {
-      val s: Decoder.this.s.type = Decoder.this.s
-      val t: Decoder.this.t.type = Decoder.this.t
-    } with inox.ast.TreeDeconstructor
+    protected val ignoredFunction = newFunctions.map(_.id)
+    protected val ignoredADT = newADTs.map(_.id)
+
+    override def transform(syms: s.Symbols): t.Symbols = t.NoSymbols
+      .withFunctions(syms.functions.values.toSeq.filterNot(fd => ignoredFunction(fd.id)).map(transformFunction))
+      .withADTs(syms.adts.values.toSeq.filterNot(adt => ignoredADT(adt.id)).map(transformADT))
+
+    protected class TreeDecoder extends TreeTransformer {
+      val s: InoxEncoder.this.t.type = InoxEncoder.this.t
+      val t: InoxEncoder.this.s.type = InoxEncoder.this.s
+
+      object deconstructor extends {
+        val s: TreeDecoder.this.s.type = TreeDecoder.this.s
+        val t: TreeDecoder.this.t.type = TreeDecoder.this.t
+      } with TreeDeconstructor
+
+      /** Transform back from encoded array ADTs into stainless arrays.
+        * Note that this translation should only occur on models returned by
+        * the Inox solver, so we can assume that the array adts have the
+        * shape:
+        * {{{
+        *    Array(FiniteMap(pairs, _, _), IntLiteral(size))
+        * }}}
+        * where all keys in {{{pairs}}} are IntLiterals. This assumption also
+        * holds on the output of [[SymbolsEncoder#TreeEncoder.transform(Expr)]].
+        */
+      override def transform(e: s.Expr): t.Expr = e match {
+        case s.ADT(
+          s.ADTType(`arrayID`, Seq(base)),
+          Seq(s.FiniteMap(elems, dflt, _), s.IntLiteral(size))
+        ) if size <= 10 =>
+          val elemsMap = elems.toMap
+          t.FiniteArray((0 until size).toSeq.map {
+            i => transform(elemsMap.getOrElse(s.IntLiteral(i), dflt))
+          }, transform(base))
+
+        case s.ADT(s.ADTType(`arrayID`, Seq(base)), Seq(s.FiniteMap(elems, dflt, _), size)) =>
+          t.LargeArray(
+            elems.map { case (s.IntLiteral(i), e) => i -> transform(e) }.toMap,
+            transform(dflt),
+            transform(size)
+          )
+
+        case _ => super.transform(e)
+      }
+
+      override def transform(tpe: s.Type): t.Type = tpe match {
+        case s.ADTType(`arrayID`, Seq(base)) => t.ArrayType(transform(base))
+        case _ => super.transform(tpe)
+      }
+    }
   }
 }
 
@@ -70,12 +180,7 @@ object InoxEncoder {
   def apply(p: StainlessProgram): InoxEncoder { val sourceProgram: p.type } = new {
     val sourceProgram: p.type = p
   } with InoxEncoder {
-    object encoder extends inox.ast.SymbolTransformer {
-      object transformer extends Encoder
-    }
-
-    object decoder extends inox.ast.SymbolTransformer {
-      object transformer extends Decoder
-    }
+    object encoder extends SymbolEncoder
+    object decoder extends SymbolDecoder
   }
 }
