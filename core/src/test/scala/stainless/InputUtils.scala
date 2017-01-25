17717d4e67a26bcfbc6c96990b0fbb5b57c2e691
Georg Schmid <gspschmid@gmail.com>
2017-01-25 02:07:53 +0100

    Deactivated LibraryLookupSuite from tests in stainless-core, since it has a dependency on (some) frontend and should thus eventually be moved to the frontend subprojects.

--- core/src/test/scala/stainless/InputUtils.scala
+++ core/src/test/scala/stainless/InputUtils.scala
@@ -1,25 +1,26 @@
-/* Copyright 2009-2016 EPFL, Lausanne */
-
-package stainless
-
-import extraction.xlang.{trees => xt}
-import java.io.{File, BufferedWriter, FileWriter}
-
-trait InputUtils {
-
-  def load(ctx: inox.Context, contents: Seq[String], compilerOpts: Seq[String] = Seq.empty):
-          (Seq[xt.UnitDef], Program { val trees: xt.type }) = {
-
-    val files = contents.map { content =>
-      val file = File.createTempFile("stainless", ".scala")
-      file.deleteOnExit()
-      val out = new BufferedWriter(new FileWriter(file))
-      out.write(content)
-      out.close()
-      file.getAbsolutePath
-    }
-
-    val allOpts = Build.libraryFiles ++ files ++ compilerOpts
-    frontends.scalac.ScalaCompiler(ctx, allOpts.toList)
-  }
-}
+// TODO: Move this to integration tests (has a dependency on the scalac frontend)
+///* Copyright 2009-2016 EPFL, Lausanne */
+//
+//package stainless
+//
+//import extraction.xlang.{trees => xt}
+//import java.io.{File, BufferedWriter, FileWriter}
+//
+//trait InputUtils {
+//
+//  def load(ctx: inox.Context, contents: Seq[String], compilerOpts: Seq[String] = Seq.empty):
+//          (Seq[xt.UnitDef], Program { val trees: xt.type }) = {
+//
+//    val files = contents.map { content =>
+//      val file = File.createTempFile("stainless", ".scala")
+//      file.deleteOnExit()
+//      val out = new BufferedWriter(new FileWriter(file))
+//      out.write(content)
+//      out.close()
+//      file.getAbsolutePath
+//    }
+//
+//    val allOpts = Build.libraryFiles ++ files ++ compilerOpts
+//    frontends.scalac.ScalaCompiler(ctx, allOpts.toList)
+//  }
+//}
