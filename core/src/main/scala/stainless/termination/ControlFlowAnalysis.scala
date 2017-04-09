415088e47eef6d3e2a3557d2b88655f654b8244d
Ravi <ravi.kandhadai@epfl.ch>
2017-04-10 01:05:06 +0200

    Trying to add a CFA phase

--- core/src/main/scala/stainless/termination/ControlFlowAnalysis.scala
+++ core/src/main/scala/stainless/termination/ControlFlowAnalysis.scala
@@ -5,30 +5,88 @@ package termination
 
 import scala.concurrent.duration._
 import scala.annotation.tailrec
-import solvers._
+import scala.collection._
+import scala.collection.mutable.{ Map => MutableMap, Set => MutableSet }
 
+import trees._
 /**
- * Checks terminations of functions using the ranking function specified in the `decreases`
- * construct. For now, it works only on first-order functions.
+ * A context-insensitive control-flow analysis that computes
+ * the closures that are passed to call backs of given function.
  */
-trait DecreasesProcessor extends Processor {
-
-  val name: String = "Decreases Processor"
-
-  import checker._
+trait CICFA { 
+  
+  val program: Program { val trees: Trees }
+  
   import program._
   import program.trees._
   import program.symbols._
   import program.trees.exprOps._
-
-  sealed abstract class FailReason
-  case object TryOther extends FailReason
-  case class FailStop(funDef: FunDef) extends FailReason
-
-  private val zero = IntegerLiteral(0)
-  private val tru = BooleanLiteral(true)
-
-  // TODO: Should we remove cached predicate before giving it to the solver ?
+  
+  val rootfun: program.trees.FunDef
+  
+  // Abstract values and closures
+  sealed abstract class AbsValue 
+//  {
+//    def greaterEquals(other: AbsValue): Boolean
+//  }
+  case class Closure(lam: program.trees.Lambda) extends AbsValue 
+//    env: AbsEnv
+//    def greaterEquals(other: AbsValue) = other match {
+//      case Closure(lam2, env2) => lam2 == lam && env.greater(env2)
+//      case _ => false
+//    }    
+//    override def hashCode = lam.hashCode // this works also for >= check
+//  }
+  case class External() extends AbsValue 
+//  {
+//    def greaterEquals(other: AbsValue) = other.isInstanceOf[External]
+//  }
+  
+  case class AbsEnv(store: Map[Variable,Set[AbsValue]]) { // mapping from a set of live variables to their value
+    // checks if this >= AbsElem
+    def greaterEquals(other: AbsEnv): Boolean = {
+      other.store.forall { (k,v) => store.contains(k) && 
+        other.store(k).subsetOf(store(k))
+      }
+    }
+    
+    def join(other: AbsEnv): AbsEnv = {
+      val ikeys = store.keySet.intersect(other.store.keySet)
+      AbsEnv((store.keySet ++ other.store.keyset).map{ 
+        case v if ikeys(v) => (v -> (store(v) ++ other.store(v)))
+        case v if store.contains(v) => (v -> store(v))
+        case v if other.store.contains(v) => (v -> other.store(v))
+      })       
+    }
+  }
+   
+  case class Summary(in: AbsEnv, out: AbsEnv) {
+    def get(fact: AbsEnv): Option[AbsEnv] = {
+      if(in.greaterEquals(fact))  Some(out) else None
+    }
+  }
+  
+  val tabulation = MutableMap[program.trees.FunDef, Summary] // summary of each function
+  val callers = Map[FunDef, Set[FunDef]]
+  
+  // initialize summaries to identity function from bot to bot 
+  program.definedFunctions.foreach { fd: FunDef => 
+    val bot = AbsEnv(fd.params.map { vd => vd.id.toVariable -> Set[AbsValue]() })
+    tabulation += (fd -> Summary(bot, bot))    
+  }
+  
+  val worklist = List(rootfun)
+  
+  // iteratively process functions from the worklist.
+  // (a) at every function call, join the arguments passed in with the in fact in the summary
+  //       -- if the join results in a greater value, add the function back to the worklist  
+  // (b) use the summary in the tabulation to complete the intra-procedural analysis
+  // (c) Update the caller information on seeing a function call.
+  // (d) if the return value of the function is found to be different from the return value in the tabulation   
+  //       -- update the entry in the tabulation to a the new value
+  //       -- add all callers of the function to the worklist
+  // Repeat this until a fix point is reached
+  
   def run(problem: Problem): Option[Seq[Result]] = {
     val fds = problem.funDefs
     val fdIds = problem.funSet.map(_.id)
---
+++ testcases/termination/usermeasure/higherorder/CyclicFibStream.scala
@@ -0,0 +1,42 @@
+package termination
+package usermeasure.higherorder
+
+import stainless._
+import lang._
+import annotation._
+import collection._
+
+object ZipWithAndFibStream {
+  /**
+   *  An infinite integer stream.
+   *  Technically, the data structure is *not* infinite but the tail has a higher-order function.
+   */
+  case class SCons(x: BigInt, tailFun: () => SCons) {    
+    lazy val tail = tailFun()
+  }
+  
+  /**
+   * A generic higher-order `zipWithFun` function. 
+   */
+  private def zipWithFun(f: (BigInt, BigInt) => BigInt, xs: SCons, ys: SCons): SCons = {
+    (xs, ys) match {
+      case (SCons(x, _), SCons(y, _)) =>
+        SCons(f(x, y), () => zipWithFun(f, xs.tail, ys.tail))
+    }
+  }
+
+  def nthElem(n: BigInt, s: SCons): BigInt = {
+    require(n >= 0)
+    if (n == 0) s.x
+    else {
+      nthElem(n - 1, s.tail)
+    }
+  } 
+
+  /**
+   * Using a `zipWithFun` function to implement a fibonacci stream.
+   */
+  val fibstream: SCons = SCons(0, () => SCons(1, () => {
+    zipWithFun(_ + _, this.fibstream, this.fibstream.tail)
+  }))
+}
