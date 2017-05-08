ed75a931d43eb15ebbf07fcb8bc18bb6f2f7fac1
Marco Antognini <antognini.marco@gmail.com>
2017-05-08 15:49:59 +0200

    Add tests for Byte & Casts

---
+++ frontends/scalac/src/it/resources/regression/verification/invalid/Bytes.scala
@@ -0,0 +1,19 @@
+/* Copyright 2009-2017 EPFL, Lausanne */
+
+import stainless.lang._
+
+object Bytes {
+
+  def test(b: Byte) = {
+    require(b % 2 != 0)
+    if (b > 0) 0 else 1
+  }
+
+  def fun(b: Byte) = test(b)
+
+  def gun(b: Byte, c: Byte) = {
+    b + c
+  } ensuring { res => -128 <= res && res <= 127 }
+
+}
+
