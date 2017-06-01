aa156b4666567319816c21bd6beab7f1493506df
Marco Antognini <antognini.marco@gmail.com>
2017-06-01 11:33:03 +0200

    Make regression tests independant of each others

--- frontends/scalac/src/it/scala/stainless/ComponentTestSuite.scala
+++ frontends/scalac/src/it/scala/stainless/ComponentTestSuite.scala
@@ -18,46 +18,50 @@ trait ComponentTestSuite extends inox.TestSuite with inox.ResourceUtils {
     "check=" + options.findOptionOrDefault(inox.solvers.optCheckModels)
   }
 
-  def extract(files: Seq[String]): (Seq[(String, Seq[Identifier])], Program { val trees: component.trees.type }) = {
+  private def extract(file: String): (String, Seq[Identifier], Program { val trees: component.trees.type }) = {
     val reporter = new inox.TestSilentReporter
 
     val ctx = inox.Context(reporter, new inox.utils.InterruptManager(reporter))
-    val (structure, program) = Main.extractFromSource(ctx, Main.libraryFiles ++ files.toList)
+    val (structure, program) = Main.extractFromSource(ctx, Main.libraryFiles :+ file)
     program.symbols.ensureWellFormed
     val exProgram = component.extract(program)
     exProgram.symbols.ensureWellFormed
 
     assert(reporter.lastErrors.isEmpty)
 
-    (for (u <- structure if u.isMain) yield {
-      val unitFuns = u.allFunctions(program.symbols)
-      val allFuns = inox.utils.fixpoint { (funs: Set[Identifier]) =>
-        funs ++ exProgram.symbols.functions.values.flatMap { fd =>
-          val source = fd.flags.collect { case component.trees.Derived(id) => id }.toSet
-          if ((source intersect funs).nonEmpty) {
-            Some(fd.id)
-          } else {
-            None
-          }
+    val uOpt = structure find { _.isMain }
+    val u = uOpt.get
+
+    val unitFuns = u.allFunctions(program.symbols)
+    val allFuns = inox.utils.fixpoint { (funs: Set[Identifier]) =>
+      funs ++ exProgram.symbols.functions.values.flatMap { fd =>
+        val source = fd.flags.collect { case component.trees.Derived(id) => id }.toSet
+        if ((source intersect funs).nonEmpty) {
+          Some(fd.id)
+        } else {
+          None
         }
-      } (unitFuns.toSet)
-      (u.id.name, allFuns.toSeq)
-    }, exProgram)
+      }
+    } (unitFuns.toSet)
+
+    (u.id.name, allFuns.toSeq, exProgram)
   }
 
-  def filter(ctx: inox.Context, name: String): FilterStatus = Test
+  protected def filter(ctx: inox.Context, name: String): FilterStatus = Test
 
   def testAll(dir: String)(block: (component.Report, inox.Reporter) => Unit): Unit = {
     val fs = resourceFiles(dir, _.endsWith(".scala")).toList
 
-    val (funss, program) = extract(fs.map(_.getPath))
-
-    for ((name, funs) <- funss) {
-      test(s"$dir/$name", ctx => filter(ctx, s"$dir/$name")) { ctx =>
-        val newProgram = program.withContext(ctx)
-        val report = component.apply(funs, newProgram)
-        block(report, ctx.reporter)
-      }
+    for {
+      file <- fs
+      path = file.getPath
+      name = file.getName dropRight (".scala".length)
+    } test(s"$dir/$name", ctx => filter(ctx, s"$dir/$name")) { ctx =>
+      val (uName, funs, program) = extract(path)
+      assert(uName == name)
+      val newProgram = program.withContext(ctx)
+      val report = component.apply(funs, newProgram)
+      block(report, ctx.reporter)
     }
   }
 }
