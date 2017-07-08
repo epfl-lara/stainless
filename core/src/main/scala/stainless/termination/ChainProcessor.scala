aa8856680e9872a033ef507629e54da6ba2875b3
Nicolas Voirol <voirol.nicolas@gmail.com>
2017-07-08 22:58:02 +0200

    Fixed unsoundness in chain processor

--- core/src/main/scala/stainless/termination/ChainProcessor.scala
+++ core/src/main/scala/stainless/termination/ChainProcessor.scala
@@ -37,45 +37,7 @@ trait ChainProcessor extends OrderingProcessor {
       funDef => funDef -> getChains(funDef)
     }.toMap
 
-    val chainConstraints: Map[Chain, SizeConstraint] = {
-      val relationConstraints: MutableMap[Relation, SizeConstraint] = MutableMap.empty
-
-      chainsMap.flatMap { case (_, (_, chains)) =>
-        chains.map(chain => chain -> {
-          val constraints = chain.relations.map(relation => relationConstraints.getOrElse(relation, {
-            val Relation(funDef, path, FunctionInvocation(_, _, args), _) = relation
-            val args0 = funDef.params.map(_.toVariable)
-            val constraint = if (api.solveVALID(path implies ordering.lessEquals(args, args0)).contains(true)) {
-              if (api.solveVALID(path implies ordering.lessThan(args, args0)).contains(true)) {
-                StrongDecreasing
-              } else {
-                WeakDecreasing
-              }
-            } else {
-              NoConstraint
-            }
-
-            relationConstraints(relation) = constraint
-            constraint
-          })).toSet
-
-          if (constraints(NoConstraint)) {
-            NoConstraint
-          } else if (constraints(StrongDecreasing)) {
-            StrongDecreasing
-          } else {
-            WeakDecreasing
-          }
-        })
-      }
-    }
-
-    val filteredChains: Map[FunDef, (Set[FunDef], Set[Chain])] = chainsMap.map { case (fd, (fds, chains)) =>
-      val remainingChains = chains.filter(chain => chainConstraints(chain) != StrongDecreasing)
-      fd -> ((fds, remainingChains))
-    }
-
-    val loopPoints = filteredChains.foldLeft(Set.empty[FunDef]) { case (set, (fd, (fds, chains))) => set ++ fds }
+    val loopPoints = chainsMap.foldLeft(Set.empty[FunDef]) { case (set, (fd, (fds, chains))) => set ++ fds }
 
     if (loopPoints.size > 1) {
       reporter.debug("-+> Multiple looping points, can't build chain proof")
@@ -85,12 +47,12 @@ trait ChainProcessor extends OrderingProcessor {
       val funDefs = if (loopPoints.nonEmpty) {
         loopPoints
       } else {
-        filteredChains.collect { case (fd, (fds, chains)) if chains.nonEmpty => fd }
+        chainsMap.collect { case (fd, (fds, chains)) if chains.nonEmpty => fd }
       }
 
       var cleared = false
       for (funDef <- funDefs if !cleared) {
-        val chains = filteredChains(funDef)._2
+        val chains = chainsMap(funDef)._2
         val allChains = chainsMap(funDef)._2
         reporter.debug("- Searching for size decrease")
 
