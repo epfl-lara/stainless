6dde322926d0533f574fbf8e518a6cd9007f925e
Nicolas Voirol <voirol.nicolas@gmail.com>
2017-07-19 23:16:00 +0200

    Enable lambda compilation with many closures

--- frontends/scalac/src/it/scala/stainless/verification/VerificationSuite.scala
+++ frontends/scalac/src/it/scala/stainless/verification/VerificationSuite.scala
@@ -107,6 +107,7 @@ class SMTCVC4VerificationSuite extends VerificationSuite {
     // These tests are too slow on CVC4 and make the regression unstable
     case "verification/valid/ConcRope" => Ignore
     case "verification/invalid/BadConcRope" => Ignore
+    case "verification/invalid/ForallAssoc" => Ignore
     case _ => super.filter(ctx, name)
   }
 }
