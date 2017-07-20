b7309271453bf2551cf1a5e53349f531c1c7360e
Nicolas Voirol <voirol.nicolas@gmail.com>
2017-07-20 21:21:58 +0200

    Some extra benchmarks

---
+++ frontends/benchmarks/verification/invalid/LambdaPrecondition.scala
@@ -0,0 +1,18 @@
+import stainless.lang._
+
+object LambdaPreconditions {
+  def a() = {
+    require(false)
+    false
+  } holds
+
+  def f(b: Boolean): () => Boolean = {
+    if (b) a
+    else () => true
+  }
+
+  def g() = {
+    assert(f(true)())
+    assert(false)
+  }
+}
