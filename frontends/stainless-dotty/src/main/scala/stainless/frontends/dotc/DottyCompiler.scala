71147d43d55a9b1503ca2522c505a6ec5c1a23c6
Marco Antognini <antognini.marco@gmail.com>
2017-07-26 10:11:35 +0200

    Refactoring of frontends

--- frontends/stainless-dotty/src/main/scala/stainless/frontends/dotc/DottyCompiler.scala
+++ frontends/stainless-dotty/src/main/scala/stainless/frontends/dotc/DottyCompiler.scala
@@ -9,7 +9,7 @@ import dotty.tools.dotc.transform._
 import dotty.tools.dotc.core.Phases._
 import dotty.tools.dotc.core.Contexts._
 
-import frontend.{ Frontend, FrontendFactory, CallBack }
+import frontend.{ Frontend, ThreadedFrontend, FrontendFactory, CallBack }
 
 class DottyCompiler(ctx: inox.Context, callback: CallBack) extends Compiler {
   override def phases: List[List[Phase]] = List(
@@ -39,69 +39,23 @@ object DottyCompiler {
     override val libraryFiles: Seq[String]
   ) extends FrontendFactory {
 
-    override def apply(ctx: inox.Context, compilerArgs: Seq[String], callback: CallBack): Frontend = {
+    override def apply(ctx: inox.Context, compilerArgs: Seq[String], callback: CallBack): Frontend =
+      new ThreadedFrontend(callback, ctx) {
+        val compiler = new DottyCompiler(ctx, callback)
+        val args = allCompilerArguments(compilerArgs)
 
-      val compiler = new DottyCompiler(ctx, callback)
-      val args = allCompilerArguments(compilerArgs)
-
-      val driver = new DottyDriver(args, compiler)
-
-      // FIXME some of this code is duplicated from ScalaCompiler!
-      val frontend = new Frontend(callback) {
-        var thread: Thread = null
-        var exception: Throwable = null
+        val driver = new DottyDriver(args, compiler)
 
         override val sources = driver.files
 
-        override def run(): Unit = {
-          assert(!isRunning)
-
-          // Run the compiler in the background in order to make the factory
-          // non-blocking, and implement a clean stop action.
-          val runnable = new Runnable {
-            override def run() = try {
-              callback.beginExtractions()
-              driver.run()
-            } finally {
-              callback.endExtractions()
-            }
-          }
-
-          thread = new Thread(runnable, "stainless compiler")
-          thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-            override def uncaughtException(t: Thread, e: Throwable): Unit = exception = e
-          })
-
-          thread.start()
-        }
-
-        override def isRunning: Boolean = thread != null && thread.isAlive
-
-        override def onStop(): Unit = {
-          if (isRunning) {
-            // TODO implement a graceful stop! Current implementation might not work anyway...
-            thread.interrupt()
-          }
-
-          rethrow()
-        }
-
-        override def onJoin(): Unit = {
-          if (isRunning) thread.join()
-
-          rethrow()
-        }
-
-        private def rethrow(): Unit = if (exception != null) {
-          val e = exception
-          exception = null
-          ctx.reporter.error(s"Rethrowing exception emitted from within the compiler: ${e.getMessage}")
-          throw e
+        override def initRun(): Unit = { /* nothing */ }
+        override def onRun(): Unit = { driver.run() }
+        override def onEnd(): Unit = { /* nothing */ }
+        override def onStop(thread: Thread): Unit = {
+          // TODO implement a graceful stop! Current implementation might not work anyway...
+          thread.interrupt()
         }
       }
-
-      frontend
-    }
 
   }
 
