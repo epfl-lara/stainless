35d994aef0705902402586a21743a4460d41e974
Marco Antognini <antognini.marco@gmail.com>
2017-07-26 11:59:03 +0200

    Implement termination callback

--- core/src/main/scala/stainless/termination/TerminationCallBack.scala
+++ core/src/main/scala/stainless/termination/TerminationCallBack.scala
@@ -5,16 +5,32 @@ package termination
 
 import extraction.xlang.{ trees => xt }
 
-// TODO Implement TerminationCallBack
 /** Callback for termination */
-class TerminationCallBack(val ctx: inox.Context) extends frontend.CallBack {
-  override def beginExtractions(): Unit = ???
-  override def apply(file: String, unit: xt.UnitDef, classes: Seq[xt.ClassDef], functions: Seq[xt.FunDef]): Unit = ???
-  override def endExtractions(): Unit = ???
+class TerminationCallBack(val ctx: inox.Context) extends frontend.CallBackWithRegistry {
+  private implicit val debugSection = DebugSectionTermination
 
-  override def stop(): Unit = ()
-  override def join(): Unit = ()
+  override type Report = TerminationComponent.Report
 
-  override def getReports: Seq[AbstractReport] = Nil
+  override def solve(program: Program { val trees: extraction.xlang.trees.type }): Report = {
+    ctx.reporter.debug(
+      s"Checking termination fo the following program: " +
+      "\n\tfunctions = [" + (program.symbols.functions.keySet mkString ", ") + "]" +
+      "\n\tclasses   = [" + (program.symbols.classes.keySet mkString ", ") + "]"
+    )
+
+    TerminationComponent(program)
+  }
+
+  /** Checks whether the given function/class should be verified at some point. */
+  // TODO this check should be moved to a utility package and copy/past code removed from stainless.
+  override def shouldBeChecked(fd: xt.FunDef): Boolean = {
+    val isLibrary = fd.flags contains "library"
+    val isUnchecked = fd.flags contains "unchecked"
+    !(isLibrary || isUnchecked)
+    // TODO check --functions=... options for proper filter
+  }
+
+  // Invariants are already extracted to functions, so no need to process the class unless it's a dependency.
+  override def shouldBeChecked(cd: xt.ClassDef): Boolean = false
 }
 
