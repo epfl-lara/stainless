43b8e81b004ea5c489b20c535cdf53a20ebe7f87
Marco Antognini <antognini.marco@gmail.com>
2017-08-04 16:18:48 +0200

    Improve support for --functions

--- core/src/main/scala/stainless/utils/CheckFilter.scala
+++ core/src/main/scala/stainless/utils/CheckFilter.scala
@@ -3,23 +3,45 @@
 package stainless
 package utils
 
-import extraction.xlang.{ trees => xt }
-
 /** Filter functions / classes for verification purposes */
 trait CheckFilter {
 
   val ctx: inox.Context
 
-  /** Checks whether the given function/class should be verified at some point. */
-  def shouldBeChecked(fd: xt.FunDef): Boolean = {
-    val isLibrary = fd.flags contains "library"
-    val isUnchecked = fd.flags contains "unchecked"
-    !(isLibrary || isUnchecked)
-    // TODO check --functions=... options for proper filter
+  private lazy val functions: Option[Seq[String]] = ctx.options.findOption(optFunctions)
+
+  // TODO this is probably done somewhere else in a cleaner fasion...
+  private def fixedFullName(id: Identifier): String = id.fullName
+    .replaceAllLiterally("$bar", "|")
+    .replaceAllLiterally("$up", "^")
+    .replaceAllLiterally("$eq", "=")
+    .replaceAllLiterally("$plus", "+")
+    .replaceAllLiterally("$minus", "-")
+    .replaceAllLiterally("$times", "*")
+    .replaceAllLiterally("$div", "/")
+    .replaceAllLiterally("$less", "<")
+    .replaceAllLiterally("$geater", ">")
+    .replaceAllLiterally("$colon", ":")
+    .replaceAllLiterally("$amp", "&")
+    .replaceAllLiterally("$tilde", "~")
+
+  /** Same as below. Note the _ <: ... tricks because Set are invariant. */
+  def shouldBeChecked(fid: Identifier, flags: Set[_ <: inox.ast.Trees#Flag]): Boolean = functions match {
+    case None =>
+      val isLibrary = flags map { _.name } contains "library"
+      val isUnchecked = flags map { _.name } contains "unchecked"
+      !(isLibrary || isUnchecked)
+
+    case Some(funs) =>
+      val fullName = fixedFullName(fid)
+      funs exists { f => fullName endsWith f }
   }
 
+  /** Checks whether the given function/class should be verified at some point. */
+  def shouldBeChecked(fd: extraction.xlang.trees.FunDef): Boolean = shouldBeChecked(fd.id, fd.flags)
+
   // Invariants are already extracted to functions, so no need to process the class unless it's a dependency.
-  def shouldBeChecked(cd: xt.ClassDef): Boolean = false
+  def shouldBeChecked(cd: extraction.xlang.trees.ClassDef): Boolean = false
 
 }
 
