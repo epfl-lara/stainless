c8195222d9368093d320723b8b22eb589edd54fa
jad <jad.hamza@epfl.ch>
2017-08-22 16:24:49 +0200

    removed commented code

--- core/src/main/scala/stainless/MainHelpers.scala
+++ core/src/main/scala/stainless/MainHelpers.scala
@@ -120,10 +120,6 @@ trait MainHelpers extends inox.MainHelpers {
 
     // Shutdown the pool for a clean exit.
     MainHelpers.executor.shutdown()
-    // val unexecuted = MainHelpers.executor.shutdownNow()
-    // if (!ctx.interruptManager.isInterrupted && unexecuted.size != 0) {
-    //   ctx.reporter.error("Some tasks were not run (" + unexecuted.size + ")")
-    // }
   } catch {
     case _: inox.FatalError => System.exit(1)
   }
