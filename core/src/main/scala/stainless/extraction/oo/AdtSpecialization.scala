05a3a779d3b67126419e9e21b8a748502c7c1adb
jad <jad.hamza@epfl.ch>
2017-08-23 12:02:51 +0200

    removed more non-determinism

--- core/src/main/scala/stainless/extraction/oo/AdtSpecialization.scala
+++ core/src/main/scala/stainless/extraction/oo/AdtSpecialization.scala
@@ -79,7 +79,7 @@ trait AdtSpecialization extends inox.ast.SymbolTransformer { self =>
         case s.IsInstanceOf(e, s.ClassType(id, tps)) if candidates(id) =>
           val te = transform(e)
           val ttps = tps map transform
-          t.orJoin(classToConstructors(id).toSeq.map {
+          t.orJoin(classToConstructors(id).toSeq.sortBy(_.name).map {
             id => t.IsInstanceOf(te, t.ADTType(id, ttps)).setPos(e)
           })
 
@@ -135,7 +135,7 @@ trait AdtSpecialization extends inox.ast.SymbolTransformer { self =>
         case (ovd, vd) => ovd.tpe match {
           case s.ClassType(id, tps) if candidates(id) && approximate(id) != id =>
             val ttps = tps map transformer.transform
-            Some(t.orJoin(classToConstructors(id).toSeq.map {
+            Some(t.orJoin(classToConstructors(id).toSeq.sortBy(_.name).map {
               id => t.IsInstanceOf(vd.toVariable, t.ADTType(id, ttps))
             }))
           case _ => None
