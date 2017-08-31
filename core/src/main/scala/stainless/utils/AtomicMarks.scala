14bda06c0a12d3e3830a8a5d287b2dd6f2637a6a
Marco Antognini <antognini.marco@gmail.com>
2017-08-31 16:10:10 +0200

    Add filter on functions/VCs
    This filter avoids computing VCs for the same function twice during the
    same extraction cycle

---
+++ core/src/main/scala/stainless/utils/AtomicMarks.scala
@@ -0,0 +1,28 @@
+/* Copyright 2009-2016 EPFL, Lausanne */
+
+package stainless
+package utils
+
+/** Set of marks that are atomically set. */
+class AtomicMarks[A] {
+
+  /**
+   * Atomically set the mark for [[a]].
+   *
+   * Returns [[true]] if the mark was **not** set before,
+   * [[false]] on subsequent call for the same [[a]] until
+   * a call to [[clear]].
+   */
+  def compareAndSet(a: A): Boolean = underlying.putIfAbsent(a, unusedValue).isEmpty
+
+  /**
+   * Clear all marks.
+   */
+  def clear(): Unit = underlying.clear()
+
+  private type UnusedType = Unit
+  private val unusedValue = ()
+  private val underlying = scala.collection.concurrent.TrieMap[A, UnusedType]()
+
+}
+
