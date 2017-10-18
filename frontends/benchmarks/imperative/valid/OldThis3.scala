df506dffce6831429ff4f7a323eb79aae1e5c41e
Marco Antognini <antognini.marco@gmail.com>
2017-10-18 17:36:42 +0200

    Add regression tests

---
+++ frontends/benchmarks/imperative/valid/OldThis3.scala
@@ -0,0 +1,11 @@
+import stainless.lang._
+
+object OldThis3 {
+
+  case class A(var a: Int) {
+    def foo(b: Int): Unit = {
+      a + b // no assignement
+    } ensuring(_ => a == old(this).a)
+  }
+
+}
---
+++ frontends/benchmarks/imperative/valid/OldThis4.scala
@@ -0,0 +1,11 @@
+import stainless.lang._
+
+object OldThis4 {
+
+  case class A(var a: Int) {
+    def foo(b: Int): Unit = {
+      b + b // a never used
+    } ensuring(_ => a == old(this).a)
+  }
+
+}
