38c800b8b02ef65f150e2c4211f340af5d699afc
Nicolas Voirol <voirol.nicolas@gmail.com>
2017-12-06 23:25:57 +0100

    Add benchmark for negative datatypes

---
+++ frontends/benchmarks/termination/looping/NegativeDatatype.scala
@@ -0,0 +1,10 @@
+import stainless.lang._
+
+object Inconsistency {
+  sealed abstract class Code
+  case class Fold(f: Code => Boolean) extends Code
+
+  def looping(c: Code): Boolean = c match {
+    case Fold(f) => !f(c)
+  }
+}
