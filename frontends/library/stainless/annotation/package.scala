168686c0b9dcc1decd3db817f8b2aac7df2b2721
Marco Antognini <antognini.marco@gmail.com>
2017-12-12 14:08:38 +0100

    Remove @traceInduct

--- frontends/library/stainless/annotation/package.scala
+++ frontends/library/stainless/annotation/package.scala
@@ -19,13 +19,6 @@ package object annotation {
   @ignore
   class induct     extends Annotation
 
-  /** Introduce an inductive hypothesis on `name` when verifying the annotated function. */
-  // FIXME is this supported? (if not, remove TraceInductTacticTest.scala.BAK)
-  // FIXME what does the default value "" mean/how is it handled?
-  // FIXME using default value results in: [ Error  ] monotonicity$0 depends on missing dependencies: <init>$default$1$0.
-  @ignore
-  class traceInduct(name: String = "") extends Annotation
-
   /** Only extract the contracts and replace the annotated function's body with a choose. */
   @ignore
   class extern     extends Annotation
