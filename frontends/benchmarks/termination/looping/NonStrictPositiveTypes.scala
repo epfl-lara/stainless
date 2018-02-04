33eeabcb2a3ca6f2eb8bfb953b94d9cb9a24d740
Nicolas Voirol <voirol.nicolas@gmail.com>
2018-02-04 15:19:36 +0100

    Add some benchmarks for non-strict positive datatypes

---
+++ frontends/benchmarks/termination/looping/NonStrictPositiveTypes.scala
@@ -0,0 +1,9 @@
+object NonStrictPositiveTypes extends App {
+  case class A(m: (A => Boolean) => Boolean)
+
+  def looping(): A => Boolean = y => y.m(looping())
+  def x(): A = A(p => p(x()))
+
+  // looping()(x()) reduces to x().m(looping()) and
+  // x().m(looping()) reduces to looping()(x()) (does not terminate)
+}
---
+++ frontends/benchmarks/termination/looping/NonStrictPositiveTypesIR.scala
@@ -0,0 +1,16 @@
+object NonStrictPositiveTypesIR extends App {
+  case class A(m: (A => Boolean) => Boolean)
+
+  // f is not recursive, and does not directly refer to named functions
+  def f(y: A, pp: () => A => Boolean) = y.m(pp())
+
+  // looping only invokes f, and itself under a lambda
+  // saying that looping is "terminating" is probably the issue?
+  def looping(): A => Boolean = y => f(y, () => looping())
+
+  // x1 refers to app, which is non-recursive, and to x1() itself under a lambda
+  def x1(): A = A(p => app(p,x1()))
+
+  // app is non-recursive, and does not directly refer to named functions
+  def app(p: A => Boolean, x: A) = p(x)
+}
