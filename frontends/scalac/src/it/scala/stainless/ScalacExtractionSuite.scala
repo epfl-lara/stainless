2e858abcee9bbeab21581411591d15ea254bc045
Iulian Dragos <jaguarul@gmail.com>
2018-02-28 17:19:48 +0100

    Run extraction tests on all examples.

--- frontends/scalac/src/it/scala/stainless/ScalacExtractionSuite.scala
+++ frontends/scalac/src/it/scala/stainless/ScalacExtractionSuite.scala
@@ -7,5 +7,13 @@ class ScalacExtractionSuite extends ExtractionSuite {
   testExtractAll("extraction/valid")
   testRejectAll("extraction/invalid")
 
+  testExtractAll("verification/valid")
+  testExtractAll("verification/invalid")
+  testExtractAll("verification/unchecked")
+  testExtractAll("imperative/valid")
+  testExtractAll("imperative/invalid")
+  testExtractAll("termination/valid")
+  testExtractAll("termination/looping")
+
 }
 
