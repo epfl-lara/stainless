9b044e4a6b2db06caf525521a2c21df9b92a5b7b
Romain Ruetschi <romain.ruetschi@gmail.com>
2018-03-27 15:05:44 +0200

    Implement map and flatMap on stainless.lang.Either

--- frontends/library/stainless/lang/Either.scala
+++ frontends/library/stainless/lang/Either.scala
@@ -8,20 +8,39 @@ import stainless.annotation._
  * @author Viktor
  */
 @library
-sealed abstract class Either[A,B] {
-  def isLeft : Boolean
-  def isRight : Boolean
-  def swap : Either[B,A]
+sealed abstract class Either[A, B] {
+  def isLeft: Boolean
+  def isRight: Boolean
+  def swap: Either[B,A]
+
+  def map[C](f: B => C): Either[A, C] = this match {
+    case Left(a)  => Left(a)
+    case Right(b) => Right(f(b))
+  }
+
+  def flatMap[C](f: B => Either[A, C]): Either[A, C] = this match {
+    case Left(a)  => Left(a)
+    case Right(b) => f(b)
+  }
+
+  def get: B = {
+    require(isRight)
+    val Right(value) = this
+    value
+  }
 }
+
 @library
 case class Left[A,B](content: A) extends Either[A,B] {
-  def isLeft = true
+  def isLeft  = true
   def isRight = false
-  def swap = Right[B,A](content)
+  def swap    = Right[B, A](content)
 }
+
 @library
 case class Right[A,B](content: B) extends Either[A,B] {
-  def isLeft = false
+  def isLeft  = false
   def isRight = true
-  def swap = Left[B,A](content)
+  def swap    = Left[B, A](content)
 }
+
