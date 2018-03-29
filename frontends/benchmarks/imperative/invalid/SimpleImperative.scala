0844feb1bfec22fa4ffdca4ef0e650593144a37b
Nicolas Voirol <voirol.nicolas@gmail.com>
2018-03-29 14:22:00 +0200

    Added invalid imperative benchmark

---
+++ frontends/benchmarks/imperative/invalid/SimpleImperative.scala
@@ -0,0 +1,23 @@
+import stainless.lang._
+
+object SimpleImperative {
+  abstract class A {
+    def f(): Unit 
+  }
+
+  def proc(a: A) = {
+    a.f()
+  }
+
+  case class B(var x: BigInt) extends A {
+    def f(): Unit = {
+      x = x + 1
+    }
+  }
+
+  def theorem() = {
+    val a = B(0)
+    proc(a)
+    assert(a.x == 0) // should be 1
+  }
+}
