066463d49d321b20856a1d7e066c17ab9a0af928
Nicolas Voirol <voirol.nicolas@gmail.com>
2018-04-06 10:30:59 +0200

    Avoid scope clashes in extraction benchmarks

--- frontends/benchmarks/extraction/valid/CaseObject.scala
+++ frontends/benchmarks/extraction/valid/CaseObject.scala
@@ -1,11 +1,13 @@
-case class S()
+object CaseObject {
+  case class S()
 
-abstract class A
-case object Main extends A {
-  val s: S = S()
+  abstract class A
+  case object Main extends A {
+    val s: S = S()
 
-  def f(x: S): S = x match {
-    case S() => S()
-    case _ => s
-  }    
+    def f(x: S): S = x match {
+      case S() => S()
+      case _ => s
+    }    
+  }
 }
