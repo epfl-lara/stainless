dcb2a1296e8617296fd161e2ffef5b4e900c878d
Nicolas Voirol <voirol.nicolas@gmail.com>
2018-05-02 15:50:32 +0200

    Use fast simplifier

--- core/src/main/scala/stainless/ast/SymbolOps.scala
+++ core/src/main/scala/stainless/ast/SymbolOps.scala
@@ -10,7 +10,9 @@ trait SymbolOps extends inox.ast.SymbolOps { self: TypeOps =>
 
   override protected def createSimplifier(popts: inox.solvers.PurityOptions): SimplifierWithPC = new {
     val opts: inox.solvers.PurityOptions = popts
-  } with transformers.SimplifierWithPC with SimplifierWithPC
+  } with transformers.SimplifierWithPC with SimplifierWithPC with inox.transformers.FastSimplifier {
+    override def pp = implicitly[PathProvider[Env]]
+  }
 
   override protected def createTransformer[P <: PathLike[P]](path: P, f: (Expr, P, TransformerOp[P]) => Expr)
                                                             (implicit ppP: PathProvider[P]): TransformerWithPC[P] =
