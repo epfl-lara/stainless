1196c46639cd34457192997ccfff1f5f12ee2011
Romain Ruetschi <romain.ruetschi@gmail.com>
2018-06-21 15:43:14 +0200

    Disable partial evaluator for now

--- core/src/main/scala/stainless/verification/VerificationComponent.scala
+++ core/src/main/scala/stainless/verification/VerificationComponent.scala
@@ -41,7 +41,8 @@ class VerificationRun(override val pipeline: StainlessPipeline)
 
   override def parse(json: Json): Report = VerificationReport.parse(json)
 
-  override protected def createPipeline = pipeline andThen PartialEvaluation(extraction.trees) andThen lowering
+  // override protected def createPipeline = pipeline andThen PartialEvaluation(extraction.trees) andThen lowering
+  override protected def createPipeline = pipeline andThen lowering
 
   implicit val debugSection = DebugSectionVerification
 
