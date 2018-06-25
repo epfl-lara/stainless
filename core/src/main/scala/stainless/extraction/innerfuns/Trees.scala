187df061f0ff4d29087512fd8129cb4ea23c293b
Romain Ruetschi <romain.ruetschi@gmail.com>
2018-06-25 09:35:49 +0200

    Add toString method to innerfuns.FunAbstraction

--- core/src/main/scala/stainless/extraction/innerfuns/Trees.scala
+++ core/src/main/scala/stainless/extraction/innerfuns/Trees.scala
@@ -67,6 +67,11 @@ trait Trees extends inlining.Trees { self =>
 
     def toFun: FunDef = asInstanceOf[Outer].fd
     def toLocal: LocalFunDef = asInstanceOf[Inner].fd
+
+    override def toString: String = {
+      if (isInstanceOf[Outer]) asInstanceOf[Outer].toString
+      else asInstanceOf[Inner].toString
+    }
   }
 
   case class Outer(fd: FunDef) extends FunAbstraction(
