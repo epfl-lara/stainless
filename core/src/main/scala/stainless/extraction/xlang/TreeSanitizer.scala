a0de4c06d61ff749fc4c78156cfc5232422630a7
Nicolas Voirol <voirol.nicolas@gmail.com>
2018-08-23 13:59:09 +0200

    Remove strange sanitization visitor

--- core/src/main/scala/stainless/extraction/xlang/TreeSanitizer.scala
+++ core/src/main/scala/stainless/extraction/xlang/TreeSanitizer.scala
@@ -24,23 +24,7 @@ trait TreeSanitizer {
     symbols.functions.values foreach preconditions.traverse
 
     val ignored = new CheckIgnoredFields()(symbols)
-    symbols.functions.values foreach ignored
-  }
-
-  private sealed abstract class Action
-  private case object Continue extends Action
-  private case class ContinueWith(e: Expr) extends Action
-  private case object Stop extends Action
-
-  private def traverse(e: Expr)(visitor: Expr => Action): Unit = {
-    def rec(f: Expr) = traverse(f)(visitor)
-    visitor(e) match{
-      case Stop => /* nothing */
-      case ContinueWith(e) => rec(e)
-      case Continue =>
-        val Operator(es, _) = e
-        es foreach rec
-    }
+    symbols.functions.values foreach ignored.traverse
   }
 
   /* This detects both multiple `require` and `require` after `decreases`. */
@@ -96,41 +80,38 @@ trait TreeSanitizer {
   }
 
   /* This detects accesses to @ignored fields */
-  private class CheckIgnoredFields(implicit symbols: Symbols) extends Visitor {
-    override def visit(id: Identifier, body: Expr): Unit = {
-      traverse(body) {
-        case e @ ClassSelector(obj, selector) =>
-          val ct = obj.getType.asInstanceOf[ClassType]
-          ct.getField(selector) match {
-            case None =>
-              throw MissformedStainlessCode(e, s"Cannot find field `$selector` of class $ct.")
-            case Some(field) if field.flags contains Ignore =>
-              throw MissformedStainlessCode(e, s"Cannot access ignored field `$selector` from non-extern context.")
-            case _ =>
-              Continue
-          }
+  private class CheckIgnoredFields(implicit symbols: Symbols) extends TreeTraverser {
+    override def traverse(e: Expr): Unit = e match {
+      case e @ ClassSelector(obj, selector) =>
+        val ct = obj.getType.asInstanceOf[ClassType]
+        ct.getField(selector) match {
+          case None =>
+            throw MissformedStainlessCode(e, s"Cannot find field `$selector` of class $ct.")
+          case Some(field) if field.flags contains Ignore =>
+            throw MissformedStainlessCode(e, s"Cannot access ignored field `$selector` from non-extern context.")
+          case _ =>
+            super.traverse(e)
+        }
 
-        case e @ ClassConstructor(ct, args) =>
-          ct.lookupClass match {
-            case None =>
-              throw MissformedStainlessCode(e, s"Cannot find class for type `$ct`.")
+      case e @ ClassConstructor(ct, args) =>
+        ct.lookupClass match {
+          case None =>
+            throw MissformedStainlessCode(e, s"Cannot find class for type `$ct`.")
 
-            case Some(tcd) if tcd.fields.exists(_.flags contains Ignore) =>
-              val ignoredFields = tcd.fields.filter(_.flags contains Ignore).map(_.id).mkString(", ")
-              throw MissformedStainlessCode(
-                e,
-                s"Cannot build an instance of a class with ignored fields in non-extern context " +
-                s"($ct has ignored fields: $ignoredFields)."
-              )
+          case Some(tcd) if tcd.fields.exists(_.flags contains Ignore) =>
+            val ignoredFields = tcd.fields.filter(_.flags contains Ignore).map(_.id).mkString(", ")
+            throw MissformedStainlessCode(
+              e,
+              s"Cannot build an instance of a class with ignored fields in non-extern context " +
+              s"($ct has ignored fields: $ignoredFields)."
+            )
 
-            case _ => Continue
-          }
+          case _ => super.traverse(e)
+        }
 
-        case _ => Continue
-      }
+      case _ => super.traverse(e)
     }
   }
-
 }
 
 object TreeSanitizer {
