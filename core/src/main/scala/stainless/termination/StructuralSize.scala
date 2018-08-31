fe34c841dd6448bd04394ed2bb85069b3be8f54c
Jad <jad.hamza@epfl.ch>
2018-08-31 10:42:29 +0200

    Adapted code to new BVLiteral API

--- core/src/main/scala/stainless/termination/StructuralSize.scala
+++ core/src/main/scala/stainless/termination/StructuralSize.scala
@@ -76,8 +76,8 @@ trait StructuralSize { self: SolverProvider =>
    */
   def bvAbs2Integer(tpe: BVType): FunDef = bv2IntegerCache.getOrElseUpdate(tpe, {
     val funID = FreshIdentifier("bvAbs2Integer$" + tpe.size)
-    val zero = BVLiteral(0, tpe.size)
-    val one = BVLiteral(1, tpe.size)
+    val zero = BVLiteral(true, 0, tpe.size)
+    val one = BVLiteral(true, 1, tpe.size)
     val fd = mkFunDef(funID)()(_ => (
       Seq("x" :: tpe), IntegerType(), { case Seq(x) =>
         Ensuring(if_ (x === zero) {
