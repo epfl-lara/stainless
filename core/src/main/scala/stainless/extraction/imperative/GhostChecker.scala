676252c2cf9f6f4fc3c799c688b4a44a9c271a22
Nicolas Voirol <voirol.nicolas@gmail.com>
2018-09-23 18:38:56 +0200

    Add ghost flow checks

--- core/src/main/scala/stainless/extraction/imperative/GhostChecker.scala
+++ core/src/main/scala/stainless/extraction/imperative/GhostChecker.scala
@@ -31,62 +31,119 @@ trait GhostChecker { self: EffectsAnalyzer =>
       (effect.receiver.flags contains Ghost) || rec(effect.receiver.getType, effect.target.path)
     }
 
-    object traverser extends TreeTraverser {
-      override def traverse(e: Expr): Unit = e match {
+    def checkEffects(fd: FunDef): Unit = new TreeTraverser {
+      override def traverse(expr: Expr): Unit = expr match {
         case l @ Let(vd, e, b) if (vd.flags contains Ghost) && !effects(e).forall(isGhostEffect) =>
-          throw ImperativeEliminationException(e,
+          throw ImperativeEliminationException(expr,
             "Right-hand side of ghost variable must only have effects on ghost fields")
 
         case l @ LetVar(vd, e, b) if (vd.flags contains Ghost) && !effects(e).forall(isGhostEffect) =>
-          throw ImperativeEliminationException(e,
+          throw ImperativeEliminationException(expr,
             "Right-hand side of ghost variable must only have effects on ghost fields")
 
         case Assignment(v, e) if (v.flags contains Ghost) && !effects(e).forall(isGhostEffect) =>
-          throw ImperativeEliminationException(e,
+          throw ImperativeEliminationException(expr,
             "Right-hand side of ghost variable assignment must only have effects on ghost fields")
 
+        case FieldAssignment(obj, id, e) if (
+          isGhostExpression(ADTSelector(obj, id)) &&
+          !effects(e).forall(isGhostEffect)
+        ) =>
+          throw ImperativeEliminationException(expr,
+            "Right-hand side of ghost field assignment must only have effects on ghost fields")
+
         case l @ Lambda(params, body) if params.exists(_.flags contains Ghost) =>
-          throw ImperativeEliminationException(e, "Lambdas cannot have ghost parameters")
+          throw ImperativeEliminationException(expr, "Lambdas cannot have ghost parameters")
 
         case LetRec(fds, body) =>
           fds.foreach(fd => checkGhostFunction(Inner(fd)))
-          super.traverse(e)
+          super.traverse(expr)
 
-        case fi @ FunctionInvocation(id, tps, args) if fi.tfd.params.exists(_.flags contains Ghost) =>
-          (fi.tfd.params zip args)
+        case FunInvocation(id, _, args, _) =>
+          val fun = lookupFunction(id).map(Outer(_)).getOrElse(effects.local(id))
+          (fun.params zip args)
             .filter { case (vd, arg) => (vd.flags contains Ghost) && !effects(arg).forall(isGhostEffect) }
             .foreach { case (vd, arg) =>
               throw ImperativeEliminationException(arg,
-                s"Argument to ghost parameter `${vd.id}` of `${fi.id}` must only have effects on ghost fields")
+                s"Argument to ghost parameter `${vd.id}` of `${id}` must only have effects on ghost fields")
             }
 
-          super.traverse(fi)
-
-        case ApplyLetRec(v, _, _, args) if effects.local(v.id).params.exists(_.flags contains Ghost) =>
-          (effects.local(v.id).params zip args)
-            .filter { case (vd, arg) => (vd.flags contains Ghost) && !effects(arg).forall(isGhostEffect) }
-            .foreach { case (vd, arg) =>
-              throw ImperativeEliminationException(arg,
-                s"Argument to ghost parameter `${vd.id}` of `${v.id}` must only have effects on ghost fields")
-            }
-
-          super.traverse(e)
+          super.traverse(expr)
 
         case adt @ ADT(id, tps, args) =>
-          adt.getConstructor.fields.zip(args)
+          (adt.getConstructor.fields zip args)
             .filter { case (vd, arg) => (vd.flags contains Ghost) && !effects(arg).forall(isGhostEffect) }
             .foreach { case (vd, arg) =>
               throw ImperativeEliminationException(arg,
                 s"Argument to ghost field `${vd.id}` of class `${id}` must only have effects on ghost fields")
             }
 
-          super.traverse(adt)
+          super.traverse(expr)
 
-        case _ => super.traverse(e)
+        case _ => super.traverse(expr)
       }
+    }.traverse(fd)
+
+
+    def checkNonGhostFunction(fun: FunAbstraction): Unit = {
+      if (!(fun.flags contains Ghost) && isGhostExpression(fun.fullBody))
+        throw ImperativeEliminationException(fun, s"Non-ghost function cannot return a ghost result")
     }
 
+    def isGhostExpression(e: Expr): Boolean = e match {
+      case v: Variable => v.flags contains Ghost
+
+      case FunInvocation(id, _, args, _) =>
+        val fun = lookupFunction(id).map(Outer(_)).getOrElse(effects.local(id))
+        (fun.flags contains Ghost) ||
+        (fun.params zip args).exists { case (vd, arg) =>
+          !(vd.flags contains Ghost) && isGhostExpression(arg)
+        }
+
+      case ADT(id, _, args) =>
+        (getConstructor(id).fields zip args).exists { case (vd, arg) =>
+          !(vd.flags contains Ghost) && isGhostExpression(arg)
+        }
+
+      case sel @ ADTSelector(e, id) =>
+        isGhostExpression(e) ||
+        (sel.constructor.fields.find(_.id == id).get.flags contains Ghost)
+
+      case Operator(es, _) => es.exists(isGhostExpression)
+    }
+
+    def checkExpressions(fd: FunDef): Unit = new TreeTraverser {
+      override def traverse(expr: Expr): Unit = expr match {
+        case Let(vd, e, _) if !(vd.flags contains Ghost) && isGhostExpression(e) =>
+          throw ImperativeEliminationException(expr,
+            "Right-hand side of non-ghost variable cannot be ghost")
+
+        case LetVar(vd, e, _) if !(vd.flags contains Ghost) && isGhostExpression(e) =>
+          throw ImperativeEliminationException(expr,
+            "Right-hand side of non-ghost variable cannot be ghost")
+
+        case Assignment(v, e) if !(v.flags contains Ghost) && isGhostExpression(e) =>
+          throw ImperativeEliminationException(expr,
+            "Right-hand side of non-ghost variable assignment cannot be ghost")
+
+        case FieldAssignment(obj, id, e) if (
+          !isGhostExpression(ADTSelector(obj, id)) &&
+          isGhostExpression(e)
+        ) =>
+          throw ImperativeEliminationException(expr,
+            "Right-hand side of non-ghost field assignment cannot be ghost")
+
+        case LetRec(fds, _) =>
+          fds.foreach(fd => checkNonGhostFunction(Inner(fd)))
+          super.traverse(expr)
+
+        case _ => super.traverse(expr)
+      }
+    }.traverse(fd)
+
     checkGhostFunction(Outer(fd))
-    traverser.traverse(fd)
+    checkNonGhostFunction(Outer(fd))
+    checkEffects(fd)
+    checkExpressions(fd)
   }
 }
