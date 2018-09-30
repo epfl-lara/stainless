ffa180333c18e4b93f990f91eb99483aca78b551
Nicolas Voirol <voirol.nicolas@gmail.com>
2018-09-30 20:01:35 +0200

    Cleanup in super calls transformer

--- core/src/main/scala/stainless/extraction/methods/SuperCalls.scala
+++ core/src/main/scala/stainless/extraction/methods/SuperCalls.scala
@@ -58,100 +58,148 @@ import stainless.ast.SymbolIdentifier.IdentifierOps
  * }
  * }}}
  */
-trait SuperCalls extends oo.CachingPhase
-  with DependentlyCachedFunctions
-  with IdentitySorts
-  with oo.IdentityClasses { self =>
+trait SuperCalls
+  extends oo.CachingPhase
+     with SimpleSorts
+     with oo.SimpleClasses { self =>
 
-  val s: methods.Trees
-  val t: methods.Trees
+  val s: Trees
+  val t: Trees
 
-  override protected type FunctionResult = Seq[t.FunDef]
+  private[this] val superCache = new utils.ConcurrentCache[SymbolIdentifier, SymbolIdentifier]
+  private[this] def superID(id: SymbolIdentifier)(implicit symbols: s.Symbols): SymbolIdentifier =
+    superCache.cached(id) {
+      val cid = symbols.getFunction(id).flags.collectFirst { case s.IsMethodOf(cid) => cid }.get
+      val last = s"${cid.name}$$${id.symbol.path.last}"
+      val newSymbol = Symbol((id.symbol.path.init :+ last) mkString ".")
+      SymbolIdentifier(newSymbol)
+    }
 
-  override protected def registerFunctions(symbols: t.Symbols, functions: Seq[Seq[t.FunDef]]): t.Symbols = {
-    symbols.withFunctions(functions.flatten)
+  private def firstSuper(id: SymbolIdentifier)(implicit symbols: s.Symbols): Option[SymbolIdentifier] = {
+    def rec(cd: s.ClassDef): Option[SymbolIdentifier] = {
+      import s._
+      cd.methods.find(_.symbol == id.symbol)
+        .orElse(cd.parents.headOption.flatMap(ct => rec(symbols.getClass(ct.id))))
+    }
+
+    rec(symbols.getFunction(id).flags.collectFirst { case s.IsMethodOf(id) => symbols.getClass(id) }.get)
   }
 
-  override protected def getContext(symbols: s.Symbols) = new TransformerContext(symbols)
+  private class SuperCollector(implicit symbols: s.Symbols) extends s.TreeTraverser {
+    private[this] var supers: Set[Identifier] = Set.empty
+    def getSupers: Set[Identifier] = supers
 
-  protected class TransformerContext(val symbols: s.Symbols) extends oo.TreeTransformer {
+    override def traverse(e: s.Expr): Unit = e match {
+      case s.MethodInvocation(s.Super(ct), id, _, _) =>
+        supers += firstSuper(id.unsafeToSymbolIdentifier).get
+        super.traverse(e)
+      case _ => super.traverse(e)
+    }
+  }
+
+  override protected final val funCache = new CustomCache[s.FunDef, FunctionResult]({ (fd, symbols) =>
+    val collector = new SuperCollector()(symbols)
+    collector.traverse(fd)
+    FunctionKey(fd, symbols) + new ValueKey(collector.getSupers)
+  })
+
+  override protected final val sortCache = new CustomCache[s.ADTSort, SortResult]({ (sort, symbols) =>
+    val collector = new SuperCollector()(symbols)
+    collector.traverse(sort)
+    SortKey(sort, symbols) + new ValueKey(collector.getSupers)
+  })
+
+  override protected final val classCache = new CustomCache[s.ClassDef, ClassResult]({ (cd, symbols) =>
+    val collector = new SuperCollector()(symbols)
+    collector.traverse(cd)
+    ClassKey(cd, symbols) + new ValueKey(collector.getSupers)
+  })
+
+  override protected def getContext(symbols: s.Symbols) = new TransformerContext()(symbols)
+
+  protected class TransformerContext(implicit val symbols: s.Symbols) extends oo.TreeTransformer {
     override val s: self.s.type = self.s
     override val t: self.t.type = self.t
 
     import s._
     import symbols._
 
-    private def findFirstSuperDef(symbol: Symbol, ct: s.ClassType): Option[(SymbolIdentifier, s.ClassDef)] = {
-      val cd = classes(ct.id)
-      cd.methods(symbols)
-        .find(_.symbol == symbol)
-        .map(sym => (sym, cd))
-        .orElse {
-          cd.parents.headOption flatMap { ct =>
-            findFirstSuperDef(symbol, ct)
-          }
+    val supers: Set[Identifier] = {
+      var supers: Set[Identifier] = Set.empty
+      val traverser = new s.TreeTraverser {
+        override def traverse(e: s.Expr): Unit = e match {
+          case s.MethodInvocation(s.Super(ct), id, _, _) =>
+            supers += firstSuper(id.unsafeToSymbolIdentifier).get
+            super.traverse(e)
+          case _ => super.traverse(e)
         }
-    }
-
-    private def prefixedSymbol(prefix: String, symbol: Symbol): Symbol = {
-      val last = s"${prefix}$$${symbol.path.last}"
-      Symbol((symbol.path.init :+ last) mkString ".")
-    }
-
-    val superId: Map[Identifier, Identifier] = {
-      functions.values flatMap { fd =>
-        val superRefs = s.exprOps.collect[(Identifier, Identifier)] {
-          case s.Super(ct) =>
-            val symbol = fd.id.unsafeToSymbolIdentifier.symbol
-            val Some((superFunId, superClass)) = findFirstSuperDef(symbol, ct)
-
-            val superSymbol = prefixedSymbol(superClass.id.asString, superFunId.symbol)
-            Set(superFunId -> SymbolIdentifier(superSymbol))
-
-          case _ => Set()
-
-        } (fd.fullBody)
-
-        superRefs.toSeq
       }
-    }.toMap
+
+      symbols.functions.values.foreach(traverser.traverse)
+      symbols.sorts.values.foreach(traverser.traverse)
+      symbols.classes.values.foreach(traverser.traverse)
+      supers
+    }
 
     override def transform(e: s.Expr): t.Expr = e match {
-      case s.MethodInvocation(s.Super(ct), id, tps, args) =>
-        super.transform(s.MethodInvocation(s.This(ct), superId(id), tps, args))
+      case s.MethodInvocation(sup @ s.Super(ct), id, tps, args) =>
+        t.MethodInvocation(
+          t.This(transform(ct).asInstanceOf[t.ClassType]).copiedFrom(sup),
+          superID(firstSuper(id.unsafeToSymbolIdentifier).get),
+          tps map transform,
+          args map transform
+        ).copiedFrom(e)
 
       case _ => super.transform(e)
     }
   }
 
-  override protected def extractFunction(context: TransformerContext, fd: s.FunDef): Seq[t.FunDef] = {
-    import s._
+  override protected type FunctionResult = (t.FunDef, Option[t.FunDef])
+
+  override protected def registerFunctions(symbols: t.Symbols, functions: Seq[FunctionResult]): t.Symbols = {
+    symbols.withFunctions(functions.flatMap { case (fd, ofd) => fd +: ofd.toSeq })
+  }
+
+  override protected def extractFunction(context: TransformerContext, fd: s.FunDef): FunctionResult = {
     import context.symbols
+    import s._
 
-    context.superId.get(fd.id) match {
-      case None => Seq(context.transform(fd))
-      case Some(dupId) =>
-        val cid = fd.flags collectFirst { case IsMethodOf(cid) => cid }
+    if (context.supers contains fd.id) {
+      val typeArgs = symbols.freshenTypeParams(fd.typeArgs)
+      val tpSubst = (fd.typeArgs zip typeArgs).toMap
 
-        assert(cid.isDefined)
-        assert(symbols.classes.contains(cid.get))
+      val (paramSubst, params) = fd.params
+        .map(vd => vd.copy(tpe = s.typeOps.instantiateType(vd.tpe, tpSubst)))
+        .foldLeft((Map[s.ValDef, s.Expr](), Seq[s.ValDef]())) { case ((paramSubst, params), vd) =>
+          val ntpe = s.typeOps.replaceFromSymbols(paramSubst, vd.tpe)
+          val nvd = s.ValDef(vd.id.freshen, ntpe, vd.flags).copiedFrom(vd)
+          (paramSubst + (vd -> nvd.toVariable), params :+ nvd)
+        }
 
-        val superFun = s.exprOps.freshenTypeParams(fd.copy(id = dupId), freshenIdentifiers = true)
+      val sid = superID(fd.id.unsafeToSymbolIdentifier)
+      val superFd = new s.FunDef(sid, typeArgs.map(s.TypeParameterDef(_)), params,
+        s.typeOps.replaceFromSymbols(paramSubst, typeOps.instantiateType(fd.returnType, tpSubst)),
+        s.exprOps.replaceFromSymbols(paramSubst, typeOps.instantiateType(fd.fullBody, tpSubst)),
+        fd.flags
+      ).setPos(fd)
 
-        val cd = symbols.classes(cid.get)
-        val newBody = s.MethodInvocation(
-          s.This(cd.typed(symbols).toType),
-          dupId,
-          fd.tparams.map(_.tp),
-          fd.params.map(_.toVariable)
-        )
+      val cd = symbols.getClass(fd.flags.collectFirst { case s.IsMethodOf(cid) => cid }.get)
+      val newFd = fd.copy(fullBody = s.exprOps.withBody(
+        fd.fullBody,
+        s.MethodInvocation(
+          s.This(s.ClassType(cd.id, cd.typeArgs).setPos(fd)).setPos(fd),
+          sid, fd.tparams.map(_.tp), fd.params.map(_.toVariable)
+        ).copiedFrom(fd)
+      ))
 
-        val rewrittenFun = fd.copy(fullBody = s.exprOps.withBody(fd.fullBody, newBody))
-
-        Seq(rewrittenFun, superFun).map(context.transform(_))
+      (context.transform(newFd), Some(context.transform(superFd)))
+    } else {
+      (context.transform(fd), None)
     }
   }
 
+  override protected def extractSort(context: TransformerContext, sort: s.ADTSort) = context.transform(sort)
+  override protected def extractClass(context: TransformerContext, cd: s.ClassDef) = context.transform(cd)
 }
 
 object SuperCalls {
