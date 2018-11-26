aca93aebe8af422665389884de18fac115d0644c
Romain Ruetschi <romain.ruetschi@gmail.com>
2018-11-26 13:47:47 +0100

    Don't display @unchecked annotations when printing VCs

--- core/src/main/scala/stainless/verification/VerificationChecker.scala
+++ core/src/main/scala/stainless/verification/VerificationChecker.scala
@@ -195,6 +195,15 @@ trait VerificationChecker { self =>
     rec(expr)
   }
 
+  private def prettify(expr: Expr): Expr = {
+    def rec(expr: Expr): Expr = expr match {
+      case Annotated(e, Seq(Unchecked)) => rec(e)
+      case Operator(es, recons) => recons(es map rec)
+    }
+
+    rec(expr)
+  }
+
   protected def checkVC(vc: VC, sf: SolverFactory { val program: self.program.type }): VCResult = {
     import SolverResponses._
     val s = sf.getNewSolver
@@ -203,7 +212,7 @@ trait VerificationChecker { self =>
       val cond = vc.condition
       reporter.synchronized {
         reporter.info(s" - Now solving '${vc.kind}' VC for ${vc.fd.asString} @${vc.getPos}...")
-        reporter.debug(cond.asString)
+        reporter.debug(prettify(cond).asString)
         reporter.debug("Solving with: " + s.name)
       }
 
