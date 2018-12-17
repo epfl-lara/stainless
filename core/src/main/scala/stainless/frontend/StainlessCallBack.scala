3ae7b3340b70c5af087e276a96ea842522185cce
Romain Ruetschi <romain.ruetschi@gmail.com>
2018-12-17 11:48:27 +0100

    Bypass the registry in the pipeline

--- core/src/main/scala/stainless/frontend/StainlessCallBack.scala
+++ core/src/main/scala/stainless/frontend/StainlessCallBack.scala
@@ -6,7 +6,7 @@ package frontend
 import scala.language.existentials
 
 import extraction.xlang.{ TreeSanitizer, trees => xt }
-import utils.{ CheckFilter, DependenciesFinder, JsonUtils, Registry }
+import utils.{ CheckFilter, DependenciesFinder, JsonUtils }
 
 import scala.collection.mutable.{ ListBuffer, Map => MutableMap, Set => MutableSet }
 
@@ -29,18 +29,10 @@ class StainlessCallBack(components: Seq[Component])(override implicit val contex
 
   private implicit val debugSection = DebugSectionFrontend
 
-  /** Name of the sub-directory of [[optPersistentCache]] in which the registry cache files are saved. */
-  protected val cacheSubDirectory: String = "stainless"
-
   /******************* Public Interface: Override CallBack ***************************************/
 
   final override def beginExtractions(): Unit = {
     assert(tasks.isEmpty)
-
-    if (firstCycle) {
-      loadCaches()
-      firstCycle = false
-    }
   }
 
   final override def apply(file: String, unit: xt.UnitDef,
@@ -49,21 +41,18 @@ class StainlessCallBack(components: Seq[Component])(override implicit val contex
     reporter.debug(s"\tfunctions -> [${functions.map { _.id }.sorted mkString ", "}]")
     reporter.debug(s"\tclasses   -> [${classes.map { _.id }.sorted mkString ", "}]")
 
-    // Update our state with the new data, producing new symbols through the registry.
     this.synchronized {
-      recentIdentifiers ++= (classes map { _.id }) ++ (functions map { _.id })
-      toProcess ++= functions map { _.id }
-    }
+      recentIdentifiers ++= (classes map (_.id)) ++ (functions map (_.id))
+      toProcess ++= functions map (_.id)
 
-    val symss = registry.update(classes, functions)
-    processSymbols(symss)
+      symbols = symbols.withClasses(classes).withFunctions(functions)
+    }
   }
 
-  final override def failed(): Unit = registry.failed()
+  final override def failed(): Unit = ()
 
   final override def endExtractions(): Unit = {
-    val symss = registry.checkpoint()
-    processSymbols(symss)
+    processSymbols(symbols)
 
     if (report != null) report = report.filter(recentIdentifiers.toSet)
     recentIdentifiers.clear()
@@ -81,9 +70,6 @@ class StainlessCallBack(components: Seq[Component])(override implicit val contex
     val reports = (report +: newReports) filter { _ != null }
     if (reports.nonEmpty) report = reports reduce { _ ~ _ }
     tasks.clear()
-
-    // Save cache now that we have our report
-    saveCaches()
   } catch {
     case SomeFatalError(e) =>
       stop()
@@ -130,15 +116,6 @@ class StainlessCallBack(components: Seq[Component])(override implicit val contex
     }
   }
 
-  /** Parse a JSON value into a proper Report. We assume this doesn't fail. */
-  protected def parseReportCache(json: Json): Report = json.as[Seq[(String, Json)]] match {
-    case Right(jsons) => Report(runs.flatMap { run =>
-      jsons.find(_._1 == run.component.name)
-        .map(p => RunReport(run)(run.parse(p._2)): RunReport)
-    })
-    case Left(error) => throw error
-  }
-
   // See assumption/requirements in [[CallBack]]
   final override def getReport: Option[Report] = Option(report)
 
@@ -154,63 +131,15 @@ class StainlessCallBack(components: Seq[Component])(override implicit val contex
   /** Set of functions that still need to be processed. */
   private val toProcess = MutableSet[Identifier]()
 
-  private val registry = new Registry {
-    override val context = self.context
-
-    override def computeDirectDependencies(fd: xt.FunDef): Set[Identifier] = new DependenciesFinder()(fd)
-    override def computeDirectDependencies(cd: xt.ClassDef): Set[Identifier] = new DependenciesFinder()(cd)
-
-    override def shouldBeChecked(fd: xt.FunDef): Boolean = self.shouldBeChecked(fd)
-  }
-
-  private var firstCycle = true // used to trigger cache loading the first time.
+  /** Current set of symbols */
+  private var symbols = xt.NoSymbols
 
 
   /******************* Internal Helpers ***********************************************************/
 
-  private def getCacheFile(filename: String): Option[File] =
-    utils.Caches.getCacheFile(context, optPersistentCache, cacheSubDirectory, filename)
-
-  private def getRegistryCacheFile: Option[File] = getCacheFile("registry.bin")
-  private def getReportCacheFile: Option[File] = getCacheFile("report.json")
-
-  case class CacheFiles(registry: File, report: File)
-  private def getCacheFiles: Option[CacheFiles] = (getRegistryCacheFile, getReportCacheFile) match {
-    case (None, None) => None
-    case (Some(registry), Some(report)) => Some(CacheFiles(registry, report))
-    case _ => reporter.internalError(s"inconsistent state") // either both are off, or both are on.
-  }
-
-  /** Load the registry & report caches, if specified by the user and available. */
-  private def loadCaches(): Unit = getCacheFiles foreach { caches =>
-    reporter.debug(s"Loading registry & report caches from ${caches.registry.getParent}")
-
-    if (caches.registry.isFile != caches.report.isFile) {
-      reporter.error(s"Inconsistent cache state, ignoring cache from ${caches.registry.getParent}")
-    } else if (caches.registry.isFile()) {
-      // Load registry cache
-      registry.loadCache(caches.registry)
-
-      // Load report cache
-      val json = JsonUtils.parseFile(caches.report)
-      report = parseReportCache(json)
-    }
-  }
-
-  /** Save the registry & report caches, if specified by the user. */
-  private def saveCaches(): Unit = if (report != null) getCacheFiles foreach { caches =>
-    reporter.debug(s"Saving registry & report caches to ${caches.registry.getParent}")
-
-    registry.saveCache(caches.registry)
-
-    val json = report.emitJson
-    JsonUtils.writeFile(caches.report, json)
-  }
-
-
-  private def processSymbols(symss: Iterable[xt.Symbols]): Unit = {
+  private def processSymbols(syms: xt.Symbols): Unit = {
     val ignoreFlags = Set("library", "synthetic")
-    def shouldProcess(id: Identifier, syms: xt.Symbols): Boolean = {
+    def shouldProcess(id: Identifier): Boolean = {
       !syms.getFunction(id).flags.exists(f => ignoreFlags contains f.name) && this.synchronized {
         val res = toProcess(id)
         toProcess -= id
@@ -218,8 +147,7 @@ class StainlessCallBack(components: Seq[Component])(override implicit val contex
       }
     }
 
-    // The registry tells us something should be verified in these symbols.
-    for (syms <- symss; id <- syms.functions.keys if shouldProcess(id, syms)) {
+    for (id <- syms.functions.keys if shouldProcess(id)) {
       val deps = syms.dependencies(id) + id
       val clsDeps = syms.classes.values.filter(cd => deps(cd.id)).toSeq
       val funDeps = syms.functions.values.filter(fd => deps(fd.id)).toSeq
