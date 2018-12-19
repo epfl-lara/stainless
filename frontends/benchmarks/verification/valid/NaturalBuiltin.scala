d8f8dcc9fd6cff4aefaaed6f2047a3ded4871223
Romain Ruetschi <romain.ruetschi@gmail.com>
2018-12-19 14:10:22 +0100

    Add more aliases + fix benchmark

--- frontends/benchmarks/verification/valid/NaturalBuiltin.scala
+++ frontends/benchmarks/verification/valid/NaturalBuiltin.scala
@@ -4,23 +4,22 @@ import stainless.math._
 object NaturalBuiltin {
 
   def test = {
-    assert(Nat.Zero + Nat.Zero == Nat.Zero)
-    assert(Nat.Zero + Nat.One == Nat.One)
+    assert(Nat.`0` + Nat.`0` == Nat.`0`)
+    assert(Nat.`0` + Nat.`1` == Nat.`1`)
   }
 
   def test2(n: Nat) = {
-    assert(Nat.Zero + n == n)
-    assert(n + Nat.Zero == n)
+    assert(Nat.`0` + n == n)
+    assert(n + Nat.`0` == n)
   }
 
   def test3(n: Nat) = {
-    assert(Nat.One * n == n)
-    assert(n * Nat.One == n)
+    assert(Nat.`1` * n == n)
+    assert(n * Nat.`1` == n)
   }
 
   def test4(n: Nat) = {
-    assert(Nat.Zero * n == Nat.Zero)
-    assert(n * Nat.Zero == Nat.Zero)
+    assert(Nat.`0` * n == Nat.`0`)
+    assert(n * Nat.`0` == Nat.`0`)
   }
-
 }
