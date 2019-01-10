7efe877296ae9a61f9dab4a30749856318552b61
Romain Ruetschi <romain.ruetschi@gmail.com>
2019-01-10 16:43:47 +0100

    Rename Nat benchmark to Peano avoid conflicts with stainless.math.Nat

--- frontends/benchmarks/verification/valid/NaturalBuiltin.scala
+++ frontends/benchmarks/verification/valid/NaturalBuiltin.scala
@@ -3,22 +3,22 @@ import stainless.math._
 
 object NaturalBuiltin {
 
-  def test = {
+  def natTest0 = {
     assert(Nat.`0` + Nat.`0` == Nat.`0`)
     assert(Nat.`0` + Nat.`1` == Nat.`1`)
   }
 
-  def test2(n: Nat) = {
+  def natTest2(n: Nat) = {
     assert(Nat.`0` + n == n)
     assert(n + Nat.`0` == n)
   }
 
-  def test3(n: Nat) = {
+  def natTest3(n: Nat) = {
     assert(Nat.`1` * n == n)
     assert(n * Nat.`1` == n)
   }
 
-  def test4(n: Nat) = {
+  def natTest4(n: Nat) = {
     assert(Nat.`0` * n == Nat.`0`)
     assert(n * Nat.`0` == Nat.`0`)
   }
