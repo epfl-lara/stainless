f4d6ec2f1c528f9842f17b9834c80c2437879540
Romain Ruetschi <romain.ruetschi@gmail.com>
2019-02-21 14:14:58 +0100

    Re-enable all extraction benchmarks

--- frontends/scalac/src/it/scala/stainless/ScalacExtractionSuite.scala
+++ frontends/scalac/src/it/scala/stainless/ScalacExtractionSuite.scala
@@ -4,22 +4,14 @@ package stainless
 
 class ScalacExtractionSuite extends ExtractionSuite {
 
-  testExtractAll("extraction/valid",
-    "extraction/valid/AccessorFlags.scala", // FIXME
-  )
-  testRejectAll("extraction/invalid",
-    "extraction/invalid/TraitVar1.scala", // FIXME
-  )
+  testExtractAll("extraction/valid")
+  testRejectAll("extraction/invalid")
 
   testExtractAll("verification/valid")
   testExtractAll("verification/invalid")
   testExtractAll("verification/unchecked")
 
-  testExtractAll("imperative/valid",
-    "imperative/valid/Blocks1.scala",
-    "imperative/valid/TraitVar1.scala", // FIXME
-    "imperative/valid/TraitVar2.scala", // FIXME
-  )
+  testExtractAll("imperative/valid")
   testExtractAll("imperative/invalid")
 
   testExtractAll("termination/valid")
