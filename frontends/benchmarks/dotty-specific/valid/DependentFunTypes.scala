05af2aa3c149eeb96dfcfebde986cf5ff6739346
Romain Ruetschi <romain.ruetschi@gmail.com>
2019-04-11 15:06:00 +0200

    Extract dependent function types as pi-types

---
+++ frontends/benchmarks/dotty-specific/valid/DependentFunTypes.scala
@@ -0,0 +1,26 @@
+
+object DependentFunTypes {
+
+  trait Entry {
+    type Key
+    val key: Key
+  }
+
+  def extractKey(e: Entry): e.Key = e.key
+
+  def extractor: (e: Entry) => e.Key = extractKey(_)
+
+  case class IntEntry() extends Entry {
+    type Key = Int
+    val key: Int = 42
+  }
+
+  def test1(entry: IntEntry) = {
+    assert(extractor(entry) == 42)
+  }
+
+  def test2(entry: Entry) = {
+    assert(extractor(entry) == entry.key)
+  }
+
+}
