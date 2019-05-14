epfl-lara/smart

Disallow aliasing on map/array/field updates. Fix #409

core/src/main/scala/stainless/extraction/imperative/AntiAliasing.scala
@@ -321,7 +321,7 @@ trait AntiAliasing
             val effects = getExactEffects(so)
 
             if (effects.exists(eff => !env.bindings.contains(eff.receiver.toVal)))
-              throw MissformedStainlessCode(as, "Unsupported form of array update")
+              throw MissformedStainlessCode(as, "Unsupported form of field assignment")
 
             val accessor = o.getType match {
               case _: ADTType => ADTFieldAccessor(id)

epfl-lara/stainless

core/src/main/scala/stainless/extraction/imperative/AntiAliasing.scala
@@ -321,7 +321,7 @@ trait AntiAliasing
             val effects = getExactEffects(so)
 
             if (effects.exists(eff => !env.bindings.contains(eff.receiver.toVal)))
-              throw MalformedStainlessCode(as, "Unsupported form of array update")
+              throw MalformedStainlessCode(as, "Unsupported form of field assignment")
 
             val accessor = o.getType match {
               case _: ADTType => ADTFieldAccessor(id)