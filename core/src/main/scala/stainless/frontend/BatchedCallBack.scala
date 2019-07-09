c77a28b8c575ff3fe759bad30e774dc353ee0472
Lucien Iseli <lucien.iseli93@gmail.com>
2019-07-09 16:38:29 +0200

    Batched doesn't remove library methods with non-library subclasses

--- core/src/main/scala/stainless/frontend/BatchedCallBack.scala
+++ core/src/main/scala/stainless/frontend/BatchedCallBack.scala
@@ -47,6 +47,46 @@ class BatchedCallBack(components: Seq[Component])(implicit val context: inox.Con
   def failed(): Unit = {}
 
   def endExtractions(): Unit = {
+    implicit val popts = xt.PrinterOptions.fromContext(context)
+
+    def rewriteSymbols(symbols: xt.Symbols): xt.Symbols = {
+      // remove library flag from all classes and laws that have subclasses without @library
+      ???
+    }
+
+    def keepDefinition(defn: xt.Definition, symbols: xt.Symbols): Boolean = {
+      if(defn.flags.contains(xt.Synthetic))
+        false
+      else defn match {
+        case cd: xt.ClassDef => !cd.flags.exists(f => f.name == "library")
+        case td: xt.TypeDef => !td.flags.exists(f => f.name == "library")
+        case fn: xt.FunDef =>
+          val isLibrary = fn.flags.exists(f => f.name == "library")
+
+          if (fn.isLaw)
+            if(isLibrary) {
+              val optClass = fn.getClassDef(symbols)
+
+              optClass match {
+                case None => false
+                case Some(cd) => {
+
+                  (for {
+                    subclass <- cd.descendants(symbols)
+
+                    if !subclass.flags.exists(f => f.name == "library")
+                    if !subclass.methods(symbols).map(symbols.getFunction).exists(subFn => subFn.id.name == fn.id.name && subFn.flags.exists(flag => flag.name == "library"))
+                  } yield subclass).nonEmpty
+                }
+              }
+
+            } else
+              true
+          else
+            !isLibrary
+      }
+    }
+
     val allSymbols = xt.NoSymbols
       .withClasses(currentClasses)
       .withFunctions(currentFunctions)
@@ -55,9 +95,11 @@ class BatchedCallBack(components: Seq[Component])(implicit val context: inox.Con
     def notUserFlag(f: xt.Flag) = f.name == "library" || f == xt.Synthetic
 
     val userIds =
-      currentClasses.filterNot(cd => cd.flags.exists(notUserFlag)).map(_.id) ++
-      currentFunctions.filterNot(fd => fd.flags.exists(notUserFlag)).map(_.id) ++
-      currentTypeDefs.filterNot(td => td.flags.exists(notUserFlag)).map(_.id)
+      currentClasses.filter(cd => keepDefinition(cd, allSymbols)).map(_.id) ++
+      currentFunctions.filter(fd => keepDefinition(fd, allSymbols)).map(_.id) ++
+      currentTypeDefs.filter(td => keepDefinition(td, allSymbols)).map(_.id)
+
+    // println(userIds.map(_.asString))
 
     val userDependencies = userIds.flatMap(id => allSymbols.dependencies(id)) ++ userIds
     val keepGroups = context.options.findOptionOrDefault(optKeep)
@@ -69,6 +111,8 @@ class BatchedCallBack(components: Seq[Component])(implicit val context: inox.Con
       xt.NoSymbols.withClasses(currentClasses.filter(cd => hasKeepFlag(cd.flags) || userDependencies.contains(cd.id)))
                   .withFunctions(currentFunctions.filter(fd => hasKeepFlag(fd.flags) || userDependencies.contains(fd.id)))
                   .withTypeDefs(currentTypeDefs.filter(td => hasKeepFlag(td.flags) || userDependencies.contains(td.id)))
+
+    // println(preSymbols.asString)
 
     val symbols = Recovery.recover(preSymbols)
 
