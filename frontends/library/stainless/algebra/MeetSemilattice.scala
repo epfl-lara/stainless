16097b432467d2497b8790c6bbd649946633e6e0
Lucien Iseli <lucien.iseli93@gmail.com>
2019-07-15 18:32:05 +0200

    Add join-semilattice and meet-semilattice and example min-max BigInt

---
+++ frontends/library/stainless/algebra/MeetSemilattice.scala
@@ -0,0 +1,26 @@
+package stainless.algebra
+
+import stainless.annotation._
+
+object MeetSemilattice {
+  abstract class MeetSemilattice[A] {
+    def meet(x: A, y: A): A
+
+    private def lteqv(x: A, y: A): Boolean = meet(x, y) == x
+
+    @law
+    def law_associativity(x: A, y: A, z: A): Boolean = {
+      meet(meet(x, y), z) == meet(x, meet(y, z))
+    }
+
+    @law
+    def law_commutativity(x: A, y: A): Boolean = {
+      meet(x, y) == meet(y, x)
+    }
+
+    @law
+    def law_idempotency(x: A): Boolean = {
+      meet(x, x) == x
+    }
+  }
+}
