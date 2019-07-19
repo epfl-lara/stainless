1c6c4d4196083643cee3fe70a618859eb6efafc3
Romain Ruetschi <romain.ruetschi@gmail.com>
2019-07-19 14:09:44 +0200

    Refactor TreeSanitizer test suite

--- frontends/common/src/test/scala/stainless/ast/TreeSanitizerSuite.scala
+++ frontends/common/src/test/scala/stainless/ast/TreeSanitizerSuite.scala
@@ -2,134 +2,56 @@
 package stainless
 package ast
 
+import scala.language.experimental.macros
 import org.scalatest._
 
+import stainless.macros.FileProvider
 import stainless.extraction.xlang.{trees => xt, TreeSanitizer}
 
 class TreeSanitizerSuite extends FunSuite with InputUtils {
 
-  val sources1 = List(
-    """|
-       |import stainless.lang._
-       |import stainless.annotation._
-       |import stainless.lang.StaticChecks
-       |
-       |object test {
-       |
-       |  abstract class NonSealed {
-       |    def bar: BigInt
-       |  }
-       |
-       |  def foo(outer: BigInt): NonSealed = {
-       |    case class Foo(y: BigInt) extends NonSealed {
-       |      def bar = outer
-       |    }
-       |    Foo(12)
-       |  }
-       |
-       |  def oops = {
-       |    assert(foo(1) != foo(2))
-       |  }
-       |
-       |  @ghost
-       |  def ok = {
-       |    assert(foo(1) != foo(2))
-       |  }
-       |
-       |  sealed abstract class Sealed {
-       |    def bar: BigInt
-       |  }
-       |
-       |  def foo2(outer: BigInt): Sealed = {
-       |    case class Foo2(y: BigInt) extends Sealed {
-       |      def bar = outer
-       |    }
-       |    Foo2(12)
-       |  }
-       |
-       |  def oops2 = {
-       |    assert(foo2(1) != foo2(2))
-       |  }
-       |
-       |  def oops3 = {
-       |    val a = (x: BigInt) => x
-       |    val b = (x: BigInt) => x
-       |    assert(a == b)
-       |  }
-       |
-       |  @ghost
-       |  def ok3 = {
-       |    val a = (x: BigInt) => x
-       |    val b = (x: BigInt) => x
-       |    assert(a == b)
-       |  }
-       |
-       |  def ok3bis = {
-       |    val a = (x: BigInt) => x
-       |    val b = (x: BigInt) => x
-       |    StaticChecks.assert(a == b)
-       |  }
-       |
-       |  def compare(prop: Boolean): Unit = ()
-       |  def compareGhost(@ghost prop: Boolean): Unit = ()
-       |
-       |  def oops4 = {
-       |    val a = (x: BigInt) => x
-       |    val b = (x: BigInt) => x
-       |    compare(a == b)
-       |  }
-       |
-       |  def ok4 = {
-       |    val a = (x: BigInt) => x
-       |    val b = (x: BigInt) => x
-       |    compareGhost(a == b)
-       |  }
-       |
-       |  def oops5 = {
-       |    val a = (x: BigInt) => x
-       |    val b = (x: BigInt) => x
-       |    compare((a, a) == (b, b))
-       |  }
-       |
-       |  def oops6 = {
-       |    val a = (x: BigInt) => x
-       |    val b = (x: BigInt) => x
-       |
-       |    case class InnerClass() {
-       |      @ghost def innerOk(test: Boolean): Boolean = test && a == b // ok
-       |      def innerBad(@ghost test: Boolean): Boolean = a == b // bad
-       |    }
-       |    StaticChecks.assert(InnerClass().innerOk(a == b)) // ok
-       |    assert(InnerClass().innerBad(a == b)) // ok
-       |  }
-       |}
-       |""".stripMargin)
+  val sources = List(
+    FileProvider.getFileContents(
+      "frontends/common/src/test/resources/SoundEquality.scala"
+    )
+  )
 
-  test("SoundEquality check yields the right errors") {
-    implicit val ctx = stainless.TestContext.empty
-    val (_, program) = load(sources1, sanitize = false)
+  implicit val ctx = stainless.TestContext.empty
+  val (_, program) = load(sources, sanitize = false)
 
-    val errors = TreeSanitizer(xt).check(program.symbols)
+  val errors = TreeSanitizer(xt).check(program.symbols)
 
-    // errors.sortBy(_.tree.getPos).foreach { err =>
-    //   println(s"${err.tree.getPos.fullString}")
-    //   println(s"${err.getMessage}")
-    //   println(s"${err.tree}")
-    //   println()
-    // }
+  // errors.sortBy(_.tree.getPos).foreach { err =>
+  //   println(s"${err.tree.getPos.fullString}")
+  //   println(s"${err.getMessage}")
+  //   println(s"${err.tree}")
+  //   println()
+  // }
 
-    errors
-      .sortBy(_.tree.getPos)
-      .zipWithIndex foreach {
-        case (err, 0) => assert(err.tree.getPos.line == 20)
-        case (err, 1) => assert(err.tree.getPos.line == 40)
-        case (err, 2) => assert(err.tree.getPos.line == 46)
-        case (err, 3) => assert(err.tree.getPos.line == 68)
-        case (err, 4) => assert(err.tree.getPos.line == 80)
-        case (err, 5) => assert(err.tree.getPos.line == 89)
-        case (err, _) => assert(false, s"Unexpected error yielded at ${err.tree.getPos}: ${err.getMessage}")
+  val expected = Map(
+    0 -> 20,
+    1 -> 40,
+    2 -> 46,
+    3 -> 68,
+    4 -> 80,
+    5 -> 89,
+    6 -> 98,
+  )
+
+  test(s"SoundEquality check yield exactly ${expected.size} errors") {
+    assert(errors.length == expected.size)
+  }
+
+  errors.zipWithIndex foreach {
+    case (err, i) if expected.contains(i) =>
+      val line = expected(i)
+      test(s"SoundEquality check yields an error at line $line") {
+        assert(err.tree.getPos.line == line)
       }
 
-    assert(errors.length == 6)
+    case (err, _) =>
+      test(s"SoundEquality check does not yield any other errors") {
+        assert(false, s"Unexpected error yielded at ${err.tree.getPos}: ${err.getMessage}")
+      }
   }
 }
