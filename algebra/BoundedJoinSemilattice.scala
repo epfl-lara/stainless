76e64c538eb7b3197ab4e2a5369f44181694ea5c
Lucien Iseli <lucien.iseli93@gmail.com>
2019-07-22 14:37:50 +0200

    Add template implementations inside files

--- algebra/BoundedJoinSemilattice.scala
+++ algebra/BoundedJoinSemilattice.scala
@@ -1,6 +1,8 @@
 package stainless.algebra
 
 import stainless.annotation._
+import stainless.math.Nat
+
 
 object BoundedJoinSemilattice {
   import JoinSemilattice._
@@ -12,5 +14,11 @@ object BoundedJoinSemilattice {
     def law_identity(x: A): Boolean = {
       join(x, zero) == x
     }
+  }
+
+  def maxNatSemilattice: BoundedJoinSemilattice[Nat] = new BoundedJoinSemilattice[Nat] {
+    def zero: Nat = Nat(0)
+
+    def join(x: Nat, y: Nat): Nat = stainless.math.max(x, y)
   }
 }
