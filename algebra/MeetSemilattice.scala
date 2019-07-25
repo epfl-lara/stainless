763dec2a4f65ac4c1c47e0db3c44691bdabbb727
Lucien Iseli <lucien.iseli93@gmail.com>
2019-07-25 13:32:50 +0200

    Put abstract classes at top level and put all files in same package

--- algebra/MeetSemilattice.scala
+++ algebra/MeetSemilattice.scala
@@ -2,28 +2,28 @@ package stainless.algebra
 
 import stainless.annotation._
 
-object MeetSemilattice {
-  abstract class MeetSemilattice[A] {
-    def meet(x: A, y: A): A
+abstract class MeetSemilattice[A] {
+  def meet(x: A, y: A): A
 
-    final def lteqv(x: A, y: A): Boolean = meet(x, y) == x
+  final def lteqv(x: A, y: A): Boolean = meet(x, y) == x
 
-    @law
-    def law_associativity(x: A, y: A, z: A): Boolean = {
-      meet(meet(x, y), z) == meet(x, meet(y, z))
-    }
-
-    @law
-    def law_commutativity(x: A, y: A): Boolean = {
-      meet(x, y) == meet(y, x)
-    }
-
-    @law
-    def law_idempotency(x: A): Boolean = {
-      meet(x, x) == x
-    }
+  @law
+  def law_associativity(x: A, y: A, z: A): Boolean = {
+    meet(meet(x, y), z) == meet(x, meet(y, z))
   }
 
+  @law
+  def law_commutativity(x: A, y: A): Boolean = {
+    meet(x, y) == meet(y, x)
+  }
+
+  @law
+  def law_idempotency(x: A): Boolean = {
+    meet(x, x) == x
+  }
+}
+
+object MeetSemilattice {
   def minSemilattice: MeetSemilattice[BigInt] = new MeetSemilattice[BigInt] {
     def meet(x: BigInt, y: BigInt): BigInt = stainless.math.min(x, y)
   }
--- algebra/PartialOrder.scala
+++ algebra/PartialOrder.scala
@@ -1,34 +1,32 @@
+package stainless.algebra
+
 import stainless.annotation._
 import stainless.lang._
 
-object PartialOrder {
-    import Eq._
+abstract class PartialOrder[A] extends Eq[A] {
+    def lteq(x: A, y: A): Boolean
 
-    abstract class PartialOrder[A] extends Eq[A] {
-        def lteq(x: A, y: A): Boolean
-
-        final def partialOrder(x: A, y: A): Option[BigInt] = {
-            (lteq(x, y), lteq(y, x)) match {
-                case (true, true) => Some(0)
-                case (true, false) => Some(-1)
-                case (false, true) => Some(1)
-                case (false, false) => None[BigInt]()
-            }
+    final def partialOrder(x: A, y: A): Option[BigInt] = {
+        (lteq(x, y), lteq(y, x)) match {
+            case (true, true) => Some(0)
+            case (true, false) => Some(-1)
+            case (false, true) => Some(1)
+            case (false, false) => None[BigInt]()
         }
+    }
 
-        @law
-        def law_reflexivity(x: A): Boolean = {
-            lteq(x, x)
-        }
+    @law
+    def law_reflexivity(x: A): Boolean = {
+        lteq(x, x)
+    }
 
-        @law
-        def law_antisymmetry(x: A, y: A): Boolean = {
-            (lteq(x, y) && lteq(y, x)) ==> eq(x, y)
-        }
+    @law
+    def law_antisymmetry(x: A, y: A): Boolean = {
+        (lteq(x, y) && lteq(y, x)) ==> eq(x, y)
+    }
 
-        @law
-        def law_transitivity(x: A, y: A, z: A): Boolean = {
-            (lteq(x, y) && lteq(y, z)) ==> lteq(x, z)
-        }
+    @law
+    def law_transitivity(x: A, y: A, z: A): Boolean = {
+        (lteq(x, y) && lteq(y, z)) ==> lteq(x, z)
     }
 }
