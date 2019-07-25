71430b297e0437ddb852c51e5c77592b368a07d6
Lucien Iseli <lucien.iseli93@gmail.com>
2019-07-25 13:32:50 +0200

    Put abstract classes at top level and put all files in same package

--- algebra/BoundedJoinSemilattice.scala
+++ algebra/BoundedJoinSemilattice.scala
@@ -3,19 +3,16 @@ package stainless.algebra
 import stainless.annotation._
 import stainless.math.Nat
 
+abstract class BoundedJoinSemilattice[A] extends JoinSemilattice[A] {
+  def zero: A
+
+  @law
+  def law_identity(x: A): Boolean = {
+    join(x, zero) == x
+  }
+}
 
 object BoundedJoinSemilattice {
-  import JoinSemilattice._
-
-  abstract class BoundedJoinSemilattice[A] extends JoinSemilattice[A] {
-    def zero: A
-
-    @law
-    def law_identity(x: A): Boolean = {
-      join(x, zero) == x
-    }
-  }
-
   def maxNatSemilattice: BoundedJoinSemilattice[Nat] = new BoundedJoinSemilattice[Nat] {
     def zero: Nat = Nat(0)
 
