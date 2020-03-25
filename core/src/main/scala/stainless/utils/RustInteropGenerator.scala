d094c3ce3e9213225eff8da1a11f6b8fc10c9b75
Georg Schmid <georg.schmid@epfl.ch>
2020-03-25 20:21:50 +0100

    RustInteropGeneratorTool: Rewrite all Identifiers to SymbolIdentifiers

--- core/src/main/scala/stainless/utils/RustInteropGenerator.scala
+++ core/src/main/scala/stainless/utils/RustInteropGenerator.scala
@@ -3,9 +3,13 @@
 package stainless
 package utils
 
+import scala.reflect.runtime.universe._
 import inox.utils.{RustInteropGeneration, RustInteropGenerator}
 
 object RustInteropGeneratorTool extends inox.utils.RustInteropGenerator {
   override protected val serializer: RustInteropGeneration =
-    new XLangSerializer(stainless.extraction.xlang.trees) with RustInteropGeneration
+    new XLangSerializer(stainless.extraction.xlang.trees) with RustInteropGeneration {
+      override protected val optSymbolIdentifierT: Option[Type] =
+        Some(typeOf[stainless.ast.SymbolIdentifier])
+    }
 }
