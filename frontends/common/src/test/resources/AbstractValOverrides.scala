ababcfe798f9b01c76dd393b9b1621b722c09758
Jad Hamza <jad.hamza@epfl.ch>
2020-08-03 14:48:22 +0200

    Rearrange tests

--- frontends/common/src/test/resources/AbstractValOverrides.scala
+++ frontends/common/src/test/resources/AbstractValOverrides.scala
@@ -14,23 +14,3 @@ object AbstractValsOverrideOk {
   case class Ok(x: Int, y: Int) extends Sub
 }
 
-object AbstractValsOverrideBad {
-
-  abstract class Abs {
-    val x: Int
-    val y: Int
-    def z: Int
-  }
-
-  abstract class Sub extends Abs {
-    val x: Int = 1
-  }
-
-  case class Ok(y: Int, z: Int) extends Sub
-
-  case class Bad() extends Sub {
-    def y: Int = 1
-    def z: Int = 2
-  }
-}
-
