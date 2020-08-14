76f9298a6092aefaed2281f35e98eb52b0ed2e0b
Georg Stefan Schmid <georg.schmid@epfl.ch>
2020-08-14 15:05:13 +0200

    Add conservative handling of function invocations and escaped objects

--- core/src/main/scala/stainless/extraction/imperative/AliasAnalyzer.scala
+++ core/src/main/scala/stainless/extraction/imperative/AliasAnalyzer.scala
@@ -42,6 +42,18 @@ trait AliasAnalyzer
       // TODO: Handle other types
     }
 
+    def isHeapParam(vd: ValDef): Boolean =
+      isHeapType(vd.tpe)
+
+    def getEscapingParams(fd: FunDef): Seq[ValDef] =
+      // fd.params.filter(isHeapParam)
+      fd.params.filter(p => isHeapParam(p) && !p.flags.contains(IsPure))
+
+    def getReturnType(fd: FunDef): Type = {
+      val escapingParams = getEscapingParams(fd)
+      tupleTypeWrap(fd.returnType +: escapingParams.map(_.tpe))
+    }
+
     // Data structures
 
     lazy val True = BooleanLiteral(true)
@@ -66,6 +78,8 @@ trait AliasAnalyzer
 
     // A symbolic heap object (NOT a program binding)
     case class Object(vd: ValDef) {
+      require(isHeapType(vd.tpe))
+
       override def toString: String = s"Object#${vd.id}"
     }
 
@@ -125,9 +139,11 @@ trait AliasAnalyzer
       contents: Map[Object, Map[Accessor, Target]],
       containers: Map[Object, Target], // back edges for contents  // FIXME, doesn't make sense atm
       bindings: Map[ValDef, Target], // mapping from program bindings to heap objects
+      escaped: Set[Object], // objects that escaped and thus don't have precise information about
     ) {
       def withObject(obj: Object): Graph =
         this.copy(objects = objects + obj, contents = contents + (obj -> Map.empty))
+
       def withBinding(vd: ValDef, target: Target): Graph =
         this.copy(bindings = bindings + (vd -> target))
 
@@ -137,6 +153,15 @@ trait AliasAnalyzer
         this.copy(contents = entries.foldLeft(contents) { case (cs, (obj, target)) =>
           Graph.updatedContents(cs, obj, accessor, target)
         })
+
+      def havoc(objs: Set[Object]): Graph = {
+        assert(objs.subsetOf(this.objects))
+        val newContents = contents ++ objs.map(_ -> Map.empty[Accessor, Target])
+        this.copy(contents = newContents)
+      }
+
+      def withEscaped(objs: Set[Object]): Graph =
+        havoc(objs).copy(escaped = escaped ++ objs)
 
       // Ensures that we have explicit objects representing the values accessible at `accessor` of
       // all `objects`.
@@ -154,6 +179,15 @@ trait AliasAnalyzer
         }
         this.copy(objects = newObjects, contents = newContents)
       }
+
+      // Find all the transitively reachable objects starting from a given set of objects
+      // TODO: Use a path condition to allow computing the precise set
+      def computeReachable(initialObjects: Set[Object]): Set[Object] =
+        inox.utils.fixpoint[Set[Object]](objects => {
+          objects ++ objects.flatMap { obj =>
+            this.contents(obj).values.flatMap(_.objects.toSet)
+          }
+        })(initialObjects)
     }
 
     object Graph {
@@ -183,10 +217,6 @@ trait AliasAnalyzer
       // TODO: Try to eliminate targets using the path condition and a simplifier or solver?
       def update(g: Graph, recvTarget: Target, accessor: Accessor, valueTarget: Target): Graph = {
         // Gather the conditions under which a receiver object will be modified
-        // val updateConds: MutableMap[Object, Seq[Expr]] = MutableMap.empty
-        // recvTarget.toSeq.foreach { case (cond, recvObj) =>
-        //   updateConds(recvObj) = cond +: updateConds.getOrElse(recvObj, Seq.empty)
-        // }
         val updateConds = recvTarget.toSeq.foldLeft(Map.empty[Object, Seq[Expr]]) {
           case (map, (cond, recvObj)) =>
             map + (recvObj -> (cond +: map.getOrElse(recvObj, Seq.empty)))
@@ -209,10 +239,41 @@ trait AliasAnalyzer
         g.withContent(updates, accessor)
       }
 
+      // Erase all information about objects reachable from the given object
+      def havoc(g: Graph, targets: Seq[Target]): Graph = {
+        // TODO: Cut down the initialObjects set by using the path condition
+        val initialObjects = targets.flatMap(target => target.objects)
+        val reachableObjects = g.computeReachable(initialObjects.toSet)
+        g.havoc(reachableObjects)
+      }
+
+      // Mark all objects reachable from the given targets as escaped
+      def escape(g: Graph, targets: Seq[Target]): Graph = {
+        // TODO: Cut down the initialObjects set by using the path condition
+        val initialObjects = targets.flatMap(target => target.objects)
+        val reachableObjects = g.computeReachable(initialObjects.toSet)
+        g.withEscaped(reachableObjects)
+      }
+
       def rec(g: Graph, expr: Expr): (Graph, Option[Target]) = {
+        def recSequential(g: Graph, exprs: Seq[Expr]): (Graph, Seq[Option[Target]]) =
+          exprs.foldLeft((g, Seq.empty[Option[Target]])) { case ((ga, targets), expr) =>
+            val (gb, target) = rec(ga, expr)
+            (gb, target +: targets)
+          }
+
         expr match {
           case v: Variable =>
-            (g, Some(g.bindings(v.toVal)))
+            val targetOpt = g.bindings.get(v.toVal)
+            assert(!isHeapType(v.tpe) || targetOpt.isDefined)
+
+            val escapedTargetObjs = g.escaped intersect targetOpt.get.objects.toSet
+            if (escapedTargetObjs.nonEmpty) {
+              val objsStr = escapedTargetObjs.mkString("{", ", ", "}")
+              ctx.reporter.fatalError(s"Invalid program: reference to escaped objects $objsStr")
+            }
+
+            (g, targetOpt)
 
           case Let(vd, value, body) =>
             val (g1, valueTargetOpt) = rec(g, value)
@@ -252,16 +313,39 @@ trait AliasAnalyzer
               (g, None)
             }
 
-          case fi: FunctionInvocation if !symbols.isRecursive(fi.id) =>
-            exprOps.withoutSpecs(symbols.simplifyLets(fi.inlined))
-              .map(rec(g, _))
-              .getOrElse(???)
+          // case fi: FunctionInvocation if !symbols.isRecursive(fi.id) =>
+          //   exprOps.withoutSpecs(symbols.simplifyLets(fi.inlined))
+          //     .map(rec(g, _))
+          //     .getOrElse(???)
 
-          case FunctionInvocation(fun, _, args) =>
-            // Handle properly
-            // TODO: Mark arguments as moved
-            // TODO: Add object to represent result
-            ???
+          case fi: FunctionInvocation =>
+            val fd = fi.tfd.fd
+
+            val (g1, argTargets) = recSequential(g, fi.args)
+
+            val escParams = getEscapingParams(fd)
+            val (escTargets_, nonEscTargets_) = fd.params
+              .zip(argTargets)
+              .partition { case (p, _) => escParams.contains(p) }
+            val escTargets = escTargets_.collect { case (p, Some(t)) => t }
+            val nonEscTargets = nonEscTargets_.collect { case (p, Some(t)) => t }
+
+            // Havoc non-escaping targets
+            val g2 = havoc(g1, nonEscTargets)
+
+            // Havoc and mark escaping arguments
+            val g3 = escape(g2, escTargets)
+
+            // Add new object returned from the call
+            val returnTpe = fi.getType
+            val (g4, targetOpt) = if (isHeapType(returnTpe)) {
+              val returnObj = Object.fresh("ret", returnTpe)
+              (g3.withObject(returnObj), Some(Target(returnObj)))
+            } else {
+              (g3, None)
+            }
+
+            (g4, targetOpt)
 
           case _ =>
             val kind = expr.getClass.getName
@@ -269,10 +353,11 @@ trait AliasAnalyzer
         }
       }
 
-      val inputObjects = inputs.map(input => Object(input.freshen))
+      // Create the initial graph containing only the inputs
+      val inputObjects = inputs.filter(isHeapParam).map(input => Object(input.freshen))
       val contents = inputObjects.map(_ -> Map.empty[Accessor, Target]).toMap
       val bindings = inputs.zip(inputObjects.map(Target.apply)).toMap
-      val graph = Graph(inputObjects.toSet, contents, Map.empty, bindings)
+      val graph = Graph(inputObjects.toSet, contents, Map.empty, bindings, Set.empty)
 
       val (resultGraph, resultTargetOpt) = rec(graph, expr)
       (resultGraph, resultTargetOpt.map(target => (ValDef(ResultId, expr.getType), target)))
@@ -301,12 +386,20 @@ trait AliasAnalyzer
           s"""$src -> obj_${I(recv.vd.id)} [${allOpts.mkString(", ")}]"""
         } .mkString("\n")
       }
+
       val objects = graph.objects.map { obj =>
+        // Object name and record fields
         val fields = graph.contents(obj)
         val fieldsStr = if (fields.nonEmpty) {
           fields.keys.toSeq.sortBy(_.id).map(f => s"<${I(f.id)}> ${N(f.id)}").mkString("|")
         } else ""
-        s"""obj_${I(obj.vd.id)}[label="{${N(obj.vd.id)} | {$fieldsStr}}"]"""
+        val lbl = s"""label="{${N(obj.vd.id)} | {$fieldsStr}}""""
+
+        // Mark escaped objects
+        val escapedOpt = if (graph.escaped.contains(obj)) Some("color=crimson") else None
+
+        val allOpts = Seq(lbl) ++ escapedOpt
+        s"""obj_${I(obj.vd.id)}[${allOpts.mkString(", ")}]"""
       }
 
       val contents = graph.contents.toSeq.flatMap { case (obj, fields) =>
