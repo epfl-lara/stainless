342e4c7eab15c3d95a4c7fbac6b29375baf9d38d
Jad Hamza <jad.hamza@epfl.ch>
2020-09-03 12:17:19 +0200

    Improve measure type-check and HTML output

--- core/src/main/scala/stainless/verification/TypeCheckerDerivation.scala
+++ core/src/main/scala/stainless/verification/TypeCheckerDerivation.scala
@@ -14,10 +14,12 @@ object TypeCheckerDerivation {
   sealed abstract class Judgment(val tc: TypingContext) extends inox.utils.Positioned {
     override def getPos = tc.getPos
     def asString(implicit opts: PrinterOptions): String
+    def htmlClass: String
   }
 
   case class IsType(override val tc: TypingContext, t: Type) extends Judgment(tc) {
     override def asString(implicit opts: PrinterOptions) = s"⊢ ${typeColor(shortString(t.asString))} is a type"
+    def htmlClass = "isType"
   }
 
   case class CheckType(override val tc: TypingContext, e: Expr, t: Type) extends Judgment(tc) {
@@ -25,30 +27,37 @@ object TypeCheckerDerivation {
       case TrueBoolean() => s"⊢ ${termColor(shortString(e.asString))} is true"
       case _ => s"⊢ ${termColor(shortString(e.asString))} ⇓ ${typeColor(shortString(t.asString))}"
     }
+    def htmlClass = "checkType"
   }
 
   case class InferType(override val tc: TypingContext, e: Expr, t: Type) extends Judgment(tc) {
     override def asString(implicit opts: PrinterOptions) = s"⊢ ${termColor(shortString(e.asString))} ⇑ ${typeColor(shortString(t.asString))}"
+    def htmlClass = "inferType"
   }
 
   case class IsSubtype(override val tc: TypingContext, t1: Type, t2: Type) extends Judgment(tc) {
     override def asString(implicit opts: PrinterOptions) = s"⊢ ${typeColor(shortString(t1.asString))} <: ${typeColor(shortString(t2.asString))}"
+    def htmlClass = "isSubtype"
   }
 
   case class AreEqualTypes(override val tc: TypingContext, t1: Type, t2: Type) extends Judgment(tc) {
     override def asString(implicit opts: PrinterOptions) = s"⊢ ${typeColor(shortString(t1.asString))} =:= ${typeColor(shortString(t2.asString))}"
+    def htmlClass = "areEqualTypes"
   }
 
   case class JVC(override val tc: TypingContext, e: Expr) extends Judgment(tc) {
     override def asString(implicit opts: PrinterOptions) = s"<span style='font-weight: bold'>⊢ ${termColor(shortString(e.asString))} is true (VC: ${tc.vcKind})</span>"
+    def htmlClass = "vc"
   }
 
   case class OKFunction(id: Identifier) extends Judgment(TypingContext.empty) {
     override def asString(implicit opts: PrinterOptions) = s"⊢ ${termColor(id.asString)} OK"
+    def htmlClass = "okFun"
   }
 
   case class OKADT(id: Identifier) extends Judgment(TypingContext.empty) {
     override def asString(implicit opts: PrinterOptions) = s"⊢ ${typeColor(id.asString)} OK"
+    def htmlClass = "okADT"
   }
 
   case class NodeTree[T](node: T, children: Seq[NodeTree[T]])
@@ -77,7 +86,7 @@ object TypeCheckerDerivation {
   def prettyPrint(t: NodeTree[Judgment], depth: Int)(implicit opts: PrinterOptions): String = {
     val indentation = "  " * depth
     val childrenString = prettyPrint(t.children, depth + 1)
-    indentation + s"<li> <span title='${t.node.getPos.fullString}\n${t.node.tc.asString()}'> ${t.node.asString} </span>\n" +
+    indentation + s"<li> <span class='node ${t.node.htmlClass}' title='${t.node.getPos.fullString}\n${t.node.tc.asString()}'> ${t.node.asString} </span>\n" +
       childrenString +
     indentation + "</li>"
   }
@@ -92,8 +101,101 @@ object TypeCheckerDerivation {
     fw.write("<html lang=\"en\">")
     fw.write("<head>\n")
     fw.write("<meta charset=\"UTF-8\">\n")
+    fw.write("""|<style>
+                |body {
+                |  font-family: "Fira Code", Menlo, Monaco, monospace;
+                |  background-color: white;
+                |  color: black;
+                |}
+                |
+                |.inferType {
+                |  background-color: #f0f0ff
+                |}
+                |
+                |.inferType:hover {
+                |  background-color: #e0e0ee
+                |}
+                |
+                |.checkType {
+                |  background-color: #f0fff0
+                |}
+                |
+                |.checkType:hover {
+                |  background-color: #e0eee0
+                |}
+                |
+                |.areEqualTypes {
+                |  background-color: #feffc2
+                |}
+                |
+                |.areEqualTypes:hover {
+                |  background-color: #dadba7
+                |}
+                |
+                |.isSubType {
+                |  background-color: #fff6eb
+                |}
+                |
+                |.isSubType:hover {
+                |  background-color: #d4d492
+                |}
+                |
+                |.isType {
+                |  background-color: #fff6eb
+                |}
+                |
+                |.isType:hover {
+                |  background-color: #d4d492
+                |}
+                |
+                |.vc {
+                |  background-color: #ffeccf
+                |}
+                |
+                |.vc:hover {
+                |  background-color: #d9c8b0
+                |}
+                |
+                |.okFun {
+                |  background-color: #efd1ff
+                |}
+                |
+                |.okFun:hover {
+                |  background-color: #c9b0d6
+                |}
+                |
+                |.okADT {
+                |  background-color: #efd1ff
+                |}
+                |
+                |.okADT:hover {
+                |  background-color: #c9b0d6
+                |}
+                |
+                |ul {
+                |  list-style-type: none;
+                |  padding-left: 10px;
+                |}
+                |</style>""".stripMargin)
     fw.write("</head>\n\n")
+
     fw.write("<body>\n")
+    fw.write("""<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>""")
+    fw.write("<script>\n")
+    fw.write("""|$(document).ready(function () {
+                |  $('.node').click(function(e) {
+                |    if (!getSelection().toString()) {
+                |      text = $(this).html()
+                |      if (text.startsWith("(Folded) "))
+                |        $(this).html(text.substring(9));
+                |      else
+                |        $(this).html("(Folded) " + text);
+                |      $(this).parent().find("ul").toggle();
+                |    }
+                |  });
+                |});
+                |""".stripMargin)
+    fw.write("</script>\n")
     fw.write(prettyPrint(trees, 0) + "\n")
     fw.write("</body>\n")
     fw.write("</html>")
