b57ff34f14200eacc6dad0cce0fb2d32948d4e18
Antoine Brunner <antoine.brunner@epfl.ch>
2020-10-21 11:19:07 +0200

    Add the AnyHeapRef trait to represent reference types
    - AnyHeapRef represents heap allocated objects that will be handled by the imperative phase.
      The user will have to explicitely annotate such types.
    - The `reads` and `modifies` clauses now take sets of `AnyHeapRef` as arguments, meaning
      that only types annotated with the trait can be passed in those clauses.
    - The function `refEq` is now used to express reference equality.

---
+++ frontends/benchmarks/verification/valid/HeapProjections.scala
@@ -0,0 +1,37 @@
+
+import stainless.lang._
+import stainless.annotation._
+
+object Examples {
+  final case class Box(var value: BigInt) extends AnyHeapRef
+
+  @extern
+  def read(x: Box): BigInt = {
+    reads(Set(x))
+    modifies(Set())
+    x.value
+  }
+
+  def readInvariant(x: Box, y: Box): Unit = {
+    require(!(x refEq y))
+    val x1 = read(x)
+    y.value += 1
+    val x2 = read(x)
+    assert(x1 == x2)
+  }
+
+  @extern
+  def write(x: Box, y: Box): Unit = {
+    reads(Set(x, y))
+    modifies(Set(x))
+    x.value += 1
+  }
+
+  def writeInvariant(x: Box, y: Box): Unit = {
+    require(!(x refEq y))
+    val y1 = y.value
+    write(x, y)
+    val y2 = y.value
+    assert(y1 == y2)
+  }
+}
