85b59cb4e8b952feb6ecb06127361d105743aaee
Georg Stefan Schmid <georg.schmid@epfl.ch>
2020-11-20 16:39:58 +0100

    Fix Streams example by adding exceptions to new AssertionInjector behavior

--- core/src/main/scala/stainless/verification/AssertionInjector.scala
+++ core/src/main/scala/stainless/verification/AssertionInjector.scala
@@ -54,21 +54,34 @@ trait AssertionInjector extends transformers.TreeTransformer {
         ti
       ).copiedFrom(i), transform(v)).copiedFrom(e)
 
-    // case sel @ s.ADTSelector(expr, _) =>
-    //   t.Assert(
-    //     t.IsConstructor(transform(expr), sel.constructor.id).copiedFrom(e),
-    //     Some("Cast error"),
-    //     super.transform(e)
-    //   ).copiedFrom(e)
-
     case sel @ s.ADTSelector(expr, _) =>
-      let("recv" :: expr.getType, transform(expr)) { recv =>
+      def isIndexedType(tpe: s.Type) =
+        tpe match {
+          case _: s.RecursiveType => true
+          case _ => false
+        }
+      val hasIndexedType = expr match {
+        case v: s.Variable => isIndexedType(v.tpe)
+        case fi: s.FunctionInvocation => isIndexedType(fi.tfd.returnType)
+        case _ => false
+      }
+
+      // NOTE(gsps): Keeping the old, code-duplicating behavior here, since index annotations on
+      //   types are not propagated through `expr.getType`, breaking the Streams example.
+      if (hasIndexedType)
         t.Assert(
-          t.IsConstructor(recv, sel.constructor.id).copiedFrom(e),
+          t.IsConstructor(transform(expr), sel.constructor.id).copiedFrom(e),
           Some("Cast error"),
-          super.transform(sel.copy(adt = recv).copiedFrom(e))
+          super.transform(e)
         ).copiedFrom(e)
-      }.copiedFrom(e)
+      else
+        let("recv" :: expr.getType, transform(expr)) { recv =>
+          t.Assert(
+            t.IsConstructor(recv, sel.constructor.id).copiedFrom(e),
+            Some("Cast error"),
+            super.transform(sel.copy(adt = recv).copiedFrom(e))
+          ).copiedFrom(e)
+        }.copiedFrom(e)
 
     case BVTyped(true, size, e0 @ s.Plus(lhs0, rhs0)) if checkOverflow =>
       val lhs = transform(lhs0)
