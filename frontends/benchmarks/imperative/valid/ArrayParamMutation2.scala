f76e4fe7abcacc92fcd4160776eb31005a4caf5d
Georg Stefan Schmid <georg.schmid@epfl.ch>
2021-01-16 11:32:22 +0100

    Fix old imperative benchmarks breaking after assertionInjector not running on unfolded functions

--- frontends/benchmarks/imperative/valid/ArrayParamMutation2.scala
+++ frontends/benchmarks/imperative/valid/ArrayParamMutation2.scala
@@ -4,13 +4,13 @@ object ArrayParamMutation2 {
 
   def rec(a: Array[BigInt]): BigInt = {
     require(a.length > 1 && a(0) >= 0)
-    if(a(0) == 0) 
+    if(a(0) == 0)
       a(1)
     else {
       a(0) = a(0) - 1
       a(1) = a(1) + a(0)
       rec(a)
     }
-  } ensuring(res => a(0) == 0)
+  } ensuring(res => a.length > 0 && a(0) == 0)
 
 }
