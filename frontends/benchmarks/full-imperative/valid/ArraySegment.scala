22861e6ed7ba8725cafb22ee724e5d8800bec055
Georg Stefan Schmid <georg.schmid@epfl.ch>
2021-01-18 21:26:13 +0100

    Fix TypeEncoding bug using old specs API (duplicating lets). Make ArraySegment test case fast with update postconditions.

--- frontends/benchmarks/full-imperative/valid/ArraySegment.scala
+++ frontends/benchmarks/full-imperative/valid/ArraySegment.scala
@@ -6,6 +6,18 @@ import stainless.proof._
 import stainless.lang.StaticChecks._
 
 object ArraySegment {
+   def updateSizeLemma[T](xs: List[T], i: BigInt, y: T): Boolean = {
+    require(0 <= i && i < xs.size)
+    decreases(xs)
+
+    xs match {
+      case Nil() => true
+      case Cons(x, xs) => if (i == 0) true else updateSizeLemma[T](xs, i - 1, y)
+    }
+
+    xs.updated(i, y).size == xs.size
+  }.holds
+
   final case class SArray[T](var content: List[T]) extends AnyHeapRef
   {
     def fill(n: BigInt)(default: T): Unit = {
@@ -15,11 +27,11 @@ object ArraySegment {
       require(0 <= n)
       modifies(Set(this))
       content = List.fill(n)(default)
-    }
-  
+    } ensuring { _ => content.size == n }
+
     def apply(i: BigInt): T = {
       reads(Set(this))
-      require(0 <= i && i < content.size)      
+      require(0 <= i && i < content.size)
       content(i)
     }
 
@@ -27,16 +39,16 @@ object ArraySegment {
       reads(Set(this))
       require(0 <= i && i < content.size)
       modifies(Set(this))
+      updateSizeLemma(content, i, v)
       content = content.updated(i, v)
-    }
-
+    } ensuring { _ => content.size == old(content.size) }
   }
-  
+
   final case class ArraySlice[T](a: SArray[T], from: BigInt, until: BigInt)
   // these slices retain their original indices but preclude access outside of range
   {
     require(0 <= from && from <= until)
-      
+
     def valid: Boolean = { // this method aspires to become part of the above `require`
       reads(Set(a))
       until <= a.content.size
@@ -45,7 +57,7 @@ object ArraySegment {
     def apply(i: BigInt): T = {
       reads(Set(a))
       require(from <= i && i < until && valid)
-      
+
       a.content(i)
     }
 
@@ -53,9 +65,9 @@ object ArraySegment {
       reads(Set(a))
       require(from <= i && i < until && valid)
       modifies(Set(a))
-      
+
       a.content = a.content.updated(i, v)
-    }
+    } ensuring { _ => valid }
 
     def reSlice(from1: BigInt, until1: BigInt): ArraySlice[T] = {
       reads(Set(a))
@@ -70,15 +82,15 @@ object ArraySegment {
     println(s)
   }
 
-  @extern // remove the `@extern` for some hard VCs
+  // @extern // remove the `@extern` for some hard VCs
   def testSlices(a: SArray[String]): Unit = {
     reads(Set(a))
     modifies(Set(a))
     a.fill(5)("")
     a(3) = "s3"
+    a(4) = "s4"
     a(0) = "s0"
     a(2) = "s2"
-    a(4) = "s4"
     a(1) = "s1"
     val slice14 = ArraySlice(a, 1, 4)
     assert(slice14.valid)
@@ -87,7 +99,7 @@ object ArraySegment {
     slice14(2) = "42"
     pr("slice23(2) = " + slice23(2))
   }
-  
+
   @extern
   def main(args: Array[String]): Unit = {
     testSlices(SArray[String](List[String]()))
