8102b7da887d66b8da03b83e17c4bd1eb82f937e
Viktor Kuncak <viktor.kuncak@epfl.ch>
2021-01-18 22:31:11 +0100

    cell and lazy

---
+++ frontends/benchmarks/full-imperative/valid/EasyLazyCell.scala
@@ -0,0 +1,40 @@
+import stainless.lang._
+import stainless.collection._
+import stainless.lang.Option._
+import stainless.annotation._
+import stainless.proof._
+import stainless.lang.StaticChecks._
+
+object EasyLazyCell {
+  final case class Ref[T](var unref: T) extends AnyHeapRef {
+    def apply(): T = {
+      reads(Set(this))
+      unref
+    }
+  }
+
+  final case class Lazy[T](computation : Unit => T, var value: Option[T]) extends AnyHeapRef
+  {
+    def apply(): T = {
+      reads(Set(this))
+      modifies(Set(this))
+      value match {
+        case Some(v) => v
+        case None() => {
+          val v = computation(())
+          value = Some(v)
+          v
+        }
+      }
+    }
+  }
+  
+  @extern
+  def main(args: Array[String]): Unit = {
+    val v1 = Lazy[BigInt](((u:Unit) => {println("Hello before!"); BigInt(42)}),
+                          None[BigInt])
+    println("First me!")
+    println(v1() + v1() + v1())
+  }
+  
+}
