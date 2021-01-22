be36054690e4939644a0d9f6c8c9689ad8397489
Georg Stefan Schmid <georg.schmid@epfl.ch>
2021-01-22 15:39:55 +0100

    Remove BodyWithSpecs assertion. Fix and extend InheritanceTest example.

--- frontends/benchmarks/full-imperative/valid/InheritanceTest.scala
+++ frontends/benchmarks/full-imperative/valid/InheritanceTest.scala
@@ -5,36 +5,57 @@ import stainless.annotation._
 import stainless.lang.StaticChecks._
 
 object SimpleInheritanceTest {
-  abstract class Task {
+  @mutable abstract class Task {
     def region: Set[AnyHeapRef]
-    
-    def run: BigInt = {
+
+    def run: Unit = {
       reads(region)
       modifies(region)
-      42
+      ??? : Unit
     }
-    
+
+    def join: BigInt = {
+      reads(region)
+      ??? : BigInt
+    }
   }
 
   case object pureTask extends Task {
     def region = Set[AnyHeapRef]()
-    def run = {
-      42
+    override def run = ()
+    override def join = 42
+  }
+
+  case class IntBox(var value: BigInt) extends AnyHeapRef
+
+  case class SumTask(box: IntBox) extends Task {
+    def region: Set[AnyHeapRef] = Set[AnyHeapRef](box)
+
+    override def run = {
+      reads(region)
+      modifies(region)
+      box.value = box.value + box.value
+      ()
+    }
+
+    override def join: BigInt = {
+      reads(region)
+      box.value
     }
   }
-  
+
   @inline
   def disjoint(t1: Task, t2: Task): Boolean = {
       (t1.region & t2.region) == Set[AnyHeapRef]()
   }
 
+  @opaque
   def parallel(t1: Task, t2: Task): (BigInt, BigInt) = {
     require(disjoint(t1, t2))
     reads(t1.region ++ t2.region)
     modifies(t1.region ++ t2.region)
-    (t1.run, t2.run)
-  }
-  @extern
-  def main(args: Array[String]): Unit = {
-  }
+    t1.run
+    t2.run
+    (t1.join, t2.join)
+  } ensuring { res => res == (t1.join, t2.join) }
 }
