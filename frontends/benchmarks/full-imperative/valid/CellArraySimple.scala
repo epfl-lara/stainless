5b6cfce71a8d428ea3f66d94d157cc365c19b9d2
Viktor Kuncak <viktor.kuncak@epfl.ch>
2021-01-22 10:28:27 +0100

    tiny change to cell array

--- frontends/benchmarks/full-imperative/valid/CellArraySimple.scala
+++ frontends/benchmarks/full-imperative/valid/CellArraySimple.scala
@@ -4,34 +4,28 @@ import stainless.lang.Option._
 import stainless.annotation._
 import stainless.lang.StaticChecks._
 
-object CellArraySimpleExample {
+object LangCollection {
   final case class Ref[T](var unref: T) extends AnyHeapRef
 
-  def safeListLookup[T](l: List[Ref[T]], i: BigInt): Set[AnyHeapRef] = {
-    require(0 <= i && i < l.size)
-    Set[AnyHeapRef](l(i))
-  }
-
-  // def upcast[T](l: List[Ref[T]]): List[AnyHeapRef] = {
-  //   l match {
-  //     case Nil() => Nil[AnyHeapRef]()
-  //     case Cons(x, xs) => Cons[AnyHeapRef](x, upcast(xs))
-  //   }
-  // } ensuring (res => res.content == l.content)
+  // no longer necessary now that require is taken into account in `reads` and `modifies`
+  // def safeListLookup[T](l: List[Ref[T]], i: BigInt): Set[AnyHeapRef] = {
+  //   require(0 <= i && i < l.size)
+  //   Set[AnyHeapRef](l(i))
+  // }
 
   final case class CellArray[T](content: List[Ref[T]])
   {
-    def xapply(i: BigInt): Unit = {
-      reads(safeListLookup[T](content, i))
+    def apply(i: BigInt): Unit = {
       require(0 <= i && i < content.size)
+      reads(Set(content(i)))
       assert(safeListLookup(content, i) == Set(content(i)))
       content(i).unref
     }
 
     def update(i: BigInt, v: T): Unit = {
-      reads(safeListLookup[T](content, i))
-      modifies(safeListLookup[T](content, i))
       require(0 <= i && i < content.size)
+      reads(Set(content(i)))
+      modifies(safeListLookup[T](content, i))
       content(i).unref = v
     }
   }
