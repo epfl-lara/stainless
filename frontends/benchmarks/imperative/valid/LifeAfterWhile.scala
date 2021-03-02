f29415e8663cebfa24adfdeaaea969c8d654373e
Jad Hamza <jad.hamza@epfl.ch>
2021-03-02 14:58:25 +0100

    Add another @vkuncak example for return

---
+++ frontends/benchmarks/imperative/valid/LifeAfterWhile.scala
@@ -0,0 +1,17 @@
+import stainless.lang._
+
+object LifeAfterWhile {
+  def f(N: BigInt): BigInt = {
+    require(0 <= N)
+    var i: BigInt = 0
+    (while (i < N) {
+      decreases(N - i)
+      if (i == 42)
+        return N - 200
+      else
+        i = i + 1
+    }).invariant(0 <= i && i <= N)
+    i = i - 100
+    i
+  } ensuring (res => res <= N - 100)
+}
