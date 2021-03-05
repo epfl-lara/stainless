3b0f1a55c22e5bf4c8c6bb723a2a658b09c4de64
Jad Hamza <jad.hamza@epfl.ch>
2021-03-05 11:14:00 +0100

    Update return examples

--- frontends/benchmarks/imperative/valid/ReturnInWhile.scala
+++ frontends/benchmarks/imperative/valid/ReturnInWhile.scala
@@ -12,7 +12,21 @@ object ReturnInWhile {
     0
   }
 
-  def testReturn5: Unit = {
+  def returnN(n: Int): Int = {
+    require(n >= 0)
+    var i = 0
+    (while (true) {
+      decreases(n - i)
+      if (i == n) return i
+      i += 1
+    }).invariant(0 <= i && i <= n)
+
+    assert(false, "unreachable code")
+    0
+  }.ensuring((res: Int) => res == n)
+
+  def tests: Unit = {
     assert(return5 == 5)
+    assert(returnN(100) == 100)
   }
 }
