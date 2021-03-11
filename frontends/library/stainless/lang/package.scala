11a007124fa6127f5ce5a07c78e14768136b38ac
Jad Hamza <jad.hamza@epfl.ch>
2021-03-11 18:44:58 +0100

    Fix extraction of Array.updated

--- frontends/library/stainless/lang/package.scala
+++ frontends/library/stainless/lang/package.scala
@@ -145,4 +145,14 @@ package object lang {
 
   @library
   def specialize[T](call: T): T = call
+
+  @ignore @library
+  implicit class ArrayUpdating[T](a: Array[T]) {
+    def updated(index: Int, value: T): Array[T] = {
+      val res = a.clone
+      res(index) = value
+      res
+    }
+  }
+
 }
