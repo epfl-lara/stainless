d0790a5953e2c1082a8321db28c11e27ce28421e
Jad Hamza <jad.hamza@epfl.ch>
2021-03-15 11:17:47 +0100

    Uncomment AccessorAliasing benchmark

--- frontends/benchmarks/imperative/valid/AccessorAliasing.scala
+++ frontends/benchmarks/imperative/valid/AccessorAliasing.scala
@@ -3,12 +3,11 @@ trait B { var a: A }
 
 object Test {
   def test(b: B): Unit = {
-    // val a = b.a
+    val a = b.a
     val old = b.a.x
-    // a.x += 1
+    a.x += 1
     b.a.x += 1
-    // assert(b.a.x == old + 2)
-    assert(b.a.x == old + 1)
-    // assert(a == b.a)
+    assert(b.a.x == old + 2)
+    assert(a == b.a)
   }
 }
