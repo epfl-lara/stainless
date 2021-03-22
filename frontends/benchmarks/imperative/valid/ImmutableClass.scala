6c37961857bcee5c303e668c5f78f0eabfbf73d1
Jad Hamza <jad.hamza@epfl.ch>
2021-03-22 16:37:25 +0100

    Add benchmark where a class with a mutable type parameter is not considered mutable

---
+++ frontends/benchmarks/imperative/valid/ImmutableClass.scala
@@ -0,0 +1,11 @@
+import stainless.annotation._
+
+trait Immutable[@mutable T] {
+  def compare(t1: T, t2: T): Boolean
+}
+
+case class Box(var x: BigInt)
+
+case object ImmutableObject extends Immutable[Box] {
+  def compare(t1: Box, t2: Box): Boolean = true
+}
