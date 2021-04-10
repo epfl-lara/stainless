56f5a10a8a8c4e785422a1726ec69a56fd608d6b
Jad Hamza <jad.hamza@epfl.ch>
2021-04-10 07:47:06 +0200

    Make check return unit instead of boolean

--- frontends/library/stainless/collection/Queue.scala
+++ frontends/library/stainless/collection/Queue.scala
@@ -50,61 +50,64 @@ object Queue {
 object QueueSpecs {
   import ListSpecs._
 
-  def propEnqueue[A](queue: Queue[A], elem: A) : Boolean = {
+  def propEnqueue[A](queue: Queue[A], elem: A) : Unit = {
     import Queue._
 
     val (front, rear) = (queue.front, queue.rear)
 
-    (queue.enqueue(elem).asList == queue.asList ++ Cons(elem, Nil[A]())) because {
-      assert(queue.enqueue(elem) == amortizedQueue(front, Cons(elem, rear)))
+    assert(queue.enqueue(elem) == amortizedQueue(front, Cons(elem, rear)))
 
-      if (Cons(elem, rear).size <= front.size) {
-        assert(amortizedQueue(front, Cons(elem, rear)) == Queue(front, Cons(elem, rear)))
-        assert(Queue(front, Cons(elem, rear)).asList == front ++ Cons(elem, rear).reverse)
-        assert(front ++ Cons(elem, rear).reverse == front ++ (rear.reverse ++ Cons(elem, Nil[A]())))
-        assert(appendAssoc(front, rear.reverse, Cons(elem, Nil[A]())))
-        assert(front ++ (rear.reverse ++ Cons(elem, Nil[A]())) == (front ++ rear.reverse) ++ Cons(elem, Nil[A]()))
-        assert((front ++ rear.reverse) ++ Cons(elem, Nil[A]()) == queue.asList ++ Cons(elem, Nil[A]()))
+    if (Cons(elem, rear).size <= front.size) {
+      assert(amortizedQueue(front, Cons(elem, rear)) == Queue(front, Cons(elem, rear)))
+      assert(Queue(front, Cons(elem, rear)).asList == front ++ Cons(elem, rear).reverse)
+      assert(front ++ Cons(elem, rear).reverse == front ++ (rear.reverse ++ Cons(elem, Nil[A]())))
+      assert(appendAssoc(front, rear.reverse, Cons(elem, Nil[A]())))
+      assert(front ++ (rear.reverse ++ Cons(elem, Nil[A]())) == (front ++ rear.reverse) ++ Cons(elem, Nil[A]()))
+      assert((front ++ rear.reverse) ++ Cons(elem, Nil[A]()) == queue.asList ++ Cons(elem, Nil[A]()))
 
-        check(queue.enqueue(elem).asList == queue.asList ++ Cons(elem, Nil[A]()))
-      } else {
-        assert(amortizedQueue(front, Cons(elem, rear)) == Queue(front ++ Cons(elem, rear).reverse, Nil[A]()))
-        assert(Queue(front ++ Cons(elem, rear).reverse, Nil[A]()).asList == front ++ Cons(elem, rear).reverse ++ Nil[A]().reverse)
-        assert(front ++ Cons(elem, rear).reverse ++ Nil[A]().reverse == front ++ Cons(elem, rear).reverse ++ Nil[A]())
-        assert(front ++ Cons(elem, rear).reverse ++ Nil[A]() == front ++ Cons(elem, rear).reverse)
-        assert(front ++ Cons(elem, rear).reverse == front ++ (rear.reverse ++ Cons(elem, Nil[A]())))
-        assert(appendAssoc(front, rear.reverse, Cons(elem, Nil[A]())))
-        assert(front ++ (rear.reverse ++ Cons(elem, Nil[A]())) == (front ++ rear.reverse) ++ Cons(elem, Nil[A]()))
-        assert((front ++ rear.reverse) ++ Cons(elem, Nil[A]()) == queue.asList ++ Cons(elem, Nil[A]()))
+      check(queue.enqueue(elem).asList == queue.asList ++ Cons(elem, Nil[A]()))
+    } else {
+      assert(amortizedQueue(front, Cons(elem, rear)) == Queue(front ++ Cons(elem, rear).reverse, Nil[A]()))
+      assert(Queue(front ++ Cons(elem, rear).reverse, Nil[A]()).asList == front ++ Cons(elem, rear).reverse ++ Nil[A]().reverse)
+      assert(front ++ Cons(elem, rear).reverse ++ Nil[A]().reverse == front ++ Cons(elem, rear).reverse ++ Nil[A]())
+      assert(front ++ Cons(elem, rear).reverse ++ Nil[A]() == front ++ Cons(elem, rear).reverse)
+      assert(front ++ Cons(elem, rear).reverse == front ++ (rear.reverse ++ Cons(elem, Nil[A]())))
+      assert(appendAssoc(front, rear.reverse, Cons(elem, Nil[A]())))
+      assert(front ++ (rear.reverse ++ Cons(elem, Nil[A]())) == (front ++ rear.reverse) ++ Cons(elem, Nil[A]()))
+      assert((front ++ rear.reverse) ++ Cons(elem, Nil[A]()) == queue.asList ++ Cons(elem, Nil[A]()))
 
-        check(queue.enqueue(elem).asList == queue.asList ++ Cons(elem, Nil[A]()))
-      }
+      check(queue.enqueue(elem).asList == queue.asList ++ Cons(elem, Nil[A]()))
     }
-  }.holds
+  }.ensuring(_ =>
+    queue.enqueue(elem).asList == queue.asList ++ Cons(elem, Nil[A]())
+  )
 
-  def propFront[A](queue: Queue[A], elem: A) : Boolean = {
+  def propFront[A](queue: Queue[A], elem: A) : Unit = {
     require(!queue.isEmpty && queue.isAmortized)
     decreases(queue)
 
+    ()
+  }.ensuring(_ =>
     queue.asList.head == queue.head
-  }.holds
+  )
 
-  def propTail[A](queue: Queue[A], elem: A) : Boolean = {
+  def propTail[A](queue: Queue[A], elem: A) : Unit = {
     require(!queue.isEmpty && queue.isAmortized)
 
     check(rightUnitAppend(queue.front.tail ++ queue.rear.reverse))
 
+  }.ensuring(_ =>
     queue.asList.tail == queue.tail.asList
-  }.holds
+  )
 
-  def enqueueAndFront[A](queue: Queue[A], elem: A) : Boolean = {
+  def enqueueAndFront[A](queue: Queue[A], elem: A) : Unit = {
     if (queue.isEmpty)
-      queue.enqueue(elem).head == elem
+      assert(queue.enqueue(elem).head == elem)
     else
       true
-  }.holds
+  }
 
-  def enqueueDequeueThrice[A](queue: Queue[A], e1: A, e2: A, e3: A): Boolean = {
+  def enqueueDequeueThrice[A](queue: Queue[A], e1: A, e2: A, e3: A): Unit = {
     if (queue.isEmpty) {
       val q1 = queue.enqueue(e1)
       val q2 = q1.enqueue(e2)
@@ -114,8 +117,8 @@ object QueueSpecs {
       val e2prime = q4.head
       val q5 = q4.tail
       val e3prime = q5.head
-      e1 == e1prime && e2 == e2prime && e3 == e3prime
+      assert(e1 == e1prime && e2 == e2prime && e3 == e3prime)
     } else
       true
-  }.holds
+  }
 }
