0c7fe8bd8c6d711546f891066a3a0c957086a5c6
Jad Hamza <jad.hamza@epfl.ch>
2021-04-13 15:15:56 +0200

    Revert to long definition of isExpressionFresh and add more empty targets

--- core/src/main/scala/stainless/extraction/imperative/EffectsChecker.scala
+++ core/src/main/scala/stainless/extraction/imperative/EffectsChecker.scala
@@ -257,7 +257,53 @@ trait EffectsChecker { self: EffectsAnalyzer =>
      * It turns out that an expression of non-mutable type is always fresh,
      * as it can not contains reference to a mutable object, by definition
      */
-    def isExpressionFresh(expr: Expr): Boolean = getTargets(expr).isEmpty
+    def isExpressionFresh(expr: Expr): Boolean = {
+      def rec(expr: Expr, bindings: Set[ValDef]): Boolean = !isMutableType(expr.getType) || (expr match {
+        case v: Variable => bindings(v.toVal)
+        case ADT(_, _, args) => args.forall(rec(_, bindings))
+
+        case FiniteArray(elems, _) => elems.forall(rec(_, bindings))
+        case LargeArray(elems, default, _, _) => elems.forall(p => rec(p._2, bindings)) && rec(default, bindings)
+
+        // We assume `old(.)` is fresh here, although such cases will probably be
+        // rejected later in `ImperativeCleanup`.
+        case Old(_) => true
+
+        //function invocation from accessors are not fresh
+        case FunctionInvocation(id, _, _)
+          if symbols.getFunction(id).flags.exists(_.name == "accessor") => false
+
+
+        case fi @ FunctionInvocation(id, _, _) if !symbols.isRecursive(id) =>
+            BodyWithSpecs(symbols.simplifyLets(fi.inlined))
+              .bodyOpt
+              .forall(isExpressionFresh)
+
+        // other function invocations always return a fresh expression, by hypothesis (global assumption)
+        case (_: FunctionInvocation | _: ApplyLetRec | _: Application) => true
+
+        //ArrayUpdated returns a mutable array, which by definition is a clone of the original
+        case ArrayUpdated(IsTyped(_, ArrayType(base)), _, _) => !isMutableType(base)
+
+        //MutableMapDuplicate returns a fresh duplicate by definition
+        case MutableMapDuplicate(IsTyped(_, MutableMapType(from, to))) =>
+          !isMutableType(from) && !isMutableType(to)
+
+        // snapshots are fresh
+        case Snapshot(e) => true
+
+        case Let(vd, e, b) => rec(e, bindings) && rec(b, bindings + vd)
+        case LetVar(vd, e, b) => rec(e, bindings) && rec(b, bindings + vd)
+
+        case Block(_, e) => rec(e, bindings)
+
+        //any other expression is conservately assumed to be non-fresh if
+        //any sub-expression is non-fresh (i.e. an if-then-else with a reference in one branch)
+        case Operator(args, _) => args.forall(rec(_, bindings))
+      })
+
+      rec(expr, Set.empty)
+    }
 
     try {
       // We only check the bodies of functions which are not accessors
