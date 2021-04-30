96b3ff61c7406f7e98938e21571597096f985c6d
Yann Bolliger <yann.bolliger@epfl.ch>
2021-04-30 14:04:33 +0200

    Implement check as not well formed check.

--- core/src/main/scala/stainless/extraction/imperative/Definitions.scala
+++ core/src/main/scala/stainless/extraction/imperative/Definitions.scala
@@ -7,10 +7,23 @@ package imperative
 trait Definitions extends oo.Trees { self: Trees =>
   override type Symbols >: Null <: AbstractSymbols
 
-  trait AbstractSymbols
-    extends super.AbstractSymbols
-      with SymbolOps
-      with TypeOps { self0: Symbols =>
+  trait AbstractSymbols extends super.AbstractSymbols with SymbolOps with TypeOps {
+    self0: Symbols =>
+
+    override protected def ensureWellFormedFunction(fd: FunDef): Unit = {
+      exprOps.preTraversal {
+        case fa @ self.FieldAssignment(obj, field, _) =>
+          // if the field that is assigned is not '@var', throw
+          if (fa.getField(self0).exists(fieldVd => !fieldVd.flags.contains(IsVar))) {
+            throw NotWellFormedException(
+              fd,
+              Some(s"cannot assign to immutable field '${field.name}' of class '${obj.getType}'")
+            )
+          }
+        case _ => ()
+      }(fd.fullBody)
+
+      super.ensureWellFormedFunction(fd)
+    }
   }
 }
-
