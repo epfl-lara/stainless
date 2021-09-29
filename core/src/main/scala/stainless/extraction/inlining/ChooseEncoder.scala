e47ff2fb17e89c562a3b40737d9335f85337a1cd
Jad Hamza <jad.hamza@epfl.ch>
2021-09-29 10:29:18 +0200

    Fix ChooseEncoder ignoring some parts of the code

--- core/src/main/scala/stainless/extraction/inlining/ChooseEncoder.scala
+++ core/src/main/scala/stainless/extraction/inlining/ChooseEncoder.scala
@@ -8,61 +8,92 @@ trait ChooseEncoder extends CachingPhase with SimplyCachedFunctions with Identit
 
   implicit val context: inox.Context
   val s: Trees
-  val t: s.type = s
-  import s._
+  val t: Trees
 
-  type TransformerContext = Symbols
-  override def getContext(symbols: Symbols) = symbols
+  type TransformerContext = s.Symbols
+  override def getContext(symbols: s.Symbols) = symbols
 
-  override protected type FunctionResult = Seq[FunDef]
-  override protected def registerFunctions(symbols: Symbols, functions: Seq[Seq[FunDef]]): Symbols =
+  override protected type FunctionResult = Seq[t.FunDef]
+  override protected def registerFunctions(symbols: t.Symbols, functions: Seq[Seq[t.FunDef]]): t.Symbols =
     symbols.withFunctions(functions.flatten)
 
-  protected def extractFunction(context: TransformerContext, fd: FunDef): Seq[FunDef] = {
-    var fdChooses = Seq[FunDef]()
+  protected def extractFunction(context: TransformerContext, fd: s.FunDef): Seq[t.FunDef] = {
+    var fdChooses = Seq[t.FunDef]()
 
-    def rec(e: Expr, params: Seq[ValDef]): Expr = e match {
-      case l: Let => l.copy(body = rec(l.body, params :+ l.vd)).copiedFrom(l)
+    object ce extends inox.transformers.Transformer {
+      override val s: self.s.type = self.s
+      override val t: self.t.type = self.t
 
-      case l: Lambda => l.copy(body = rec(l.body, params ++ l.params)).copiedFrom(l)
+      type Env = Seq[t.ValDef]
 
-      case c @ Choose(vd, pred) =>
-        val (substMap, freshParams) = params.foldLeft((Map[ValDef, Expr](), Seq[ValDef]())) {
-          case ((substMap, vds), vd) =>
-            val ntpe = typeOps.replaceFromSymbols(substMap, vd.tpe)
-            val nvd = ValDef(vd.id.freshen, ntpe, vd.flags).copiedFrom(vd)
-            (substMap + (vd -> nvd.toVariable), vds :+ nvd)
-        }
+      import t._ // for implicit `VariableConverter`, required for `replaceFromSymbols`
 
-        val returnType = typeOps.replaceFromSymbols(substMap, vd.tpe)
-        val newVd = vd.copy(tpe = returnType).setPos(vd)
-        val newPred = exprOps.replaceFromSymbols(substMap, rec(pred, params :+ vd))
-        val newChoose = Choose(newVd, newPred).copiedFrom(c)
-
-        val newFd = new FunDef(
-          FreshIdentifier("choose", true), fd.tparams, freshParams,
-          returnType,
-          newChoose,
-          Seq(Derived(Some(fd.id)), DropVCs),
+      def transform(fd: s.FunDef): t.FunDef = {
+        val params2 = fd.params.map(transform(_, Seq()))
+        new t.FunDef(
+          fd.id,
+          fd.tparams.map(transform(_, Seq())),
+          params2,
+          transform(fd.returnType, params2),
+          transform(fd.fullBody, params2),
+          fd.flags.map(transform(_, Seq()))
         ).setPos(fd)
+      }
 
-        fdChooses = fdChooses :+ newFd
+      override def transform(e: s.Expr, env: Seq[t.ValDef]): t.Expr = e match {
+        case s.Let(vd, expr, body) =>
+          val newVd = transform(vd, env)
+          t.Let(newVd, transform(expr, env),
+            transform(body, env :+ newVd)
+          ).copiedFrom(e)
 
-        val fi = FunctionInvocation(newFd.id, newFd.tparams.map(_.tp), params.map(_.toVariable)).copiedFrom(c)
-        if (vd.flags.contains(DropVCs))
-          fi
-        else
-          Assert(
-            Not(Forall(Seq(vd), Not(pred).setPos(c)).setPos(c)).setPos(c),
-            Some("Choose satisfiability"),
+        case s.Lambda(params, body) =>
+          val (newParams, newEnv) = params.foldLeft((Seq[t.ValDef](), env)) {
+            case ((accParams, accEnv), vd) =>
+              val newVd = transform(vd, accEnv)
+              (accParams :+ newVd, accEnv :+ newVd)
+          }
+          t.Lambda(newParams, transform(body, newEnv)).copiedFrom(e)
+
+        case c @ s.Choose(vd, pred) =>
+          val (substMap, freshParams) = env.foldLeft((Map[t.ValDef, t.Expr](), Seq[t.ValDef]())) {
+            case ((substMap, vds), vd) =>
+              val ntpe = t.typeOps.replaceFromSymbols(substMap, vd.tpe)
+              val nvd = t.ValDef(vd.id.freshen, ntpe, vd.flags).copiedFrom(vd)
+              (substMap + (vd -> nvd.toVariable), vds :+ nvd)
+          }
+
+          val returnType = t.typeOps.replaceFromSymbols(substMap, transform(vd.tpe, env))
+          val newVd = transform(vd, env).copy(tpe = returnType).setPos(vd)
+          val newPred = exprOps.replaceFromSymbols(substMap, transform(pred, env :+ newVd))
+          val newChoose = Choose(newVd, newPred).copiedFrom(c)
+
+          val newFd = new t.FunDef(
+            FreshIdentifier("choose", true), fd.tparams.map(transform(_, env)), freshParams,
+            returnType,
+            newChoose,
+            Seq(t.Derived(Some(fd.id)), t.DropVCs),
+          ).setPos(fd)
+
+          fdChooses = fdChooses :+ newFd
+
+          val fi = t.FunctionInvocation(newFd.id, newFd.tparams.map(_.tp), env.map(_.toVariable)).copiedFrom(c)
+          if (vd.flags.contains(s.DropVCs))
             fi
-          ).setPos(c)
+          else
+            t.Assert(
+              t.Not(t.Forall(Seq(transform(vd, env)), t.Not(transform(pred, env)).setPos(c)).setPos(c)).setPos(c),
+              Some("Choose satisfiability"),
+              fi
+            ).setPos(c)
 
-      case Operator(es, recons) => recons(es.map(rec(_, params))).copiedFrom(e)
+        case _ => super.transform(e, env)
+      }
     }
 
+
     // bind `newFd` before returning so that `fdChooses` gets filled with the new `choose` functions
-    val newFd = fd.copy(fullBody = rec(fd.fullBody, fd.params)).setPos(fd)
+    val newFd = ce.transform(fd)
     fdChooses :+ newFd
   }
 
