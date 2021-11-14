171aec499f3cb49fad1eda1b5a1ddef25d1378fd
Viktor Kuncak <viktor.kuncak@epfl.ch>
2021-11-14 22:52:24 +0100

    Examples with dynamic dispatch violating naive substitution principle

--- frontends/benchmarks/verification/invalid/LawsExampleInvalid.scala
+++ frontends/benchmarks/verification/invalid/LawsExampleInvalid.scala
@@ -4,24 +4,25 @@
 import stainless.lang._
 import stainless.annotation._
 
-abstract class A[T] {
-  def a: T
-  def f(x: T, y: T): T
+object LawsExampleInvalid {
+  abstract class A[T] {
+    def a: T
+    def f(x: T, y: T): T
 
-  @law
-  def uniqueRight(x: T, y: T, z: T): Boolean = {
-    f(x,y) != f(x,z) || y == z
+    @law
+    def uniqueRight(x: T, y: T, z: T): Boolean = {
+      f(x,y) != f(x,z) || y == z
+    }
+
+    def something(x: T, y: T): Boolean = {
+      require(f(x,y) == f(x,a))
+      uniqueRight(x,y,a)
+      y == this.a
+    }.holds
+
+    def somethingelse(y: T): Unit = {
+      require(f(a,a) == f(a,y))
+      assert(something(a,y))
+    }.ensuring(y == a)
   }
-
-  def something(x: T, y: T): Boolean = {
-    require(f(x,y) == f(x,a))
-    uniqueRight(x,y,a)
-    y == this.a
-  }.holds
-
-  def somethingelse(y: T): Unit = {
-    require(f(a,a) == f(a,y))
-    assert(something(a,y))
-  }.ensuring(y == a)  
 }
-
