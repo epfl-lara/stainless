47bdfde655cc959ece28cb4f36e23de08cedb80e
Viktor Kuncak <viktor.kuncak@epfl.ch>
2021-11-14 22:52:24 +0100

    Examples with dynamic dispatch violating naive substitution principle

--- frontends/benchmarks/verification/valid/LawsExample.scala
+++ frontends/benchmarks/verification/valid/LawsExample.scala
@@ -3,25 +3,27 @@
 import stainless.lang._
 import stainless.annotation._
 
-abstract class A[T] {
-  def a: T
-  def f(x: T, y: T): T
+object LawsExample {
+  abstract class A[T] {
+    def a: T
+    def f(x: T, y: T): T
 
-  @law
-  def uniqueRight(x: T, y: T, z: T): Boolean = {
-    f(x,y) != f(x,z) || y == z
+    @law
+    def uniqueRight(x: T, y: T, z: T): Boolean = {
+      f(x,y) != f(x,z) || y == z
+    }
   }
-}
 
-object Aux {
-  def something[T](ceci: A[T], x: T, y: T): Boolean = {
-    require(ceci.f(x,y) == ceci.f(x,ceci.a))
-    ceci.uniqueRight(x,y,ceci.a)
-    y == ceci.a
-  }.holds
+  object Aux {
+    def something[T](ceci: A[T], x: T, y: T): Boolean = {
+      require(ceci.f(x,y) == ceci.f(x,ceci.a))
+      ceci.uniqueRight(x,y,ceci.a)
+      y == ceci.a
+    }.holds
 
-  def somethingelse[T](ceci: A[T], y: T): Unit = {
-    require(ceci.f(ceci.a,ceci.a) == ceci.f(ceci.a,y))
-    assert(something(ceci, ceci.a,y))
-  }.ensuring(y == ceci.a)  
+    def somethingelse[T](ceci: A[T], y: T): Unit = {
+      require(ceci.f(ceci.a,ceci.a) == ceci.f(ceci.a,y))
+      assert(something(ceci, ceci.a,y))
+    }.ensuring(y == ceci.a)
+  }
 }
