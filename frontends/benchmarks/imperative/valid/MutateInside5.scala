9ac63b04b095de284be457770ff47a103a4f5f28
Mario Bucev <mario.bucev@epfl.ch>
2022-04-27 10:07:27 +0200

    Applying suggestions

---
+++ frontends/benchmarks/imperative/valid/MutateInside5.scala
@@ -0,0 +1,21 @@
+import stainless.annotation._
+
+object MutateInside5 {
+  case class Mut[@mutable T](var t: T)
+  case class Thing(var field: Int)
+
+  def resetThing(m: Mut[Thing]): Unit = {
+    m.t = Thing(0)
+  }
+
+  def main(): Unit = {
+    val thing = Thing(123)
+
+    {
+      val m = Mut(thing)
+      resetThing(m)
+    }
+
+    assert(thing.field == 123)
+  }
+}
