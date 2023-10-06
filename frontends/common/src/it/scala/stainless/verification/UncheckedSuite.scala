70aea19f8430cb24df20a952ec9a2d3b9aef2c3b
Mario Bucev <mario.bucev@epfl.ch>
2023-10-06 16:25:59 +0200

    Add support for smt-cvc5

--- frontends/common/src/it/scala/stainless/verification/UncheckedSuite.scala
+++ frontends/common/src/it/scala/stainless/verification/UncheckedSuite.scala
@@ -5,26 +5,22 @@ package verification
 
 import org.scalatest._
 
-trait UncheckedSuite extends VerificationComponentTestSuite {
+class UncheckedSuite extends VerificationComponentTestSuite {
+  private val solvers = Seq("smt-z3", "smt-cvc4", "smt-cvc5")
 
-  override def configurations = super.configurations.map {
-    seq => Seq(optFailEarly(true), inox.solvers.optCheckModels(false)) ++ seq
+  override def configurations: Seq[Seq[inox.OptionValue[_]]] = {
+    solvers.flatMap { solver =>
+      super.configurations.map {
+        seq => Seq(
+          inox.optSelectedSolvers(Set(solver)),
+          optFailEarly(true),
+          inox.solvers.optCheckModels(false)) ++ seq
+      }
+    }
   }
 
   override val component: VerificationComponent.type = VerificationComponent
 
   testUncheckedAll("verification/unchecked-invalid")
   testUncheckedAll("verification/unchecked-valid")
-}
-
-class SMTZ3UncheckedSuite extends UncheckedSuite {
-  override def configurations = super.configurations.map {
-    seq => inox.optSelectedSolvers(Set("smt-z3:z3-4.8.12")) +: seq
-  }
-}
-
-class SMTCVC4UncheckedSuite extends UncheckedSuite {
-  override def configurations = super.configurations.map {
-    seq => inox.optSelectedSolvers(Set("smt-cvc4")) +: seq
-  }
 }
