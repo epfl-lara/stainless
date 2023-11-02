5ad0b33443a8c79671f30c84689d18d0cae25c5b
Mario Bucev <mario.bucev@epfl.ch>
2023-11-02 14:39:27 +0100

    Relax isExpressionFresh and improve aliasing error messages

--- frontends/benchmarks/extraction/invalid/InnerClassesInvariants2.scala
+++ frontends/benchmarks/extraction/invalid/InnerClassesInvariants2.scala
@@ -3,18 +3,18 @@ object InnerClassesInvariants1 {
   case class Ref(var value: BigInt)
 
   def rejected(x: Ref): Unit = {
-    require(0 < ref.value && ref.value < 10)
+    require(0 < x.value && x.value < 10)
     var y = x
     val z = y
 
     case class Local() {
       def smth: Unit = {
         assert(0 < x.value && x.value < 10)
-        assert(0 < y.value && y.value < 10)
+        assert(0 < y.value && y.value < 10)  // Local classes cannot close over mutable variables
         assert(0 < z.value && z.value < 10)
       }
     }
     val local = Local()
-    y.value = 42 // Illegal aliasing
+    y.value = 42
   }
 }
