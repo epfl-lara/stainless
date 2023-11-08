59347490326664fd406be0433036bcc81acdc71a
Mario Bucev <mario.bucev@epfl.ch>
2023-11-08 16:34:13 +0100

    Disable test i1306b

---
+++ frontends/benchmarks/imperative/valid/i1306c.scala
@@ -0,0 +1,43 @@
+import stainless._
+import stainless.lang._
+import stainless.annotation._
+
+object i1306c {
+  // Like coal power plants, lot of energy lost for little gain...
+  def laboriousIdentity1(a: BigInt): BigInt = {
+    require(a >= 0)
+    var i: BigInt = 0
+    def nextIncrement = i + 1
+    def accessI(x: BigInt, y: BigInt) = i + x + y
+    def modifyI(x: BigInt, y: BigInt) = { i += x + y }
+    (while (nextIncrement <= a) {
+      decreases(a - nextIncrement)
+      val z = accessI(a, nextIncrement)
+      assert(z == i + a + i + 1)
+      val w = accessI(a, i)
+      modifyI(a, -a)
+      assert(w == i + a + i)
+      i = i + 1
+    }) invariant { i >= 0 && i <= a }
+    i
+  } ensuring { _ == a }
+
+  // Variant that increments `i` with `modifyI`
+  def laboriousIdentity2(a: BigInt): BigInt = {
+    require(a >= 0)
+    var i: BigInt = 0
+    def nextIncrement = i + 1
+    def accessI(x: BigInt, y: BigInt) = i + x + y
+    def modifyI(x: BigInt, y: BigInt) = { i += x + y }
+    (while (nextIncrement <= a) {
+      decreases(a - nextIncrement)
+      val z = accessI(a, nextIncrement)
+      assert(z == i + a + i + 1)
+      val w = accessI(a, i)
+      modifyI(a, -a)
+      assert(w == i + a + i)
+      modifyI(0, 1)
+    }) invariant { i >= 0 && i <= a }
+    i
+  } ensuring { _ == a }
+}
--- frontends/common/src/it/scala/stainless/verification/ImperativeSuite.scala
+++ frontends/common/src/it/scala/stainless/verification/ImperativeSuite.scala
@@ -12,6 +12,8 @@ class ImperativeSuite extends VerificationComponentTestSuite {
   override def filter(ctx: inox.Context, name: String): FilterStatus = name match {
     // Unstable on 4.8.12, but works in non-incremental mode
     case "imperative/valid/WhileAsFun2" => Ignore
+    // Succeeds most of the time, but unsuitable for CI due to its flakiness
+    case "imperative/valid/i1306b" => Ignore
 
     case _ => super.filter(ctx, name)
   }
