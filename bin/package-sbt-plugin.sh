1e20ac50965dd6b99f8ffd38d113277e75a9f37f
Samuel Chassot <samuel.chassot@epfl.ch>
2024-05-02 11:46:02 +0200

    update scripts

--- bin/package-sbt-plugin.sh
+++ bin/package-sbt-plugin.sh
@@ -8,7 +8,10 @@ fi
 
 SCALA_VERSION="3.3.3"
 SCALA_LIB_VERSION="3.3.3"
+<<<<<<< HEAD
 PUBLISHED_SBT_PLUGIN_DIR="$HOME/.ivy2/local/ch.epfl.lara/sbt-stainless/scala_2.12/sbt_1.0/$STAINLESS_VERSION"
+=======
+>>>>>>> 1f8fccb5 (update scripts)
 LIB_SCALA_VERSION_JAR_NAME_PART=$(echo $SCALA_LIB_VERSION | cut -d '.' -f 1)
 PUBLISHED_LIB_DIR="$HOME/.ivy2/local/ch.epfl.lara/stainless-library_$LIB_SCALA_VERSION_JAR_NAME_PART/$STAINLESS_VERSION"
 PUBLISHED_DOTTY_DIR="$HOME/.ivy2/local/ch.epfl.lara/stainless-dotty-plugin_$SCALA_VERSION/$STAINLESS_VERSION"
@@ -40,9 +43,12 @@ info "$(tput bold)[] Locally publishing artifacts..."
 sbt publishLocal >> $LOG 2>&1 && okay || fail
 
 info "$(tput bold)[] Preparing SBT plugin jar..."
+<<<<<<< HEAD
 OUT_SBT_JAR_DIR="$OUTPUT/project/lib"
 mkdir -p "$OUT_SBT_JAR_DIR"
 cp "$PUBLISHED_SBT_PLUGIN_DIR/jars/sbt-stainless.jar" "$OUT_SBT_JAR_DIR/sbt-stainless.jar"
+=======
+>>>>>>> 1f8fccb5 (update scripts)
 OUT_DOTTY_DIR="$OUTPUT/stainless/ch/epfl/lara/stainless-dotty-plugin_$SCALA_VERSION/$STAINLESS_VERSION"
 mkdir -p "$OUT_DOTTY_DIR"
 cp "$PUBLISHED_DOTTY_DIR/jars/stainless-dotty-plugin_$SCALA_VERSION.jar" "$OUT_DOTTY_DIR/stainless-dotty-plugin_$SCALA_VERSION-$STAINLESS_VERSION.jar"
