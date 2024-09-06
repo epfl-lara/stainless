2fc16b6a5b8e3f27fa5f2b1b33412e0f8c183e40
Samuel Chassot <14821693+samuelchassot@users.noreply.github.com>
2024-09-06 11:10:15 +0200

    Few changes Matt suggested to CI (#1575)
    * few changes Matt suggested
    
    * rename nightly

--- stainless-ci.sh
+++ stainless-ci.sh
@@ -79,7 +79,7 @@ if [ -n "$SOLVERS_DIR" ]; then
   mkdir -p "$SOLVERS_DIR"
   mkdir -p "$TEMP_DIR"
   # cvc5
-  wget https://github.com/cvc5/cvc5/releases/download/cvc5-1.1.2/cvc5-Linux-static.zip -O "$TEMP_DIR/downloaded.zip"
+  wget https://github.com/cvc5/cvc5/releases/download/cvc5-1.1.2/cvc5-Linux-static.zip -O "$TEMP_DIR/downloaded.zip" -q
   unzip "$TEMP_DIR/downloaded.zip" -d "$TEMP_DIR"
   CVC5_DIR=$(ls "$TEMP_DIR" | grep cvc5)
   mv "$TEMP_DIR/$CVC5_DIR/bin/cvc5" "$SOLVERS_DIR/cvc5"
@@ -87,12 +87,12 @@ if [ -n "$SOLVERS_DIR" ]; then
   rm -rf "$TEMP_DIR"
   
   # CVC4
-  wget https://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/cvc4-1.8-x86_64-linux-opt -O "$SOLVERS_DIR/cvc4"
+  wget https://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/cvc4-1.8-x86_64-linux-opt -O "$SOLVERS_DIR/cvc4" -q
   chmod +x "$SOLVERS_DIR/cvc4"
 
   # z3
   mkdir -p "$TEMP_DIR"
-  wget https://github.com/Z3Prover/z3/releases/download/z3-4.13.0/z3-4.13.0-x64-glibc-2.35.zip -O "$TEMP_DIR/downloaded.zip"
+  wget https://github.com/Z3Prover/z3/releases/download/z3-4.13.0/z3-4.13.0-x64-glibc-2.35.zip -O "$TEMP_DIR/downloaded.zip" -q
   unzip "$TEMP_DIR/downloaded.zip" -d "$TEMP_DIR"
   Z3_DIR=$(ls "$TEMP_DIR" | grep z3)
   mv "$TEMP_DIR/$Z3_DIR/bin/z3" "$SOLVERS_DIR/z3"
