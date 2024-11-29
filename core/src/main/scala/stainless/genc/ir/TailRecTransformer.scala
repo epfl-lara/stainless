a2e51b4a9326285e13b2f7b400d50ee1550b7133
Kacper Korban <kacper.f.korban@gmail.com>
2024-11-29 11:14:44 +0100

    Fix tailrec phase for genc; fib works now

--- core/src/main/scala/stainless/genc/ir/TailRecTransformer.scala
+++ core/src/main/scala/stainless/genc/ir/TailRecTransformer.scala
@@ -14,7 +14,6 @@ final class TailRecTransformer(val ctx: inox.Context) extends Transformer(SIR, T
   /**
    * //TODO
    * Check:
-   * - use FreshIdentifier to create new identifiers
    * - no continue? replace with sth? do we even need it?
    */
 
@@ -22,14 +21,14 @@ final class TailRecTransformer(val ctx: inox.Context) extends Transformer(SIR, T
 
   private def isTailRecursive(fd: FunDef): Boolean = {
     var functionRefs = mutable.ListBuffer.empty[FunDef]
-    val functionRefVisitor = new ir.Visitor(SIR) {
+    val functionRefVisitor = new ir.Visitor(from) {
       override protected def visit(expr: Expr): Unit = expr match {
         case FunVal(fd) => functionRefs += fd
         case _ =>
       }
     }
     var tailFunctionRefs = mutable.ListBuffer.empty[FunDef]
-    val tailRecCallVisitor = new ir.Visitor(SIR) {
+    val tailRecCallVisitor = new ir.Visitor(from) {
       override protected def visit(expr: Expr): Unit = expr match {
         case Return(App(FunVal(fdcall), _, _)) => tailFunctionRefs += fdcall
         case _ =>
@@ -77,21 +76,21 @@ final class TailRecTransformer(val ctx: inox.Context) extends Transformer(SIR, T
   */
   private def rewriteToAWhileLoop(fd: FunDef): FunDef = fd.body match {
     case FunBodyAST(body) =>
-      val newParams = fd.params.map(p => ValDef(p.id + "$", p.typ, isVar = true))
+      val newParams = fd.params.map(p => ValDef(freshId(p.id), p.typ, isVar = true))
       val newParamMap = fd.params.zip(newParams).toMap
       val bodyWithNewParams = replaceBindings(newParamMap, body)
       val declarations = newParamMap.toList.map { case (old, nw) => Decl(nw, Some(Binding(old))) }
       val newBody = replaceRecursiveCalls(fd, bodyWithNewParams, newParams.toList)
-      val newBodyWIthAWhileLoop = Block(declarations :+ While(True, newBody))
-      FunDef(fd.id, fd.returnType, fd.ctx, newParams, FunBodyAST(Block(declarations :+ newBodyWIthAWhileLoop)), fd.isExported, fd.isPure)
+      val newBodyWIthAWhileLoop = While(True, newBody)
+      FunDef(fd.id, fd.returnType, fd.ctx, fd.params, FunBodyAST(Block(declarations :+ newBodyWIthAWhileLoop)), fd.isExported, fd.isPure)
     case _ => fd
   }
 
   private def replaceRecursiveCalls(fd: FunDef, body: Expr, valdefs: List[ValDef]): Expr = {
-    val replacer = new Transformer(SIR, SIR) with NoEnv {
+    val replacer = new Transformer(from, from) with NoEnv {
       override def rec(e: Expr)(using Env): Expr = e match {
         case Return(App(FunVal(fdcall), _, args)) if fdcall == fd =>
-          val tmpValDefs = valdefs.map(vd => ValDef(vd.id + "$very_temporary_pls_dont conflict", vd.typ, isVar = false))
+          val tmpValDefs = valdefs.map(vd => ValDef(freshId(vd.id), vd.typ, isVar = false))
           val tmpDecls = tmpValDefs.zip(args).map { case (vd, arg) => Decl(vd, Some(arg)) }
           val valdefAssign = valdefs.zip(tmpValDefs).map { case (vd, tmp) => Assign(Binding(vd), Binding(tmp)) }
           Block(tmpDecls ++ valdefAssign/*  :+ Continue() */)
@@ -103,19 +102,25 @@ final class TailRecTransformer(val ctx: inox.Context) extends Transformer(SIR, T
 
   /* Replace the bindings in the function body with the mapped variables */
   private def replaceBindings(mapping: Map[ValDef, ValDef], funBody: Expr): Expr = {
-    val replacer = new Transformer(SIR, SIR) with NoEnv {
+    val replacer = new Transformer(from, from) with NoEnv {
       override protected def rec(vd: ValDef)(using Env): to.ValDef =
         mapping.getOrElse(vd, vd)
     }
     replacer(funBody)
   }
 
-  override protected def recImpl(fd: SIR.FunDef)(using Unit): to.FunDef = {
-    if isTailRecursive(fd) then
-      val newFd = rewriteToAWhileLoop(fd).asInstanceOf[TIR.FunDef]
-      pprint.pprintln(newFd)
-      newFd
-    else
-      fd.asInstanceOf[TIR.FunDef]
+  override protected def recImpl(fd: from.FunDef)(using Unit): to.FunDef = {
+    super.recImpl{
+      if isTailRecursive(fd) then
+        val newFd = rewriteToAWhileLoop(fd)
+        pprint.pprintln(newFd)
+        newFd
+      else
+        fd
+    }
   }
+
+  private def freshId(id: String): to.Id = id + "_" + freshCounter.next(id)
+
+  private val freshCounter = new utils.UniqueCounter[String]()
 }
