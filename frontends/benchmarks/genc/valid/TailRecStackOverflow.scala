1883c7c6b167073c83b891a398ed44c754dc9a99
Zhekai Jiang <zhekai.jiang@mail.mcgill.ca>
2024-12-12 16:57:00 +0100

    Add a test that runs into stack overflow (segmentation fault) without tail recursion elimination

---
+++ frontends/benchmarks/genc/valid/TailRecStackOverflow.scala
@@ -0,0 +1,16 @@
+import stainless.annotation._
+import stainless.lang._
+import stainless.io._
+
+object TailRecStackOverflow {
+  def even(n: Int): Int =
+    if n == 0 then 1
+    else if n == 1 then 0
+    else even(n - 2)
+
+  @cCode.`export`
+  def main(): Unit = {
+    implicit val state = stainless.io.newState
+    StdOut.println(even(1000000)) // Expected: 1
+  }
+}
