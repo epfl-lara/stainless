3f77a044bfb10aca445bf6e03e72f7f17670f73f
Kacper Korban <kacper.f.korban@gmail.com>
2025-01-04 13:08:59 +0100

    Add one more Unit return test

---
+++ frontends/benchmarks/genc/valid/TailRecUnitIf.scala
@@ -0,0 +1,15 @@
+import stainless.annotation._
+import stainless.lang._
+import stainless.io._
+
+object TailRecUnit {
+  def countDown(n: Int): Unit =
+    if (n == 0) return
+    else countDown(n - 1)
+
+  @cCode.`export`
+  def main(): Unit = {
+    implicit val state = stainless.io.newState
+    countDown(1000000)
+  }
+}
