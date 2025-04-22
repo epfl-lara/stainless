1a6766699e592f0ba5739fe50d9f7f6e39415f46
Axel Bergström <axel.bergstrom@it.uu.se>
2025-04-22 16:14:37 +0200

    fix invalid floating-point verification test cases (before this commit, only some of the intended properties were tested)

--- frontends/benchmarks/verification/invalid/FloatingPointFunctionsContradiction.scala
+++ frontends/benchmarks/verification/invalid/FloatingPointFunctionsContradiction.scala
@@ -1,15 +1,17 @@
 import stainless.math.*
-import stainless.lang.*
 
 // since some properties of transcendental functions are axiomatized, we check that false is not implied
 object FloatingPointFunctionsContradiction {
-  def testSin(d: Double) = { sin(d) }.ensuring(false)
-  def testAsin(d: Double) = { asin(d) }.ensuring(false)
-  def testCos(d: Double) = { cos(d) }.ensuring(false)
-  def testAcos(d: Double) = { acos(d) }.ensuring(false)
-  def testTan(d: Double) = { tan(d) }.ensuring(false)
-  def testAtan(d: Double) = { atan(d) }.ensuring(false)
-  def testPow(d1: Double, d2: Double) = { pow(d1, d2) }.ensuring(false)
-  def testExp(d: Double) = { exp(d) }.ensuring(false)
-  def testAtan2(d1: Double, d2: Double) = { atan2(d1, d2) }.ensuring(false)
+  def test(d1: Double, d2: Double,  d3: Double, d4: Double, d5: Double, d6: Double, d7: Double, d8: Double, d9: Double, d10: Double, d11: Double): Unit = {
+    sin(d1)
+    asin(d2)
+    cos(d3)
+    acos(d4)
+    tan(d5)
+    atan(d6)
+    pow(d7, d8)
+    exp(d9)
+    atan2(d10, d11)
+    ()
+  }.ensuring(false)
 }
