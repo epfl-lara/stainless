d06c0d850b59ab6372a2779582bbb727c092773c
Matt Bovel <matthieu@bovel.net>
2025-12-09 13:11:28 +0000

    Add more `ClassTag` tests

--- frontends/benchmarks/extraction/valid/ClassTagBasics.scala
+++ frontends/benchmarks/extraction/valid/ClassTagBasics.scala
@@ -5,6 +5,15 @@ import scala.reflect.ClassTag
   */
 object ClassTagBasics {
   def id[T: ClassTag](x: T): T = x
+  def id2[T](x: T)(using ev: ClassTag[T]): T = x
+  def id3[T](x: T)(ev: ClassTag[T]): T = x
+  def id4[T](x: T, ev: ClassTag[T]): T = x
 
-  def Test = id(42)
+  def Test =
+    id(42)
+    id(42)(using ClassTag.Int)
+    id2(42)
+    id2(42)(using ClassTag.Int)
+    id3(42)(ClassTag.Int)
+    id4(42, ClassTag.Int)
 }
