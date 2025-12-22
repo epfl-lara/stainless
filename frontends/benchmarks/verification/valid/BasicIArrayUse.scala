cf59d817b28c55bc8c74ae100a67af79b7ffc66e
Samuel Chassot <samuel.chassot@epfl.ch>
2025-12-22 18:38:44 +0100

    typo

--- frontends/benchmarks/verification/valid/BasicIArrayUse.scala
+++ frontends/benchmarks/verification/valid/BasicIArrayUse.scala
@@ -1,7 +1,7 @@
 import stainless.collection._
 
 object BasicIArrayUse:
-  def head[A](arr: IArray[A]): BigInt = {
+  def head[A](arr: IArray[A]): A = {
     require(arr.list.size > 0)
     arr(0)
   }
