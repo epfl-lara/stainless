c4f372290b9289addfccbb4ce383c330e2f49434
Katarzyna Marek <kasia@marek.net>
2026-03-20 16:24:44 +0100

    fix examples after dotty bump
    We need more explicit types due to argument hoisting.

--- frontends/benchmarks/refinement/valid/Vec.scala
+++ frontends/benchmarks/refinement/valid/Vec.scala
@@ -2,8 +2,11 @@ import stainless.annotation.ignore
 import scala.annotation.nowarn
 import stainless.lang._
 
+// using a type alias instead of inlining the type makes stainless sad :(
+// type Pos = {v: BigInt with v >= 0}
+
 sealed trait Vec[T]:
-  def len: { r: BigInt with r >= 0 } = this match
+  def len: {v: BigInt with v >= 0} = this match
     case Nil() => (0: BigInt).ck
     case Cons(_, tail) => (1 + tail.len).ck
 
@@ -11,7 +14,7 @@ sealed trait Vec[T]:
     ((this, b) : @nowarn) match
       case (Nil(), Nil()) => Nil[(T, S)]().ck
       case (Cons(head, tail), Cons(bHead, bTail)) => 
-        Cons((head, bHead), tail.zip(bTail.ck)).ck
+        Cons((head, bHead), tail.zip(bTail.ck[{b: Vec[S] with b.len == tail.len}])).ck
 
   def concat(b: Vec[T]): {r: Vec[T] with r.len == len + b.len } =
     this match
@@ -22,9 +25,9 @@ case class Nil[T]() extends Vec[T]
 case class Cons[T](head: T, tail: Vec[T]) extends Vec[T]
 
 object Vec:
-  def fill[T](n: BigInt with n >= 0, v: T): {r: Vec[T] with r.len == n} =
+  def fill[T](n: {v: BigInt with v >= 0}, v: T): {r: Vec[T] with r.len == n} =
     if (n == 0) Nil[T]().ck
-    else Cons(v, fill((n - 1).ck, v)).ck
+    else Cons(v, fill((n - 1).ck[{v: BigInt with v >= 0}], v)).ck
 
 extension [T](x: T)
   @ignore
