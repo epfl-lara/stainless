b886e7e5e7f6cbe1ff174390d5789794e8f1ed60
Katarzyna Marek <kasia@marek.net>
2026-03-23 14:44:31 +0100

    sort args in function closure

---
+++ frontends/benchmarks/refinement/invalid/FunctionClosure.scala
@@ -0,0 +1,12 @@
+
+// fails on missing measure for termination of funWhile
+
+// this test only checks that the parameters are in correct order in the closure
+//  meaning, this:
+//    def funWhile(f: ({ param0: BigInt | (from <= param0) }) => Unit, from: BigInt, i: BigInt): (Unit, BigInt)
+//  would be incorrect, since `f` type depends on `from`.
+def fun(from: BigInt)(f: {i: BigInt with from <= i} => Unit): Unit =
+  var i: BigInt = from
+  while i < 10 do
+    f(i.asInstanceOf[{i: BigInt with from <= i}])
+    i = i + 1
