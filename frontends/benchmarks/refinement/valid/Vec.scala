3ead4330f88cbb57d689260583b159333365885b
Katarzyna Marek <kasia@marek.net>
2026-03-23 11:36:44 +0100

    keep refinements on type aliases that are base values

--- frontends/benchmarks/refinement/valid/Vec.scala
+++ frontends/benchmarks/refinement/valid/Vec.scala
@@ -2,12 +2,11 @@ import stainless.annotation.ignore
 import scala.annotation.nowarn
 import stainless.lang._
 
-// using a type alias instead of inlining the type makes stainless sad :(
-// type Pos = {v: BigInt with v >= 0}
+type Pos = {v: BigInt with v >= 0}
 
 sealed trait Vec[T]:
-  def len: {v: BigInt with v >= 0} = this match
-    case Nil() => (0: BigInt).ck
+  def len: Pos = this match
+    case Nil() => BigInt(0).ck
     case Cons(_, tail) => (1 + tail.len).ck
 
   def zip[S](b: Vec[S] with b.len == len): {r: Vec[(T, S)] with r.len == len} =
@@ -25,9 +24,9 @@ case class Nil[T]() extends Vec[T]
 case class Cons[T](head: T, tail: Vec[T]) extends Vec[T]
 
 object Vec:
-  def fill[T](n: {v: BigInt with v >= 0}, v: T): {r: Vec[T] with r.len == n} =
+  def fill[T](n: Pos, v: T): {r: Vec[T] with r.len == n} =
     if (n == 0) Nil[T]().ck
-    else Cons(v, fill((n - 1).ck[{v: BigInt with v >= 0}], v)).ck
+    else Cons(v, fill((n - 1).ck[Pos], v)).ck
 
 extension [T](x: T)
   @ignore
