32e83a29a69cec7d5b0d42dcc560f362a4a08928
Katarzyna Marek <kasia@marek.net>
2026-03-25 17:43:46 +0100

    add tests for partial checks

--- frontends/common/src/it/scala/stainless/verification/RefinementSuite.scala
+++ frontends/common/src/it/scala/stainless/verification/RefinementSuite.scala
@@ -15,7 +15,19 @@ class RefinementSuite extends VerificationComponentTestSuite {
 
   testNegAll("refinement/invalid", invalid)
 }
+
+class RefinementSuitePartial extends VerificationComponentTestSuite {
+
+  override protected def optionsString(options: inox.Options): String = 
+    "check-measures=false infer-measures=false strict-arithmetic=false"
+
+  import RefinementSuite._
+
+  testPosAll("refinement/valid-partial", partial)
+}
+
 object RefinementSuite {
   private lazy val valid = ComponentTestSuite.loadPrograms("refinement/valid")
+  lazy val partial = ComponentTestSuite.loadPrograms("refinement/valid-partial")
   private lazy val invalid = ComponentTestSuite.loadPrograms("refinement/invalid")
 }
