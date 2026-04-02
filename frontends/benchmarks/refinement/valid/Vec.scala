708b4877a6cbc675811a5b4f35e1aac455e38dbf
Katarzyna Marek <kasia@marek.net>
2026-04-02 15:31:13 +0200

    fix: handle `$asInstanceOf` correctly (enables silent mode)

--- frontends/benchmarks/refinement/valid/Vec.scala
+++ frontends/benchmarks/refinement/valid/Vec.scala
@@ -1,3 +1,4 @@
+import language.experimental.qualifiedTypes.silent
 import stainless.annotation.ignore
 import scala.annotation.nowarn
 import stainless.lang._
@@ -6,28 +7,26 @@ type Pos = {v: BigInt with v >= 0}
 
 sealed trait Vec[T]:
   def len: Pos = this match
-    case Nil() => BigInt(0).ck
-    case Cons(_, tail) => (1 + tail.len).ck
+    case Nil() => BigInt(0)
+    case Cons(_, tail) => (1 + tail.len)
 
   def zip[S](b: Vec[S] with b.len == len): {r: Vec[(T, S)] with r.len == len} =
     ((this, b) : @nowarn) match
-      case (Nil(), Nil()) => Nil[(T, S)]().ck
+      case (Nil(), Nil()) => Nil[(T, S)]()
       case (Cons(head, tail), Cons(bHead, bTail)) => 
-        Cons((head, bHead), tail.zip(bTail.ck[{b: Vec[S] with b.len == tail.len}])).ck
+        Cons((head, bHead), tail.zip(bTail))
 
   def concat(b: Vec[T]): {r: Vec[T] with r.len == len + b.len } =
     this match
-      case Nil() => b.ck
-      case Cons(head, tail) => Cons(head, tail.concat(b)).ck
+      case Nil() => b
+      case Cons(head, tail) => Cons(head, tail.concat(b))
 
 case class Nil[T]() extends Vec[T]
 case class Cons[T](head: T, tail: Vec[T]) extends Vec[T]
 
 object Vec:
   def fill[T](n: Pos, v: T): {r: Vec[T] with r.len == n} =
-    if (n == 0) Nil[T]().ck
-    else Cons(v, fill((n - 1).ck[Pos], v)).ck
-
-extension [T](x: T)
-  @ignore
-  inline def ck[TT] = x.asInstanceOf[TT]
+    if (n == 0) Nil[T]()
+    else
+      val nMinus1: Pos = n - 1
+      Cons(v, fill(nMinus1, v))
