868dc90e998a46a84e31ac0f54e0a84c482d1fb9
Katarzyna Marek <kasia@marek.net>
2026-05-19 13:09:39 +0200

    Revert "fix extracting while"
    This reverts commit cf532419af4325359295f135e2d54eaa2fb5365e.

--- frontends/dotty/src/main/scala/stainless/frontends/dotc/ASTExtractors.scala
+++ frontends/dotty/src/main/scala/stainless/frontends/dotc/ASTExtractors.scala
@@ -1274,7 +1274,6 @@ trait ASTExtractors {
           case WithInvariant(invariant, rest) => parseWhile(rest, Some(invariant), optWeakInv, inline, opaque)
           case WithWeakInvariant(invariant, rest) => parseWhile(rest, optInv, Some(invariant), inline, opaque)
           case WhileDo(cond, body) => Some((cond, body, optInv, optWeakInv, inline, opaque))
-          case Typed(rest, _) => parseWhile(rest, optInv, optWeakInv, inline, opaque)
           case _ => None
         }
       }
