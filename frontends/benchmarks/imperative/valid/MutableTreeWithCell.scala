8464f6f45e11ca6065a1bb27904510f8b6aaf851
Samuel Chassot <samuel.chassot@epfl.ch>
2026-06-02 22:29:17 +0200

    rename

--- frontends/benchmarks/imperative/valid/MutableTreeWithCell.scala
+++ frontends/benchmarks/imperative/valid/MutableTreeWithCell.scala
@@ -4,22 +4,20 @@ import stainless.lang.decreases
 import stainless.collection.*
 import stainless.annotation.*
 
-object CellFunction:
+object MutableTreeWithCell:
   sealed trait Tree
   case class Node(l: Cell[Tree], r: Cell[Tree]) extends Tree
   case class Leaf(x: BigInt) extends Tree
 
-  def mirror(t: Tree): Unit = {
+  def mirror(t: Tree): Unit = 
     t match
       case Node(l, r) =>
         mirror(l.v)
         mirror(r.v)
         swap(l, r)
       case Leaf(_) => ()
-  }
 
-  def main(): Unit = {
+  def main(): Unit = 
     val t = Node(Cell(Node(Cell(Leaf(1)), Cell(Leaf(2)))), Cell(Node(Cell(Leaf(3)), Cell(Leaf(4)))))
     mirror(t)
     assert(t == Node(Cell(Node(Cell(Leaf(4)), Cell(Leaf(3)))), Cell(Node(Cell(Leaf(2)), Cell(Leaf(1))))))
-  }
