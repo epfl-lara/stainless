61e32aad1a68b6379e3bd39a6c6d2752c7572c9b
Samuel Chassot <samuel.chassot@epfl.ch>
2026-06-25 15:16:02 +0200

    more benchmarks

--- frontends/benchmarks/refinement/valid/OldInRefinement.scala
+++ frontends/benchmarks/refinement/valid/OldInRefinement.scala
@@ -7,3 +7,7 @@ case class Box(var value: BigInt)
 def incrementBox(b: Box): {res: Unit with b.value == old(b).value + 1} = b.value += 1
 
 def incrementBy(b: Box, k: BigInt): {res: Unit with b.value == old(b).value + k} = b.value += k
+
+case class Box2(var value: BigInt):
+  def getValue(): BigInt = value
+  def increment(): {res: Unit with getValue() == old(this).getValue() + 1} = value += 1
