ce32fd15ce96ff48abd5077af49250d811614521
Samuel Chassot <samuel.chassot@epfl.ch>
2026-08-20 11:42:51 +0200

    more minimal example for issue 1775

---
+++ frontends/benchmarks/refinement/valid/ParamMutationIssue1775.scala
@@ -0,0 +1,11 @@
+package ParamMutationIssue1775
+import stainless.lang._
+import stainless.annotation._
+
+object A:
+  @mutable
+  case class Box(var value: BigInt)
+
+  def updateBox(b: Box with b.value >= 0, v: BigInt with v == b.value): {res: Unit with b.value == b.value * 2} = {
+    b.value = b.value + v
+  }
