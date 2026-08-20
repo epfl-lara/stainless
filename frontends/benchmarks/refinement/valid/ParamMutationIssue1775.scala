6688fe057372a9203215ea74db96f16262d2ea69
Samuel Chassot <samuel.chassot@epfl.ch>
2026-08-20 15:06:40 +0200

    more tests

--- frontends/benchmarks/refinement/valid/ParamMutationIssue1775.scala
+++ frontends/benchmarks/refinement/valid/ParamMutationIssue1775.scala
@@ -9,3 +9,19 @@ object A:
   def updateBox(b: Box with b.value >= 0, v: BigInt with v == b.value): {res: Unit with b.value >= 0} = {
     b.value = b.value + v
   }
+
+  def updateBox2(b: Box with b.value >= 0): {res: Unit with b.value >= 0} = {
+    val v: BigInt with v == b.value = b.value
+    val x: BigInt with x == b.value = b.value
+    b.value = b.value + v
+    assert(v == x)
+  }
+
+  def updateBox3(b: Box with b.value >= 0): {res: Unit with b.value >= 0} = {
+    // Let say the the b parameter gets the index b$0
+    b.value = b.value + 1 // now we have b$1.value == b$0.value + 1
+    val v: BigInt with v == b.value = b.value // == b$1.value == b$0.value + 1
+    val x: BigInt with x == b.value = b.value // == b$1.value == b$0.value + 1
+    b.value = b.value + v // now we have b$2.value == b$1.value + v, with v == b$1.value == b$0.value + 1
+    assert(v == x) // should hold, as both are equal to b$1.value
+  }
