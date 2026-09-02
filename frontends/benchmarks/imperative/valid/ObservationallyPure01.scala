a9cc3421c70ca24a08e517c1429db5aa4accdf59
Samuel Chassot <samuel.chassot@epfl.ch>
2026-09-02 16:14:11 +0200

    real usecase

--- frontends/benchmarks/imperative/valid/ObservationallyPure01.scala
+++ frontends/benchmarks/imperative/valid/ObservationallyPure01.scala
@@ -1,10 +1,21 @@
+import stainless.annotation._
 
 object ObservationallyPure01 {
-  def impureFunction(x: BigInt): BigInt = {
-    x + 1
-  }
-
-  def pureFunction(x: BigInt): BigInt = {
-    x + 1
+  
+  case class Computer(@internallyMutable private var lastComputed: (BigInt, BigInt)){
+    def pureFactorial(x: BigInt): BigInt = {
+      require(x >= 0)
+      if (x == 0) then 1
+      else x * pureFactorial(x - 1)
+    }
+    def cachedFactorial(x: BigInt): BigInt = {
+      require(x >= 0)
+      if (x == lastComputed._1) then lastComputed._2
+      else {
+        val result = pureFactorial(x)
+        lastComputed = (x, result)
+        result
+      }
+    }.ensuring(sameAs(pureFactorial(x)))
   }
 }
